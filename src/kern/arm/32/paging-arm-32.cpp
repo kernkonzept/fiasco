d5a1776d7e6416dfe3ea7ae172e322a2c541e45e
Alexander Warg <alexander.warg@kernkonzept.com>
2016-08-29 00:00:00 +0000

    ARM: split out 32bit code (paging-arm)
    Change-Id: I08f1aa41a6fb954968b98aa7e72ec3d052db48ac

---
+++ src/kern/arm/32/paging-arm-32.cpp
@@ -0,0 +1,19 @@
+INTERFACE [arm && arm_lpae]:
+
+EXTENSION class K_pte_ptr
+{
+public:
+  enum
+  {
+    Super_level    = 1,
+    Max_level      = 2,
+  };
+};
+
+typedef Ptab::Tupel< Ptab::Traits< Unsigned64, 30, 2, true>,
+                     Ptab::Traits< Unsigned64, 21, 9, true>,
+                     Ptab::Traits< Unsigned64, 12, 9, true> >::List Ptab_traits;
+
+typedef Ptab::Shift<Ptab_traits, Virt_addr::Shift>::List Ptab_traits_vpn;
+typedef Ptab::Page_addr_wrap<Page_number, Virt_addr::Shift> Ptab_va_vpn;
+
