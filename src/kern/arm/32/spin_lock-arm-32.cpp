d8f4bade34e83af30ceb52ce2fee1c1b1e656ada
Alexander Warg <alexander.warg@kernkonzept.com>
2016-08-29 00:00:00 +0000

    ARM: move 32bit specific code
     * spin_lock
     * mem_op
     * mem_unit
     * perf_cnt
    
    Change-Id: Ic74da3a634169afe8cadba4d7e0919c353d2fc41

---
+++ src/kern/arm/32/spin_lock-arm-32.cpp
@@ -0,0 +1,62 @@
+IMPLEMENTATION [arm && mp]:
+
+#include "processor.h"
+
+PRIVATE template<typename Lock_t> inline NEEDS["processor.h"]
+void
+Spin_lock<Lock_t>::lock_arch()
+{
+  Lock_t dummy, tmp;
+
+#define L(z) \
+  __asm__ __volatile__ ( \
+      "1: ldr" #z "     %[d], [%[lock]]           \n" \
+      "   tst     %[d], #2                  \n" /* Arch_lock == #2 */ \
+      "   wfene                             \n" \
+      "   bne     1b                        \n" \
+      "   ldrex"#z"   %[d], [%[lock]]           \n" \
+      "   tst     %[d], #2                  \n" \
+      "   orr     %[tmp], %[d], #2          \n" \
+      "   strex"#z"eq %[d], %[tmp], [%[lock]]   \n" \
+      "   teqeq   %[d], #0                  \n" \
+      "   bne     1b                        \n" \
+      : [d] "=&r" (dummy), [tmp] "=&r"(tmp), "+m" (_lock) \
+      : [lock] "r" (&_lock) \
+      : "cc" \
+      )
+  extern char __use_of_invalid_type_for_Spin_lock__sizeof_is_invalid;
+  switch(sizeof(Lock_t))
+    {
+    case 1: L(b); break;
+    case 2: L(h); break;
+    case 4: L(); break;
+    default: __use_of_invalid_type_for_Spin_lock__sizeof_is_invalid = 10; break;
+    }
+
+#undef L
+}
+
+PRIVATE template<typename Lock_t> inline
+void
+Spin_lock<Lock_t>::unlock_arch()
+{
+  Lock_t tmp;
+#define UNL(z) \
+  __asm__ __volatile__( \
+      "ldr"#z " %[tmp], %[lock]             \n" \
+      "bic %[tmp], %[tmp], #2          \n" /* Arch_lock == #2 */ \
+      "str"#z " %[tmp], %[lock]             \n" \
+      "mcr p15, 0, %[tmp], c7, c10, 4  \n" /* drain write buffer */ \
+      "sev                             \n" \
+      : [lock] "=m" (_lock), [tmp] "=&r" (tmp))
+  extern char __use_of_invalid_type_for_Spin_lock__sizeof_is_invalid;
+  switch (sizeof(Lock_t))
+    {
+    case 1: UNL(b); break;
+    case 2: UNL(h); break;
+    case 4: UNL(); break;
+    default: __use_of_invalid_type_for_Spin_lock__sizeof_is_invalid = 11; break;
+    }
+#undef UNL
+}
+
--- src/kern/arm/mem_unit.cpp
+++ src/kern/arm/mem_unit.cpp
@@ -23,20 +23,6 @@ public:
 //---------------------------------------------------------------------------
 IMPLEMENTATION [arm]:
 
-IMPLEMENT inline
-void Mem_unit::tlb_flush()
-{
-  asm volatile("mcr p15, 0, %0, c8, c7, 0" // TLBIALL
-               : : "r" (0) : "memory");
-}
-
-IMPLEMENT inline
-void Mem_unit::dtlb_flush(void *va)
-{
-  asm volatile("mcr p15, 0, %0, c8, c6, 1" // DTLBIMVA
-               : : "r" ((unsigned long)va & 0xfffff000) : "memory");
-}
-
 IMPLEMENT_DEFAULT inline NEEDS[Mem_unit::tlb_flush]
 void Mem_unit::kernel_tlb_flush()
 { tlb_flush(); }
@@ -47,33 +33,18 @@ Mem_unit::make_coherent_to_pou(void const *v)
 { clean_dcache(v); }
 
 //---------------------------------------------------------------------------
-IMPLEMENTATION [arm && armv5]:
-
-IMPLEMENT inline
-void Mem_unit::tlb_flush(void *va, unsigned long)
-{
-  asm volatile("mcr p15, 0, %0, c8, c7, 1"
-               : : "r" ((unsigned long)va & 0xfffff000) : "memory");
-}
-
-IMPLEMENT inline
-void Mem_unit::tlb_flush(unsigned long)
-{
-  asm volatile("mcr p15, 0, r0, c8, c7, 0" : : "r" (0) : "memory");
-}
-
-//---------------------------------------------------------------------------
-IMPLEMENTATION [arm && (armv6 || armv7) && !hyp]:
+IMPLEMENTATION [arm && armv8 && !hyp]:
 
 IMPLEMENT inline
 void Mem_unit::tlb_flush(void *va, unsigned long asid)
 {
   if (asid == Asid_invalid)
     return;
-  btc_flush();
+
   Mem::dsb();
-  asm volatile("mcr p15, 0, %0, c8, c7, 1" // TLBIMVA
-               : : "r" (((unsigned long)va & 0xfffff000) | asid) : "memory");
+  asm volatile("tlbi vae1, %0"
+               : : "r" (((unsigned long)va >> 12)
+                        | (asid << 48)) : "memory");
 }
 
 IMPLEMENT inline
@@ -81,59 +52,6 @@ void Mem_unit::tlb_flush(unsigned long asid)
 {
   btc_flush();
   Mem::dsb();
-  asm volatile("mcr p15, 0, %0, c8, c7, 2" // TLBIASID
-               : : "r" (asid) : "memory");
+  asm volatile("tlbi aside1, %0" // TLBIASID
+               : : "r" (asid << 48) : "memory");
 }
-
-//---------------------------------------------------------------------------
-IMPLEMENTATION [arm && (armv6 || armv7) && hyp]:
-
-IMPLEMENT inline
-void Mem_unit::tlb_flush(void *va, unsigned long asid)
-{
-  if (asid == Asid_invalid)
-    return;
-  btc_flush();
-  Mem::dsb();
-  Mword t1, t2;
-  if (1) asm volatile(
-      "mrrc p15, 6, %[tmp1], %[tmp2], c2 \n"
-      "mcrr p15, 6, %[tmp1], %[asid], c2 \n"
-      "isb \n"
-      "mcr  p15, 0, %[mva], c8, c7, 3 \n"
-      "mcrr p15, 6, %[tmp1], %[tmp2], c2 \n"
-      : [tmp1] "=&r" (t1), [tmp2] "=&r" (t2)
-      : [mva] "r" (((unsigned long)va & 0xfffff000)), [asid] "r" (asid << 16)
-      : "memory");
-
-  if (0) asm volatile ( "mcr p15, 4, r0, c8, c7, 4" );
-}
-
-IMPLEMENT inline
-void Mem_unit::tlb_flush(unsigned long asid)
-{
-  btc_flush();
-  Mem::dsb();
-  Mword t1, t2;
-  if (1) asm volatile(
-      "mrrc p15, 6, %[tmp1], %[tmp2], c2 \n"
-      "mcrr p15, 6, %[tmp1], %[asid], c2 \n"
-      "isb \n"
-      "mcr  p15, 0, %[tmp1], c8, c7, 0 \n"
-      "mcrr p15, 6, %[tmp1], %[tmp2], c2 \n"
-      : [tmp1] "=&r" (t1), [tmp2] "=&r" (t2)
-      : [asid] "r" (asid << 16)
-      : "memory");
-
-  if (0) asm volatile ( "mcr p15, 4, r0, c8, c7, 4" );
-}
-
-//---------------------------------------------------------------------------
-IMPLEMENTATION [arm && hyp]:
-
-IMPLEMENT_OVERRIDE inline
-void Mem_unit::kernel_tlb_flush()
-{
-  asm volatile("mcr p15, 4, r0, c8, c7, 0" : : "r" (0) : "memory");
-}
-
