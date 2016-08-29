c52291641047d11b5fb92ff0372f5642b6744e41
Alexander Warg <alexander.warg@kernkonzept.com>
2016-08-29 00:00:00 +0000

    ARM: split out 32bit code from kmem_space.cpp
    Change-Id: I227ca4404a0a49d5bdf477e5729db035ce2c3006

--- src/kern/arm/kmem_space.cpp
+++ src/kern/arm/kmem_space.cpp
@@ -57,28 +57,3 @@ IMPLEMENT inline
 Kpdir *Kmem_space::kdir()
 { return _kdir; }
 
-// initialize the kernel space (page table)
-IMPLEMENT
-void Kmem_space::init()
-{
-  unsigned domains = 0x0001;
-
-  asm volatile("mcr p15, 0, %0, c3, c0" : : "r" (domains));
-
-  Mem_unit::clean_vdcache();
-}
-
-// always 16kB also for LPAE we use 4 consecutive second level tables
-char kernel_page_directory[0x4000]
-  __attribute__((aligned(0x4000), section(".bss.kernel_page_dir")));
-
-//----------------------------------------------------------------------------------
-IMPLEMENTATION[arm && !arm_lpae]:
-
-Kpdir *Kmem_space::_kdir = (Kpdir *)&kernel_page_directory;
-
-//----------------------------------------------------------------------------------
-IMPLEMENTATION[arm && arm_lpae]:
-
-Unsigned64 kernel_lpae_dir[4] __attribute__((aligned(4 * sizeof(Unsigned64))));
-Kpdir *Kmem_space::_kdir = (Kpdir *)&kernel_lpae_dir;
