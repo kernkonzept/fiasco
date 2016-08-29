4b2f492e1e14b43c1fe5eb52d685a3cfb8fadde8
Alexander Warg <alexander.warg@kernkonzept.com>
2016-08-29 00:00:00 +0000

    ARM: split out 32bit code (mem_layout)
    Change-Id: I4a609f58c87fd40eaeece05f6de493f7587fcb5e

--- src/kern/arm/mem_layout-arm.cpp
+++ src/kern/arm/mem_layout-arm.cpp
@@ -1,52 +1,18 @@
-INTERFACE [arm && !kern_start_0xd && !hyp]:
-
-EXTENSION class Mem_layout
-{
-public:
-  enum Virt_layout_umax : Address {
-    User_max             = 0xbfffffff,
-  };
-};
-
-
-//---------------------------------------------------------------------------
-INTERFACE [arm && kern_start_0xd]:
-
-EXTENSION class Mem_layout
-{
-public:
-  enum Virt_layout_umax : Address {
-    User_max             = 0xcfffffff,
-  };
-
-};
-//---------------------------------------------------------------------------
-INTERFACE [arm && hyp]:
-
-EXTENSION class Mem_layout
-{
-public:
-  enum Virt_layout_umax : Address {
-    User_max             = 0xffffffff,
-  };
-};
-
-
-//---------------------------------------------------------------------------
 INTERFACE [arm]:
 
+#include "config.h"
+
 EXTENSION class Mem_layout
 {
 public:
-  enum Virt_layout : Address {
-    Kern_lib_base	 = 0xffffe000,
-    Syscalls		 = 0xfffff000,
-    Utcb_addr            = User_max + 1 - 0x10000,
-  };
-
   enum Phys_layout : Address {
     Sdram_phys_base      = RAM_PHYS_BASE
   };
+
+  static inline unsigned long round_superpage(unsigned long addr)
+  { return (addr + Config::SUPERPAGE_SIZE - 1) & ~(Config::SUPERPAGE_SIZE-1); }
+  static inline unsigned long trunc_superpage(unsigned long addr)
+  { return addr & ~(Config::SUPERPAGE_SIZE-1); }
 };
 
 //---------------------------------------------------------------------------
@@ -66,40 +32,6 @@ public:
     Map_base             = RAM_PHYS_BASE,
     Ivt_base             = 0xffff0000,
   };
-};
-
-
-//---------------------------------------------------------------------------
-INTERFACE [arm && !hyp]:
-
-#include "template_math.h"
-
-EXTENSION class Mem_layout
-{
-public:
-  enum Virt_layout_kern : Address {
-    Service_page         = 0xeac00000,
-    Tbuf_status_page     = Service_page + 0x5000,
-    Tbuf_ustatus_page    = Tbuf_status_page,
-    Tbuf_buffer_area	 = Service_page + 0x200000,
-    Tbuf_ubuffer_area    = Tbuf_buffer_area,
-    Jdb_tmp_map_area     = Service_page + 0x400000,
-    Registers_map_start  = 0xee000000,
-    Registers_map_end    = 0xef000000,
-    Cache_flush_area     = 0xef000000,
-    Cache_flush_area_end = 0xef100000,
-    Map_base             = 0xf0000000,
-    Pmem_start           = 0xf0400000,
-    Pmem_end             = 0xf5000000,
-
-    Caps_start           = 0xf5000000,
-    Caps_end             = 0xfd000000,
-    Utcb_ptr_page        = 0xffffd000,
-    // don't care about caches here, because arm uses a register on MP
-    utcb_ptr_align       = Tl_math::Ld<sizeof(void*)>::Res,
-    Ivt_base             = 0xffff0000,
-  };
-
 };
 
 // -------------------------------------------------------------------------
