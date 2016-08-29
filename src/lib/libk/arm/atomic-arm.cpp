7d90deeebcf240d9328d7f46f8b23f8140d5390c
Alexander Warg <alexander.warg@kernkonzept.com>
2016-08-29 00:00:00 +0000

    ARM: restructure atomic.cpp code
    Change-Id: I65ddf2080038cfc76edaae52b65d9a286c1fa165

---
+++ src/lib/libk/arm/atomic-arm.cpp
@@ -0,0 +1,16 @@
+IMPLEMENTATION [arm && armv6plus]:
+
+inline
+void
+atomic_or(Mword *l, Mword value)
+{ atomic_mp_or(l, value); }
+
+inline bool
+cas_unsafe(Mword *ptr, Mword oldval, Mword newval)
+{ return mp_cas_arch(ptr, oldval, newval); }
+
+inline
+void
+atomic_and(Mword *l, Mword mask)
+{ atomic_mp_and(l, mask); }
+
