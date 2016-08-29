d596868e6df8716fa84346cef2e01048aef91136
Alexander Warg <alexander.warg@kernkonzept.com>
2016-08-29 00:00:00 +0000

    ARM: split out 32bit code from processor-arm.cpp
    Change-Id: I2b6051252147cf7233b07dbe4a41bb409a1925e6

--- src/drivers/arm/processor-arm.cpp
+++ src/drivers/arm/processor-arm.cpp
@@ -74,7 +74,6 @@ private:
 public:
   enum : unsigned
   {
-    Status_mode_user           = 0x10,
     Status_mode_mask           = 0x1f,
 
     Status_interrupts_disabled = Status_FIQ_disabled | Status_IRQ_disabled,
@@ -94,6 +93,7 @@ public:
   static Cpu_phys_id cpu_id();
 };
 
+//--------------------------------------------------------------------
 INTERFACE[arm && !arm_em_tz]:
 
 #define ARM_CPS_INTERRUPT_FLAGS "if"
@@ -107,10 +107,11 @@ public:
       Sti_mask                = Status_interrupts_disabled,
       Status_preempt_disabled = Status_IRQ_disabled,
       Status_interrupts_mask  = Status_interrupts_disabled,
-      Status_always_mask      = 0x110,
+      Status_always_mask      = Status_mode_always_on,
     };
 };
 
+//--------------------------------------------------------------------
 INTERFACE[arm && arm_em_tz]:
 
 #define ARM_CPS_INTERRUPT_FLAGS "f"
@@ -124,20 +125,23 @@ public:
       Sti_mask                = Status_FIQ_disabled,
       Status_preempt_disabled = Status_FIQ_disabled,
       Status_interrupts_mask  = Status_FIQ_disabled,
-      Status_always_mask      = 0x110 | Status_IRQ_disabled,
+      Status_always_mask      = Status_mode_always_on | Status_IRQ_disabled,
     };
 };
 
+//--------------------------------------------------------------------
 INTERFACE[arm && !hyp]:
 
 EXTENSION class Proc
 { public: enum : unsigned { Is_hyp = 0, Status_mode_supervisor = PSR_m_svc }; };
 
+//--------------------------------------------------------------------
 INTERFACE[arm && hyp]:
 
 EXTENSION class Proc
 { public: enum : unsigned { Is_hyp = 1, Status_mode_supervisor = PSR_m_hyp }; };
 
+//--------------------------------------------------------------------
 IMPLEMENTATION[arm]:
 
 #include "types.h"
@@ -157,22 +161,6 @@ void Proc::stack_pointer(Mword sp)
   asm volatile ("mov sp, %0" : : "r" (sp));
 }
 
-IMPLEMENT static inline
-Mword Proc::program_counter()
-{
-  Mword pc;
-  asm ("mov %0, pc" : "=r" (pc));
-  return pc;
-}
-
-IMPLEMENT static inline
-Proc::Status Proc::interrupts()
-{
-  Status ret;
-  asm volatile("mrs %0, cpsr" : "=r" (ret));
-  return !(ret & Sti_mask);
-}
-
 IMPLEMENT static inline ALWAYS_INLINE
 void Proc::sti_restore(Status st)
 {
@@ -186,89 +174,12 @@ void Proc::irq_chance()
   asm volatile ("nop; nop" : : : "memory");
 }
 
-IMPLEMENTATION [arm && !armv6plus]:
-
-IMPLEMENT static inline
-void Proc::cli()
-{
-  Mword v;
-  asm volatile("mrs %0, cpsr    \n"
-               "orr %0, %0, %1  \n"
-               "msr cpsr_c, %0  \n"
-               : "=r" (v)
-               : "I" (Cli_mask)
-               : "memory");
-}
-
-IMPLEMENT static inline ALWAYS_INLINE
-void Proc::sti()
-{
-  Mword v;
-  asm volatile("mrs %0, cpsr    \n"
-               "bic %0, %0, %1  \n"
-               "msr cpsr_c, %0  \n"
-               : "=r" (v)
-               : "I" (Sti_mask)
-               : "memory");
-}
-
-IMPLEMENT static inline ALWAYS_INLINE
-Proc::Status Proc::cli_save()
-{
-  Status ret;
-  Mword v;
-  asm volatile("mrs %0, cpsr    \n"
-               "orr %1, %0, %2  \n"
-               "msr cpsr_c, %1  \n"
-               : "=r" (ret), "=r" (v)
-               : "I" (Cli_mask)
-               : "memory");
-  return ret;
-}
-
-IMPLEMENTATION [arm && armv6plus]:
-
-IMPLEMENT static inline
-void Proc::cli()
-{
-  asm volatile("cpsid " ARM_CPS_INTERRUPT_FLAGS : : : "memory");
-}
-
-IMPLEMENT static inline ALWAYS_INLINE
-void Proc::sti()
-{
-  asm volatile("cpsie " ARM_CPS_INTERRUPT_FLAGS : : : "memory");
-}
-
-IMPLEMENT static inline ALWAYS_INLINE
-Proc::Status Proc::cli_save()
-{
-  Status prev;
-  asm volatile("mrs %0, cpsr \n"
-               "cpsid " ARM_CPS_INTERRUPT_FLAGS
-               : "=r" (prev)
-               :
-               : "memory");
-  return prev;
-}
-
 //----------------------------------------------------------------
 IMPLEMENTATION[arm && !mp]:
 
 IMPLEMENT static inline
 Cpu_phys_id Proc::cpu_id()
 { return Cpu_phys_id(0); }
-
-//----------------------------------------------------------------
-IMPLEMENTATION[arm && mp]:
-
-IMPLEMENT static inline
-Cpu_phys_id Proc::cpu_id()
-{
-  unsigned mpidr;
-  __asm__("mrc p15, 0, %0, c0, c0, 5": "=r" (mpidr));
-  return Cpu_phys_id(mpidr & 0x7); // mind gic softirq
-}
 
 //----------------------------------------------------------------
 IMPLEMENTATION[arm && (pxa || sa1100 || s3c2410)]:
@@ -280,66 +191,6 @@ void Proc::halt()
 IMPLEMENT static inline
 void Proc::pause()
 {}
-
-//----------------------------------------------------------------
-IMPLEMENTATION[arm && 926]:
-
-IMPLEMENT static inline
-void Proc::pause()
-{
-}
-
-IMPLEMENT static inline
-void Proc::halt()
-{
-  Status f = cli_save();
-  asm volatile("mov     r0, #0                                              \n\t"
-               "mrc     p15, 0, r1, c1, c0, 0       @ Read control register \n\t"
-               "mcr     p15, 0, r0, c7, c10, 4      @ Drain write buffer    \n\t"
-               "bic     r2, r1, #1 << 12                                    \n\t"
-               "mcr     p15, 0, r2, c1, c0, 0       @ Disable I cache       \n\t"
-               "mcr     p15, 0, r0, c7, c0, 4       @ Wait for interrupt    \n\t"
-               "mcr     15, 0, r1, c1, c0, 0        @ Restore ICache enable \n\t"
-               :::"memory",
-               "r0", "r1", "r2", "r3", "r4", "r5",
-               "r6", "r7", "r8", "r9", "r10", "r11",
-               "r12", "r13", "r14", "r15"
-      );
-  sti_restore(f);
-}
-
-//----------------------------------------------------------------
-IMPLEMENTATION[arm && arm1136]:
-
-IMPLEMENT static inline
-void Proc::pause()
-{}
-
-IMPLEMENT static inline
-void Proc::halt()
-{
-  Status f = cli_save();
-  asm volatile("mcr     p15, 0, r0, c7, c10, 4  @ DWB/DSB \n\t"
-               "mcr     p15, 0, r0, c7, c0, 4   @ WFI \n\t");
-  sti_restore(f);
-}
-
-
-//----------------------------------------------------------------
-IMPLEMENTATION[arm && (arm1176 || mpcore)]:
-
-IMPLEMENT static inline
-void Proc::pause()
-{}
-
-IMPLEMENT static inline
-void Proc::halt()
-{
-  Status f = cli_save();
-  asm volatile("mcr     p15, 0, r0, c7, c10, 4  @ DWB/DSB \n\t"
-               "wfi \n\t");
-  sti_restore(f);
-}
 
 //----------------------------------------------------------------
 IMPLEMENTATION[arm && (armca8 || armca9)]:
