20970eb5c75a9f20a948a33732073226ef0347f7
Alexander Warg <alexander.warg@kernkonzept.com>
2016-08-29 00:00:00 +0000

    ARM: split out 32bit code from cpu-arm.cpp
    Change-Id: I47284de62ae8877eed0ab452f4bbf4657c79b1ac

---
+++ src/kern/arm/32/cpu-arm-32.cpp
@@ -0,0 +1,107 @@
+INTERFACE [arm]:
+
+EXTENSION class Cpu
+{
+public:
+  enum {
+    Cp15_c1_cache_enabled  = Cp15_c1_generic | Cp15_c1_cache_bits,
+    Cp15_c1_cache_disabled = Cp15_c1_generic,
+  };
+  bool has_generic_timer() const { return (_cpu_id._pfr[1] & 0xf0000) == 0x10000; }
+};
+
+//-------------------------------------------------------------------------
+IMPLEMENTATION [arm]:
+
+PRIVATE static inline
+Mword
+Cpu::midr()
+{
+  Mword m;
+  asm volatile ("mrc p15, 0, %0, c0, c0, 0" : "=r" (m));
+  return m;
+}
+
+IMPLEMENT
+void Cpu::early_init()
+{
+  // switch to supervisor mode and intialize the memory system
+  asm volatile ( " mov  r2, r13             \n"
+                 " mov  r3, r14             \n"
+                 " msr  cpsr_c, %1          \n"
+                 " mov  r13, r2             \n"
+                 " mov  r14, r3             \n"
+
+                 " mcr  p15, 0, %0, c1, c0  \n"
+                 :
+                 : "r" (Config::Cache_enabled
+                        ? Cp15_c1_cache_enabled : Cp15_c1_cache_disabled),
+                   "I" (Proc::Status_mode_supervisor
+                        | Proc::Status_interrupts_disabled)
+                 : "r2", "r3");
+
+  early_init_platform();
+
+  Mem_unit::flush_cache();
+}
+
+PUBLIC static inline
+void
+Cpu::enable_dcache()
+{
+  asm volatile("mrc     p15, 0, %0, c1, c0, 0 \n"
+               "orr     %0, %1                \n"
+               "mcr     p15, 0, %0, c1, c0, 0 \n"
+               : : "r" (0), "i" (Cp15_c1_cache));
+}
+
+PUBLIC static inline
+void
+Cpu::disable_dcache()
+{
+  asm volatile("mrc     p15, 0, %0, c1, c0, 0 \n"
+               "bic     %0, %1                \n"
+               "mcr     p15, 0, %0, c1, c0, 0 \n"
+               : : "r" (0), "i" (Cp15_c1_cache));
+}
+
+//---------------------------------------------------------------------------
+IMPLEMENTATION [arm && armv6plus]:
+
+PRIVATE static inline
+void
+Cpu::modify_actrl(Mword set_mask, Mword clear_mask)
+{
+  Mword t;
+  asm volatile("mrc p15, 0, %[reg], c1, c0, 1 \n\t"
+               "bic %[reg], %[reg], %[clr]    \n\t"
+               "orr %[reg], %[reg], %[set]    \n\t"
+               "mcr p15, 0, %[reg], c1, c0, 1 \n\t"
+               : [reg] "=r" (t)
+               : [set] "r" (set_mask), [clr] "r" (clear_mask));
+}
+
+PRIVATE static inline NEEDS[Cpu::modify_actrl]
+void
+Cpu::set_actrl(Mword bit_mask)
+{ modify_actrl(bit_mask, 0); }
+
+PRIVATE static inline NEEDS[Cpu::modify_actrl]
+void
+Cpu::clear_actrl(Mword bit_mask)
+{ modify_actrl(0, bit_mask); }
+
+IMPLEMENT
+void
+Cpu::id_init()
+{
+  __asm__("mrc p15, 0, %0, c0, c1, 0": "=r" (_cpu_id._pfr[0]));
+  __asm__("mrc p15, 0, %0, c0, c1, 1": "=r" (_cpu_id._pfr[1]));
+  __asm__("mrc p15, 0, %0, c0, c1, 2": "=r" (_cpu_id._dfr0));
+  __asm__("mrc p15, 0, %0, c0, c1, 3": "=r" (_cpu_id._afr0));
+  __asm__("mrc p15, 0, %0, c0, c1, 4": "=r" (_cpu_id._mmfr[0]));
+  __asm__("mrc p15, 0, %0, c0, c1, 5": "=r" (_cpu_id._mmfr[1]));
+  __asm__("mrc p15, 0, %0, c0, c1, 6": "=r" (_cpu_id._mmfr[2]));
+  __asm__("mrc p15, 0, %0, c0, c1, 7": "=r" (_cpu_id._mmfr[3]));
+}
+
