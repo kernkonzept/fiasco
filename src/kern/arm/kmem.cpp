d520f1bc11ae66b31d69680f35748e74f9061d99
Alexander Warg <alexander.warg@kernkonzept.com>
2016-08-29 00:00:00 +0000

    ARM: split out 32bit code (kmem.cpp)
    Change-Id: I94a281a26c4c6f37028f270a55206bb559e5cbdf

--- src/kern/arm/kmem.cpp
+++ src/kern/arm/kmem.cpp
@@ -63,29 +63,3 @@ Kmem::mmio_remap(Address phys)
   return phys_to_pmem(phys);
 }
 
-IMPLEMENTATION [!noncont_mem]:
-
-PUBLIC static
-Address
-Kmem::mmio_remap(Address phys)
-{
-  auto m = Kmem_space::kdir()->walk(Virt_addr(phys), Pte_ptr::Super_level);
-  if (m.is_valid())
-    {
-      assert (m.page_order() >= Config::SUPERPAGE_SHIFT);
-      assert (m.page_addr() == cxx::mask_lsb(phys, m.page_order()));
-      assert (m.attribs().type == Page::Type::Uncached());
-      return phys;
-    }
-
-  assert (m.page_order() == Config::SUPERPAGE_SHIFT);
-  Address phys_page = cxx::mask_lsb(phys, Config::SUPERPAGE_SHIFT);
-  m.set_page(m.make_page(Phys_mem_addr(phys_page),
-                         Page::Attr(Page::Rights::RWX(),
-                                    Page::Type::Uncached(),
-                                    Page::Kern::Global())));
-
-  m.write_back_if(true, Mem_unit::Asid_kernel);
-
-  return phys;
-}
