357181dd0147333f12f9b9ca5081ed8e3cdc9ad2
Adam Lackorzynski <adam@l4re.org>
2016-10-03 00:00:00 +0000

    Merge Io_register_block from user-level
    Merge with l4/pkg/drivers-frst/include/io_regblock.h, esp.
    pull in modify variant.
    
    Change-Id: I83f04f5ada1a6ff85c24f40bdf5356a9765d36bb

--- src/lib/uart/io_regblock.h
+++ src/lib/uart/io_regblock.h
@@ -12,20 +12,67 @@ namespace L4
   class Io_register_block
   {
   public:
+    /**
+     * \brief Read register with a byte access.
+     */
     virtual unsigned char  read8(unsigned long reg) const = 0;
+
+    /**
+     * \brief Read register with a 2 byte access.
+     */
     virtual unsigned short read16(unsigned long reg) const = 0;
+
+    /**
+     * \brief Read register with a 4 byte access.
+     */
     virtual unsigned int   read32(unsigned long reg) const = 0;
+
+    /*
+     * \brief Read register with an 8 byte access.
+     */
     //virtual unsigned long long read64(unsigned long reg) const = 0;
 
-    virtual void write8(unsigned long reg, unsigned char val) const = 0;
-    virtual void write16(unsigned long reg, unsigned short val) const = 0;
-    virtual void write32(unsigned long reg, unsigned int val) const = 0;
-    //virtual void write64(unsigned long reg, unsigned long long val) const = 0;
+    /**
+     * \brief Write register with a byte access.
+     */
+    virtual void write8(unsigned long reg, unsigned char value) const = 0;
 
+    /**
+     * \brief Write register with a 2 byte access.
+     */
+    virtual void write16(unsigned long reg, unsigned short value) const = 0;
+
+    /**
+     * \brief Write register with a 4 byte access.
+     */
+    virtual void write32(unsigned long reg, unsigned int value) const = 0;
+
+    /*
+     * \brief Write register with an 8 byte access.
+     */
+    //virtual void write64(unsigned long reg, unsigned long long value) const = 0;
+
+    /**
+     * \brief Get address of a register.
+     */
+    //virtual unsigned long addr(unsigned long reg) const = 0;
+
+    /**
+     * \brief A delay.
+     *
+     * Note, most likely this function does nothing.
+     */
     virtual void delay() const = 0;
 
     virtual ~Io_register_block() = 0;
 
+    /**
+     * \brief Read register.
+     * \param reg   Register to use.
+     * \return Value in the register
+     *
+     * The access width is defined by the return type.
+     */
     template< typename R >
     R read(unsigned long reg) const
     {
@@ -39,18 +86,68 @@ namespace L4
         };
     }
 
+    /**
+     * \brief Write register.
+     * \param reg   Register to use.
+     * \param value Date to write to the register.
+     *
+     * The access width is defined by the type of the value
+     */
     template< typename R >
-    void write(unsigned long reg, R val) const
+    void write(unsigned long reg, R value) const
     {
       switch (sizeof(R))
         {
-        case 1: write8(reg, val); return;
-        case 2: write16(reg, val); return;
-        case 4: write32(reg, val); return;
+        case 1: write8(reg, value); return;
+        case 2: write16(reg, value); return;
+        case 4: write32(reg, value); return;
         default: static_assert(sizeof(R) == 1 || sizeof(R) == 2 || sizeof(R) == 4,
                                "Invalid size");
         };
     }
+
+    /**
+     * \brief Modify register.
+     * \param reg         Register to use.
+     * \param clear_bits  Bits to clear in the register.
+     * \param set_bits    Bits to set in the register.
+     * \return The written value.
+     *
+     * This function is a short form of
+     * write(reg, (read(reg) & ~clear_bits) | add_bits)
+     */
+    template< typename R >
+    R modify(unsigned long reg, R clear_bits, R set_bits) const
+    {
+      R r = (read<R>(reg) & ~clear_bits) | set_bits;
+      write(reg, r);
+      return r;
+    }
+
+    /**
+     * \brief Set bits in register.
+     * \param reg         Register to use.
+     * \param set_bits    Bits to set in the registers.
+     * \return The written value.
+     */
+    template< typename R >
+    R set(unsigned long reg, R set_bits) const
+    {
+      return modify<R>(reg, 0, set_bits);
+    }
+
+    /**
+     * \brief Clear bits in register.
+     * \param reg         Register to use.
+     * \param clear_bits  Bits to clear in the register.
+     * \return The written value.
+     */
+    template< typename R >
+    R clear(unsigned long reg, R clear_bits) const
+    {
+      return modify<R>(reg, clear_bits, 0);
+    }
+
   };
 
   inline Io_register_block::~Io_register_block() {}
