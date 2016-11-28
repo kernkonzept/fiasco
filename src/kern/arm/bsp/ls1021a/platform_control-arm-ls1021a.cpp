fb2184a34a92c733da3bb535f70bce5b2f22ef3e
Adam Lackorzynski <adam@l4re.org>
2016-11-28 00:00:00 +0000

    ARM: Add PSCI functionality
    This implements PSCI functionality for the Platform_control
    class, foremost to provide the CPU_ON functionality to boot
    secondary CPUs, however it shall also provide more
    functionality later, such as suspend etc.
    
    Currently, PSCI is user-selected by adding an appropriate
    Kconfig choice. I think I could be good to have PSCI
    always enabled for PSCI-enabled platforms and detect
    at runtime whether a PSCI firmware is available.
    However, for that we need to catch the smc exception we'd
    need to cause. This would require some more support
    functionality.
    
    Change-Id: I042778d79d81f64bfc4ddec26e77c7ab451df0b5

--- src/kern/arm/bsp/ls1021a/platform_control-arm-ls1021a.cpp
+++ src/kern/arm/bsp/ls1021a/platform_control-arm-ls1021a.cpp
@@ -1,4 +1,4 @@
-IMPLEMENTATION [arm && mp && ls1021a]: // ---------------------------------
+IMPLEMENTATION [arm && mp && ls1021a && !psci]: // ------------------------
 
 #include "ipi.h"
 #include "mem.h"
@@ -14,4 +14,14 @@ Platform_control::boot_ap_cpus(Address phys_tramp_mp_addr)
   devcon.r<32>(DCFG_CCSR_SCRATCHRW1) = __builtin_bswap32(phys_tramp_mp_addr);
   Mem::mp_wmb();
   Ipi::bcast(Ipi::Global_request, Cpu_number::boot_cpu());
+}
+
+IMPLEMENTATION [arm && mp && ls1021a && psci]: // -------------------------
+
+PUBLIC static
+void
+Platform_control::boot_ap_cpus(Address phys_tramp_mp_addr)
+{
+  if (cpu_on(0xf01, phys_tramp_mp_addr))
+    printf("KERNEL: PSCI CPU_ON failed\n");
 }
