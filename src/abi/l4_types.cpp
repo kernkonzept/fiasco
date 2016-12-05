0f729a7f3d0f162c74e6163e23ed669f6c5b7e16
Adam Lackorzynski <adam@l4re.org>
2016-12-05 00:00:00 +0000

    Remove L4_msg_tag::X_cpu flag
    The X_cpu has never been implemented and thus
    it is save to remove the flag as it can be concluded
    that it is not required.
    
    Change-Id: I86413c2050ce3b20629ce9478bdb34d0dd533222

--- src/abi/l4_types.cpp
+++ src/abi/l4_types.cpp
@@ -384,14 +384,9 @@ public:
     Error        = 0x8000,
 
     /**
-     * The IPC operation did cross CPU boundaries.
-     */
-    X_cpu        = 0x4000,
-
-    /**
      * Combination of flags that are not pass through.
      */
-    Rcv_flags    = Error | X_cpu,
+    Rcv_flags    = Error,
   };
 
   /**
