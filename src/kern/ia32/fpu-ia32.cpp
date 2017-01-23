33876aa9b32e519661fd4795087c7fe6c2e9d963
Adam Lackorzynski <adam@l4re.org>
2017-01-23 00:00:00 +0000

    x86: Allow AVX512
    Allow AVX512 to work.
    
    Change-Id: Idca967ffc6f912a0a072c28b898137dc6b74cb14

--- src/kern/ia32/fpu-ia32.cpp
+++ src/kern/ia32/fpu-ia32.cpp
@@ -31,8 +31,8 @@ Fpu::init_xsave(Cpu_number cpu)
 
   Cpu::cpus.cpu(cpu).cpuid(0xd, 0, &eax, &ebx, &ecx, &edx);
 
-  // allow safe features: x87, SSE, and AVX
-  Fpu::fpu.cpu(cpu)._xcr0 = (((Unsigned64)edx << 32) | eax) & 0x07;
+  // allow safe features: x87, SSE, AVX and AVX512
+  Fpu::fpu.cpu(cpu)._xcr0 = (((Unsigned64)edx << 32) | eax) & 0xe7;
 
   // enable xsave features
   Cpu::cpus.cpu(cpu).set_cr4(Cpu::cpus.cpu(cpu).get_cr4() | CR4_OSXSAVE);
