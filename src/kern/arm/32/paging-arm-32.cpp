bf67846e9e67d3fe9dd18ca341602f810246795d
Alexander Warg <alexander.warg@kernkonzept.com>
2017-01-30 00:00:00 +0000

    ARM: rework paging infrastructure
    Rework the ARM paging / page-table infrastructure to enable support for
    different page-table layouts and attributes for kernel and user-level/VM
    mappings.
    
    Change-Id: Ib665a48fc75c8b43972abf809557aa931e4aabc8

--- src/kern/arm/32/paging-arm-32.cpp
+++ src/kern/arm/32/paging-arm-32.cpp
@@ -20,6 +20,34 @@ typedef Ptab::Page_addr_wrap<Page_number, Virt_addr::Shift> Ptab_va_vpn;
 //---------------------------------------------------------------------------
 INTERFACE [arm]:
 
-typedef Pdir_t<Pte_ptr, Ptab_traits_vpn, Ptab_va_vpn> Pdir;
+/** for ARM 32bit we use identical page-table layouts for kernel and user */
 class Kpdir : public Pdir_t<K_pte_ptr, Ptab_traits_vpn, Ptab_va_vpn> {};
+
+//---------------------------------------------------------------------------
+INTERFACE [arm && cpu_virt]:
+
+/** when using stage 2 paging the attributes in the page-tables differ */
+class Pte_ptr : public Pte_ptr_t<Pte_ptr>
+{
+public:
+  enum
+  {
+    Super_level = 1,
+    Max_level   = 2,
+  };
+  Pte_ptr() = default;
+  Pte_ptr(void *p, unsigned char level) : Pte_ptr_t(p, level) {}
+
+  unsigned char page_order() const
+  { return Ptab::Level<Ptab_traits_vpn>::shift(level) + Ptab_traits_vpn::Head::Base_shift; }
+};
+
+typedef Pdir_t<Pte_ptr, Ptab_traits_vpn, Ptab_va_vpn> Pdir;
+
+//---------------------------------------------------------------------------
+INTERFACE [arm && !cpu_virt]:
+
+/** without stage 2 paging kernel and user use the same page table. */
+typedef K_pte_ptr Pte_ptr;
+typedef Pdir_t<Pte_ptr, Ptab_traits_vpn, Ptab_va_vpn> Pdir;
 
