1093fed5e8d82418a3d684284cf4cda3a0f8c0c4
Adam Lackorzynski <adam@l4re.org>
2017-04-24 00:00:00 +0000

    MIPS: Use single asm statements only
    Convert save/restore functions to single asm
    statements to allow clang to produce useful output,
    and to keep instructions in a single asm statements
    to not let the compiler reallocate registers.
    
    Change-Id: I79b1e40bdb11e3e49971858b6e01def66ac7fadc

--- src/kern/mips/fpu-mips.cpp
+++ src/kern/mips/fpu-mips.cpp
@@ -112,8 +112,10 @@ PRIVATE static inline
 void
 Fpu::set_mipsr2_fp64()
 {
-  asm volatile(".set   mips32r2\n");
-  asm volatile(".set   fp=64\n");
+  asm volatile(".macro set_mipsr2_fp64\n"
+               "        .set mips32r2 \n"
+               "        .set fp=64    \n"
+               ".endm                 \n");
 }
 
 // ------------------------------------------------------------------------
@@ -123,8 +125,10 @@ PRIVATE static inline
 void
 Fpu::set_mipsr2_fp64()
 {
-  asm volatile(".set   mips64r2\n");
-  asm volatile(".set   fp=64\n");
+  asm volatile(".macro set_mipsr2_fp64\n"
+               "        .set mips64r2 \n"
+               "        .set fp=64    \n"
+               ".endm                 \n");
 }
 
 // ------------------------------------------------------------------------
@@ -140,7 +144,10 @@ Fpu::mode_64bit()
 PRIVATE static inline
 void
 Fpu::set_mipsr2_fp64()
-{}
+{
+  asm volatile(".macro set_mipsr2_fp64\n"
+               ".endm                 \n");
+}
 
 // ------------------------------------------------------------------------
 IMPLEMENTATION [mips && fpu]:
@@ -187,53 +194,59 @@ Fpu::fpu_save_16even(Fpu_regs *r)
 {
   Mword tmp;
 
-  asm volatile(".set   push\n");
-  asm volatile(".set   hardfloat\n");
-  asm volatile("cfc1   %0,   $31\n" : "=r" (tmp));
-  asm volatile("sw     %1,   %0 \n" : "=m" (r->fcsr) : "r" (tmp));
-  asm volatile("sdc1   $f0,  %0 \n" : "=m" (r->regs[0]));
-  asm volatile("sdc1   $f2,  %0 \n" : "=m" (r->regs[2]));
-  asm volatile("sdc1   $f4,  %0 \n" : "=m" (r->regs[4]));
-  asm volatile("sdc1   $f6,  %0 \n" : "=m" (r->regs[6]));
-  asm volatile("sdc1   $f8,  %0 \n" : "=m" (r->regs[8]));
-  asm volatile("sdc1   $f10, %0 \n" : "=m" (r->regs[10]));
-  asm volatile("sdc1   $f12, %0 \n" : "=m" (r->regs[12]));
-  asm volatile("sdc1   $f14, %0 \n" : "=m" (r->regs[14]));
-  asm volatile("sdc1   $f16, %0 \n" : "=m" (r->regs[16]));
-  asm volatile("sdc1   $f18, %0 \n" : "=m" (r->regs[18]));
-  asm volatile("sdc1   $f20, %0 \n" : "=m" (r->regs[20]));
-  asm volatile("sdc1   $f22, %0 \n" : "=m" (r->regs[22]));
-  asm volatile("sdc1   $f24, %0 \n" : "=m" (r->regs[24]));
-  asm volatile("sdc1   $f26, %0 \n" : "=m" (r->regs[26]));
-  asm volatile("sdc1   $f28, %0 \n" : "=m" (r->regs[28]));
-  asm volatile("sdc1   $f30, %0 \n" : "=m" (r->regs[30]));
-  asm volatile(".set   pop\n");
+  asm volatile(".set   push                   \n"
+               ".set   hardfloat              \n"
+               "cfc1   %[tmp],   $31          \n"
+               "sw     %[tmp],   %[fcsr]      \n"
+               "sdc1   $f0,  (8 *  0)(%[regs])\n"
+               "sdc1   $f2,  (8 *  2)(%[regs])\n"
+               "sdc1   $f4,  (8 *  4)(%[regs])\n"
+               "sdc1   $f6,  (8 *  6)(%[regs])\n"
+               "sdc1   $f8,  (8 *  8)(%[regs])\n"
+               "sdc1   $f10, (8 * 10)(%[regs])\n"
+               "sdc1   $f12, (8 * 12)(%[regs])\n"
+               "sdc1   $f14, (8 * 14)(%[regs])\n"
+               "sdc1   $f16, (8 * 16)(%[regs])\n"
+               "sdc1   $f18, (8 * 18)(%[regs])\n"
+               "sdc1   $f20, (8 * 20)(%[regs])\n"
+               "sdc1   $f22, (8 * 22)(%[regs])\n"
+               "sdc1   $f24, (8 * 24)(%[regs])\n"
+               "sdc1   $f26, (8 * 26)(%[regs])\n"
+               "sdc1   $f28, (8 * 28)(%[regs])\n"
+               "sdc1   $f30, (8 * 30)(%[regs])\n"
+               ".set   pop                    \n"
+               : [fcsr] "=m"  (r->fcsr),
+                 [tmp]  "=&r" (tmp)
+               : [regs] "r"   (r->regs));
 }
 
 PRIVATE static inline
 void
 Fpu::fpu_save_16odd(Fpu_regs *r)
 {
-  asm volatile(".set   push\n");
-  asm volatile(".set   hardfloat\n");
-  Fpu::set_mipsr2_fp64();
-  asm volatile("sdc1   $f1,  %0 \n" : "=m" (r->regs[1]));
-  asm volatile("sdc1   $f3,  %0 \n" : "=m" (r->regs[3]));
-  asm volatile("sdc1   $f5,  %0 \n" : "=m" (r->regs[5]));
-  asm volatile("sdc1   $f7,  %0 \n" : "=m" (r->regs[7]));
-  asm volatile("sdc1   $f9,  %0 \n" : "=m" (r->regs[9]));
-  asm volatile("sdc1   $f11, %0 \n" : "=m" (r->regs[11]));
-  asm volatile("sdc1   $f13, %0 \n" : "=m" (r->regs[13]));
-  asm volatile("sdc1   $f15, %0 \n" : "=m" (r->regs[15]));
-  asm volatile("sdc1   $f17, %0 \n" : "=m" (r->regs[17]));
-  asm volatile("sdc1   $f19, %0 \n" : "=m" (r->regs[19]));
-  asm volatile("sdc1   $f21, %0 \n" : "=m" (r->regs[21]));
-  asm volatile("sdc1   $f23, %0 \n" : "=m" (r->regs[23]));
-  asm volatile("sdc1   $f25, %0 \n" : "=m" (r->regs[25]));
-  asm volatile("sdc1   $f27, %0 \n" : "=m" (r->regs[27]));
-  asm volatile("sdc1   $f29, %0 \n" : "=m" (r->regs[29]));
-  asm volatile("sdc1   $f31, %0 \n" : "=m" (r->regs[31]));
-  asm volatile(".set   pop\n");
+  set_mipsr2_fp64();
+  asm volatile(".set   push                   \n"
+               ".set   hardfloat              \n"
+               "set_mipsr2_fp64               \n"
+               ".purgem set_mipsr2_fp64       \n"
+               "sdc1   $f1,  (8 *  1)(%[regs])\n"
+               "sdc1   $f3,  (8 *  3)(%[regs])\n"
+               "sdc1   $f5,  (8 *  5)(%[regs])\n"
+               "sdc1   $f7,  (8 *  7)(%[regs])\n"
+               "sdc1   $f9,  (8 *  9)(%[regs])\n"
+               "sdc1   $f11, (8 * 11)(%[regs])\n"
+               "sdc1   $f13, (8 * 13)(%[regs])\n"
+               "sdc1   $f15, (8 * 15)(%[regs])\n"
+               "sdc1   $f17, (8 * 17)(%[regs])\n"
+               "sdc1   $f19, (8 * 19)(%[regs])\n"
+               "sdc1   $f21, (8 * 21)(%[regs])\n"
+               "sdc1   $f23, (8 * 23)(%[regs])\n"
+               "sdc1   $f25, (8 * 25)(%[regs])\n"
+               "sdc1   $f27, (8 * 27)(%[regs])\n"
+               "sdc1   $f29, (8 * 29)(%[regs])\n"
+               "sdc1   $f31, (8 * 31)(%[regs])\n"
+               ".set   pop                    \n"
+               :: [regs] "r" (r->regs));
 }
 
 PRIVATE static inline
@@ -242,53 +255,59 @@ Fpu::fpu_restore_16even(Fpu_regs *r)
 {
   Mword tmp;
 
-  asm volatile(".set   push\n");
-  asm volatile(".set   hardfloat\n");
-  asm volatile("ldc1   $f0,  %0 \n" :: "m" (r->regs[0]));
-  asm volatile("ldc1   $f2,  %0 \n" :: "m" (r->regs[2]));
-  asm volatile("ldc1   $f4,  %0 \n" :: "m" (r->regs[4]));
-  asm volatile("ldc1   $f6,  %0 \n" :: "m" (r->regs[6]));
-  asm volatile("ldc1   $f8,  %0 \n" :: "m" (r->regs[8]));
-  asm volatile("ldc1   $f10, %0 \n" :: "m" (r->regs[10]));
-  asm volatile("ldc1   $f12, %0 \n" :: "m" (r->regs[12]));
-  asm volatile("ldc1   $f14, %0 \n" :: "m" (r->regs[14]));
-  asm volatile("ldc1   $f16, %0 \n" :: "m" (r->regs[16]));
-  asm volatile("ldc1   $f18, %0 \n" :: "m" (r->regs[18]));
-  asm volatile("ldc1   $f20, %0 \n" :: "m" (r->regs[20]));
-  asm volatile("ldc1   $f22, %0 \n" :: "m" (r->regs[22]));
-  asm volatile("ldc1   $f24, %0 \n" :: "m" (r->regs[24]));
-  asm volatile("ldc1   $f26, %0 \n" :: "m" (r->regs[26]));
-  asm volatile("ldc1   $f28, %0 \n" :: "m" (r->regs[28]));
-  asm volatile("ldc1   $f30, %0 \n" :: "m" (r->regs[30]));
-  asm volatile("lw     %0,   %1 \n" : "=r" (tmp) : "m" (r->fcsr));
-  asm volatile("ctc1   %0,   $31\n" :: "r" (tmp));
-  asm volatile(".set   pop\n");
+  asm volatile(".set   push                   \n"
+               ".set   hardfloat              \n"
+               "ldc1   $f0,  (8 *  0)(%[regs])\n"
+               "ldc1   $f2,  (8 *  2)(%[regs])\n"
+               "ldc1   $f4,  (8 *  4)(%[regs])\n"
+               "ldc1   $f6,  (8 *  6)(%[regs])\n"
+               "ldc1   $f8,  (8 *  8)(%[regs])\n"
+               "ldc1   $f10, (8 * 10)(%[regs])\n"
+               "ldc1   $f12, (8 * 12)(%[regs])\n"
+               "ldc1   $f14, (8 * 14)(%[regs])\n"
+               "ldc1   $f16, (8 * 16)(%[regs])\n"
+               "ldc1   $f18, (8 * 18)(%[regs])\n"
+               "ldc1   $f20, (8 * 20)(%[regs])\n"
+               "ldc1   $f22, (8 * 22)(%[regs])\n"
+               "ldc1   $f24, (8 * 24)(%[regs])\n"
+               "ldc1   $f26, (8 * 26)(%[regs])\n"
+               "ldc1   $f28, (8 * 28)(%[regs])\n"
+               "ldc1   $f30, (8 * 30)(%[regs])\n"
+               "lw     %[tmp], %[fcsr]        \n"
+               "ctc1   %[tmp], $31            \n"
+               ".set   pop                    \n"
+               : [tmp]  "=&r" (tmp)
+               : [regs] "r"   (r->regs),
+                 [fcsr] "m"   (r->fcsr));
 }
 
 PRIVATE static inline
 void
 Fpu::fpu_restore_16odd(Fpu_regs *r)
 {
-  asm volatile(".set   push\n");
-  asm volatile(".set   hardfloat\n");
-  Fpu::set_mipsr2_fp64();
-  asm volatile("ldc1   $f1,  %0 \n" :: "m" (r->regs[1]));
-  asm volatile("ldc1   $f3,  %0 \n" :: "m" (r->regs[3]));
-  asm volatile("ldc1   $f5,  %0 \n" :: "m" (r->regs[5]));
-  asm volatile("ldc1   $f7,  %0 \n" :: "m" (r->regs[7]));
-  asm volatile("ldc1   $f9,  %0 \n" :: "m" (r->regs[9]));
-  asm volatile("ldc1   $f11, %0 \n" :: "m" (r->regs[11]));
-  asm volatile("ldc1   $f13, %0 \n" :: "m" (r->regs[13]));
-  asm volatile("ldc1   $f15, %0 \n" :: "m" (r->regs[15]));
-  asm volatile("ldc1   $f17, %0 \n" :: "m" (r->regs[17]));
-  asm volatile("ldc1   $f19, %0 \n" :: "m" (r->regs[19]));
-  asm volatile("ldc1   $f21, %0 \n" :: "m" (r->regs[21]));
-  asm volatile("ldc1   $f23, %0 \n" :: "m" (r->regs[23]));
-  asm volatile("ldc1   $f25, %0 \n" :: "m" (r->regs[25]));
-  asm volatile("ldc1   $f27, %0 \n" :: "m" (r->regs[27]));
-  asm volatile("ldc1   $f29, %0 \n" :: "m" (r->regs[29]));
-  asm volatile("ldc1   $f31, %0 \n" :: "m" (r->regs[31]));
-  asm volatile(".set   pop\n");
+  set_mipsr2_fp64();
+  asm volatile(".set   push                   \n"
+               ".set   hardfloat              \n"
+               "set_mipsr2_fp64               \n"
+               ".purgem set_mipsr2_fp64       \n"
+               "ldc1   $f1,  (8 *  1)(%[regs])\n"
+               "ldc1   $f3,  (8 *  3)(%[regs])\n"
+               "ldc1   $f5,  (8 *  5)(%[regs])\n"
+               "ldc1   $f7,  (8 *  7)(%[regs])\n"
+               "ldc1   $f9,  (8 *  9)(%[regs])\n"
+               "ldc1   $f11, (8 * 11)(%[regs])\n"
+               "ldc1   $f13, (8 * 13)(%[regs])\n"
+               "ldc1   $f15, (8 * 15)(%[regs])\n"
+               "ldc1   $f17, (8 * 17)(%[regs])\n"
+               "ldc1   $f19, (8 * 19)(%[regs])\n"
+               "ldc1   $f21, (8 * 21)(%[regs])\n"
+               "ldc1   $f23, (8 * 23)(%[regs])\n"
+               "ldc1   $f25, (8 * 25)(%[regs])\n"
+               "ldc1   $f27, (8 * 27)(%[regs])\n"
+               "ldc1   $f29, (8 * 29)(%[regs])\n"
+               "ldc1   $f31, (8 * 31)(%[regs])\n"
+               ".set   pop                    \n"
+               :: [regs] "r" (r->regs));
 }
 
 IMPLEMENT
