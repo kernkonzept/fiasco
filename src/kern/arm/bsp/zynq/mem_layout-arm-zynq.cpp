018247b5b4ef1459ff460cbad517b49cbfaed956
Adam Lackorzynski <adam@l4re.org>
2017-06-12 00:00:00 +0000

    ARM: Zynq: Convert to use koptions
    Change-Id: I3c41a87d6f48f40582aeb6400fc2475af2183ddd

--- src/kern/arm/bsp/zynq/mem_layout-arm-zynq.cpp
+++ src/kern/arm/bsp/zynq/mem_layout-arm-zynq.cpp
@@ -4,7 +4,6 @@ EXTENSION class Mem_layout
 {
 public:
   enum Phys_layout_zynq : Address {
-    Uart_phys_base       = 0xe0000000 + 0x1000 * CONFIG_PF_ZYNQ_UART_NR,
     Mp_scu_phys_base     = 0xf8f00000,
     Gic_cpu_phys_base    = 0xf8f00100,
     Gic_dist_phys_base   = 0xf8f01000,
--- src/kern/arm/bsp/zynq/uart-arm-zynq.cpp
+++ src/kern/arm/bsp/zynq/uart-arm-zynq.cpp
@@ -1,19 +1,13 @@
 IMPLEMENTATION [pf_zynq]:
 
+#include "koptions.h"
 #include "uart_cadence.h"
-#include "mem_layout.h"
 
-IMPLEMENT Address Uart::base() const { return Mem_layout::Uart_phys_base; }
+IMPLEMENT Address Uart::base() const
+{ return Koptions::o()->uart.base_address; }
 
 IMPLEMENT int Uart::irq() const
-{
-  switch (CONFIG_PF_ZYNQ_UART_NR)
-    {
-    case 0: return 59;
-    default:
-    case 1: return 82;
-    };
-}
+{ return Koptions::o()->uart.irqno; }
 
 IMPLEMENT L4::Uart *Uart::uart()
 {
