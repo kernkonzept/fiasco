5570b32aff2195b13fd31068db305109683bcd59
Adam Lackorzynski <adam@l4re.org>
2017-06-12 00:00:00 +0000

    arm: rcar3: Remove Uart_phys_base
    We do not use this enum in the kernel, thus
    remove.
    
    Change-Id: I55cdcc0d5cd2ea8c512c9ef6c73e35fec6a7e4cb

--- src/kern/arm/bsp/rcar3/mem_layout-arm-rcar3.cpp
+++ src/kern/arm/bsp/rcar3/mem_layout-arm-rcar3.cpp
@@ -4,7 +4,6 @@ EXTENSION class Mem_layout
 {
 public:
   enum Phys_layout_rcar3 : Address {
-    Uart_phys_base       = 0xe6e88000,
     Gic_cpu_phys_base    = 0xf1020000,
     Gic_dist_phys_base   = 0xf1010000,
     Gic_h_phys_base      = 0xf1040000,
