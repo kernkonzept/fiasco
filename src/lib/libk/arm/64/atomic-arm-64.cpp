832767d4746fee24b67f174b5c58973883a3c1b8
Adam Lackorzynski <adam@l4re.org>
2017-06-26 00:00:00 +0000

    arm64: simplify atomic operations
    Change-Id: I0f6c1547804636a93174616d916a0dd475353984

--- src/lib/libk/arm/64/atomic-arm-64.cpp
+++ src/lib/libk/arm/64/atomic-arm-64.cpp
@@ -7,13 +7,13 @@ atomic_mp_add(Mword *l, Mword value)
   Mword tmp, ret;
 
   asm volatile (
-      "1:                                 \n"
-      "ldxr    %[v], [%[mem]]             \n"
-      "add     %[v], %[v], %[addval]      \n"
-      "stxr    %w[ret], %[v], [%[mem]]    \n"
-      "cbnz    %w[ret], 1b                \n"
-      : [v] "=&r" (tmp), [ret] "=&r" (ret), "+m" (*l)
-      :  [mem] "r" (l), [addval] "r" (value)
+      "1:                             \n"
+      "ldxr    %[v], %[l]             \n"
+      "add     %[v], %[v], %[addval]  \n"
+      "stxr    %w[ret], %[v], %[l]    \n"
+      "cbnz    %w[ret], 1b            \n"
+      : [v] "=&r" (tmp), [ret] "=&r" (ret), [l] "+Q" (*l)
+      : [addval] "r" (value)
       : "cc");
 }
 
@@ -24,13 +24,13 @@ atomic_mp_and(Mword *l, Mword value)
   Mword tmp, ret;
 
   asm volatile (
-      "1:                                 \n"
-      "ldxr    %[v], [%[mem]]             \n"
-      "and     %[v], %[v], %[andval]      \n"
-      "stxr    %w[ret], %[v], [%[mem]]    \n"
-      "cbnz    %w[ret], 1b                \n"
-      : [v] "=&r" (tmp), [ret] "=&r" (ret), "+m" (*l)
-      :  [mem] "r" (l), [andval] "r" (value)
+      "1:                             \n"
+      "ldxr    %[v], %[l]             \n"
+      "and     %[v], %[v], %[andval]  \n"
+      "stxr    %w[ret], %[v], %[l]    \n"
+      "cbnz    %w[ret], 1b            \n"
+      : [v] "=&r" (tmp), [ret] "=&r" (ret), [l] "+Q" (*l)
+      : [andval] "r" (value)
       : "cc");
 }
 
@@ -41,13 +41,13 @@ atomic_mp_or(Mword *l, Mword value)
   Mword tmp, ret;
 
   asm volatile (
-      "1:                                 \n"
-      "ldxr    %[v], [%[mem]]             \n"
-      "orr     %[v], %[v], %[orval]       \n"
-      "stxr    %w[ret], %[v], [%[mem]]    \n"
-      "cbnz    %w[ret], 1b                \n"
-      : [v] "=&r" (tmp), [ret] "=&r" (ret), "+m" (*l)
-      :  [mem] "r" (l), [orval] "r" (value)
+      "1:                             \n"
+      "ldxr    %[v], %[l]             \n"
+      "orr     %[v], %[v], %[orval]   \n"
+      "stxr    %w[ret], %[v], %[l]    \n"
+      "cbnz    %w[ret], 1b            \n"
+      : [v] "=&r" (tmp), [ret] "=&r" (ret), [l] "+Q" (*l)
+      : [orval] "r" (value)
       : "cc");
 }
 
@@ -58,20 +58,18 @@ mp_cas_arch(Mword *m, Mword o, Mword n)
   Mword tmp, res;
 
   asm volatile
-    ("mov     %[res], #1           \n"
-     "1:                           \n"
-     "ldr     %[tmp], [%[m]]       \n"
-     "cmp     %[tmp], %[o]         \n"
-     "b.ne    2f                   \n"
-     "ldxr    %[tmp], [%[m]]       \n"
-     "cmp     %[tmp], %[o]         \n"
-     "b.ne    2f                   \n"
-     "stxr    %w[res], %[n], [%[m]]\n"
-     "cbnz    %w[res], 1b          \n"
+    ("mov     %[res], #1          \n"
+     "prfm    pstl1strm, %[m]     \n"
+     "1:                          \n"
+     "ldxr    %[tmp], %[m]        \n"
+     "cmp     %[tmp], %[o]        \n"
+     "b.ne    2f                  \n"
+     "stxr    %w[res], %[n], %[m] \n"
+     "cbnz    %w[res], 1b         \n"
      "dmb     ish                 \n"
-     "2:                           \n"
-     : [tmp] "=&r" (tmp), [res] "=&r" (res), "+m" (*m)
-     : [n] "r" (n), [m] "r" (m), [o] "r" (o)
+     "2:                          \n"
+     : [tmp] "=&r" (tmp), [res] "=&r" (res), [m] "+Q" (*m)
+     : [n] "r" (n), [o] "r" (o)
      : "cc", "memory");
 
   // res == 0 is ok
