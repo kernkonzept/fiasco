63d48f2fed8d220d6778fbe660f614aa986b2fff
Jean Wolter <jean.wolter@kernkonzept.com>
2017-06-26 00:00:00 +0000

    ARM: ls1021a: Provide Gic::pcpu_to_sgi()
    ls1021a comes with 2 cpus in a single cluster. It specifies the
    affinity values in MPIDR as 0xf00 and 0xf01 (aff2 = 0xf,
    aff0 = <cpu number>). Therefore we need to provide an implementation
    for Gic::pcpu_to_sgi() which simply extracts aff0 as cpu number.
    
    Change-Id: If6f4063afc7ff6199038bf952ac1925150c676dd

---
+++ src/kern/arm/bsp/ls1021a/gic-arm-ls1021a.cpp
@@ -0,0 +1,9 @@
+IMPLEMENTATION [arm && pic_gic && pf_ls1021a]:
+
+IMPLEMENT_OVERRIDE inline
+Unsigned32 Gic::pcpu_to_sgi(Cpu_phys_id cpu)
+{
+  // mpidr is set to (aff2, aff1, aff0) = (0xf, 0, <cpu number>), we
+  // extract the lower 4 bits as sgi target
+  return cxx::int_value<Cpu_phys_id>(cpu) & 0xf;
+}
