865de7a8e44c5943d4bb1ed3ce5873dfa5c56c0a
Alexander Warg <alexander.warg@kernkonzept.com>
2017-08-28 00:00:00 +0000

    ARM: Add store memory barrier and use dmb/isb for v6
    Use dmb, dsb, and isb mnemonics for ARMv6 as described in the
    ARM reference manuals.
    
    We used a full memory berrier in all cases this change adds a store
    memory barrier that provides ordering for stores only.
    
    Change-Id: I11533507427a65d48e865dc2b91b8ed04d001d23

--- src/drivers/arm/mem-arm.cpp
+++ src/drivers/arm/mem-arm.cpp
@@ -37,6 +37,8 @@ IMPLEMENT_DEFAULT inline void Mem::prefetch_w(void *) {}
 //-----------------------------------------------------------------------------
 IMPLEMENTATION [arm_v5]:
 
+PUBLIC static inline void Mem::dmbst() { barrier(); }
+
 PUBLIC static inline void Mem::dmb() { barrier(); }
 
 PUBLIC static inline void Mem::isb() { barrier(); }
@@ -47,14 +49,17 @@ PUBLIC static inline void Mem::dsb()
 //-----------------------------------------------------------------------------
 IMPLEMENTATION [arm_v6]:
 
+PUBLIC static inline void Mem::dmbst()
+{ __asm__ __volatile__ ("dmb" : : : "memory"); }
+
 PUBLIC static inline void Mem::dmb()
-{ __asm__ __volatile__ ("mcr p15, 0, %0, c7, c10, 5" : : "r" (0) : "memory"); }
+{ __asm__ __volatile__ ("dmb" : : : "memory"); }
 
 PUBLIC static inline void Mem::isb()
-{ __asm__ __volatile__ ("mcr p15, 0, %0, c7, c5, 4" : : "r" (0) : "memory"); }
+{ __asm__ __volatile__ ("isb" : : : "memory"); }
 
 PUBLIC static inline void Mem::dsb()
-{ __asm__ __volatile__ ("mcr p15, 0, %0, c7, c10, 4" : : "r" (0) : "memory"); }
+{ __asm__ __volatile__ ("dsb" : : : "memory"); }
 
 //-----------------------------------------------------------------------------
 IMPLEMENTATION [32bit && ((arm_v7 && mp) || arm_v8)]:
@@ -65,6 +70,9 @@ Mem::prefetch_w(void *addr)
 
 //-----------------------------------------------------------------------------
 IMPLEMENTATION [arm_v7 || arm_v8]:
+
+PUBLIC static inline void Mem::dmbst()
+{ __asm__ __volatile__ ("dmb ishst" : : : "memory"); }
 
 PUBLIC static inline void Mem::dmb()
 { __asm__ __volatile__ ("dmb ish" : : : "memory"); }
@@ -80,11 +88,11 @@ IMPLEMENTATION [arm && mp]:
 
 IMPLEMENT inline static void Mem::mb() { dmb(); }
 IMPLEMENT inline static void Mem::rmb() { dmb(); }
-IMPLEMENT inline static void Mem::wmb() { dmb(); }
+IMPLEMENT inline static void Mem::wmb() { dmbst(); }
 
 IMPLEMENT inline static void Mem::mp_mb() { dmb(); }
 IMPLEMENT inline static void Mem::mp_acquire() { dmb(); }
 IMPLEMENT inline static void Mem::mp_release() { dmb(); }
 IMPLEMENT inline static void Mem::mp_rmb() { dmb(); }
-IMPLEMENT inline static void Mem::mp_wmb() { dmb(); }
+IMPLEMENT inline static void Mem::mp_wmb() { dmbst(); }
 
