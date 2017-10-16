e934f27f22a0019ac3ae26878eb8091a3cf1115b
Adam Lackorzynski <adam@l4re.org>
2017-10-16 00:00:00 +0000

    ARM: bcm283x: reset: Don't do 64bit accesses
    This fixes ticket #605.
    
    Change-Id: I48638a617f57725afe516a195f1e94fbfc30d0e3

--- src/kern/arm/bsp/bcm283x/reset-arm-bcm283x.cpp
+++ src/kern/arm/bsp/bcm283x/reset-arm-bcm283x.cpp
@@ -13,9 +13,10 @@ platform_reset(void)
   Address base = Kmem::mmio_remap(Mem_layout::Watchdog_phys_base);
 
   Mword pw = 0x5a << 24;
-  Io::write<Mword>(pw | 8, base + Wdog);
-  Io::write<Mword>((Io::read<Mword>(base + Rstc) & ~0x30) | pw | 0x20,
-                   base + Rstc);
+  Io::write<Unsigned32>(pw | 8, base + Wdog);
+  Io::write<Unsigned32>((Io::read<Unsigned32>(base + Rstc) & ~0x30)
+                        | pw | 0x20,
+                        base + Rstc);
 
   for (;;)
     ;
