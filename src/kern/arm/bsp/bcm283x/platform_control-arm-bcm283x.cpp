debbe3a7d3ed0f85cb578b719829370cc7664b98
Adam Lackorzynski <adam@l4re.org>
2017-10-16 00:00:00 +0000

    ARM: bcm283x: Support 64bit boot-up variant
    When running in 64bit mode, the CPU boot-up
    method differs from the 32bit variant.
    
    Fixes #604
    
    Change-Id: I79328ac30208ae1231bf1426dc11d9b8fb0cd9d1

--- src/kern/arm/bsp/bcm283x/platform_control-arm-bcm283x.cpp
+++ src/kern/arm/bsp/bcm283x/platform_control-arm-bcm283x.cpp
@@ -1,4 +1,4 @@
-IMPLEMENTATION [arm && mp && pf_bcm283x]: // ------------------------------
+IMPLEMENTATION [arm && mp && (pf_bcm283x_rpi2 || pf_bcm283x_rpi3) && !64bit]:
 
 #include "arm_control.h"
 #include "cpu.h"
@@ -10,5 +10,27 @@ Platform_control::boot_ap_cpus(Address phys_tramp_mp_addr)
   Cpu_phys_id myid = Proc::cpu_id();
   for (unsigned i = 0; i < 4; ++i)
     if (myid != Cpu_phys_id(i))
-      Arm_control::boot_cpu(Cpu_phys_id(i), phys_tramp_mp_addr);
+      Arm_control::o()->do_boot_cpu(Cpu_phys_id(i), phys_tramp_mp_addr);
+}
+
+// ------------------------------------------------------------------------
+IMPLEMENTATION [arm && mp && pf_bcm283x_rpi3 && 64bit]:
+
+#include "cpu.h"
+#include "mmio_register_block.h"
+
+PUBLIC static
+void
+Platform_control::boot_ap_cpus(Address phys_tramp_mp_addr)
+{
+  static Mmio_register_block a(Kmem::mmio_remap(0xd8));
+  Cpu_phys_id myid = Proc::cpu_id();
+  for (unsigned i = 0; i < 4; ++i)
+    if (myid != Cpu_phys_id(i))
+      {
+        Mem_unit::clean_dcache();
+        a.r<64>(i * 8) = phys_tramp_mp_addr;
+        Mem::dsb();
+        asm volatile("sev");
+      }
 }
