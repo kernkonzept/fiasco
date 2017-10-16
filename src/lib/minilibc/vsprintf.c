e02cc395ea5dbe66ee009a3cc8234c9a79fb6592
Adam Lackorzynski <adam@l4re.org>
2017-10-16 00:00:00 +0000

    libc: Silence gcc8 warning on format truncation
    Silence gcc8's warning on format trucation
    using GCC pragmas. This is an libc-internal
    implementation detail as sprintf is
    implemented using snprintf.
    
    Change-Id: Iea30f3c757813b50bef6be708cfd5d7aface5186

--- src/lib/minilibc/vsprintf.c
+++ src/lib/minilibc/vsprintf.c
@@ -5,5 +5,8 @@
 
 int vsprintf(char *dest,const char *format, va_list arg_ptr)
 {
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wformat-truncation"
   return vsnprintf(dest,(size_t)-1,format,arg_ptr);
+#pragma GCC diagnostic pop
 }
