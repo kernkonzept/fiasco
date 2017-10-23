3db065eb8896cb293367c402e51e8704e4837f85
Adam Lackorzynski <adam@l4re.org>
2017-10-23 00:00:00 +0000

    libc: Avoid warning introduced by pragmas
    Older GCCs warn about the pragma they don't
    understand. Consider this.
    
    Change-Id: Ib89b8fff235ff20cc56ee6fe2197fd98a360e113

--- src/lib/minilibc/vsprintf.c
+++ src/lib/minilibc/vsprintf.c
@@ -5,8 +5,12 @@
 
 int vsprintf(char *dest,const char *format, va_list arg_ptr)
 {
+#if __GNUC__ >= 7
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wformat-truncation"
+#endif
   return vsnprintf(dest,(size_t)-1,format,arg_ptr);
+#if __GNUC__ >= 7
 #pragma GCC diagnostic pop
+#endif
 }
