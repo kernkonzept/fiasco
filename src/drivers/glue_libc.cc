3505fa871f5d23583b51a8b38a118029999d70f6
Adam Lackorzynski <adam@l4re.org>
2017-11-06 00:00:00 +0000

    glue_libc: Fix indentation glitch
    Change-Id: I47a745c3f75a2c936779a4b401f53228fbdd3d1b

--- src/drivers/glue_libc.cc
+++ src/drivers/glue_libc.cc
@@ -6,27 +6,27 @@ int __libc_backend_outs(const char *s, size_t len)
   if (!Console::stdout)
     return len;
 
-   char const *e = s + len;
-   char const *p = s;
-   while (s < e)
-     {
-       for (; p < e && *p != '\n'; ++p)
-         ;
+  char const *e = s + len;
+  char const *p = s;
+  while (s < e)
+    {
+      for (; p < e && *p != '\n'; ++p)
+        ;
 
-       while (s < p)
-         {
-           int written = Console::stdout->write(s, p - s);
-           if (written < 0)
-             return written;
-           s += written;
-         }
+      while (s < p)
+        {
+          int written = Console::stdout->write(s, p - s);
+          if (written < 0)
+            return written;
+          s += written;
+        }
 
-       if (p < e && *p == '\n')
-         {
-           Console::stdout->write("\r", 1);
-           ++p;
-         }
-     }
+      if (p < e && *p == '\n')
+        {
+          Console::stdout->write("\r", 1);
+          ++p;
+        }
+    }
 
   return len;
 }
