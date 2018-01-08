2748e30bcd6fab2bd6428075323f769fe9f21614
Adam Lackorzynski <adam@l4re.org>
2018-01-08 00:00:00 +0000

    ARM64: Check for existings mapping in mmio_remap
    Do not create a new mapping if already one exists
    that covers the requested physical address.
    
    Change-Id: Ic4dfcd6c2c6a30d6ebcd0018fb3f3413e1732bd3

--- src/kern/arm/64/kmem-arm-64.cpp
+++ src/kern/arm/64/kmem-arm-64.cpp
@@ -5,6 +5,18 @@ Address
 Kmem::mmio_remap(Address phys)
 {
   static Address ndev = 0;
+  Address phys_page = cxx::mask_lsb(phys, Config::SUPERPAGE_SHIFT);
+
+  for (Address a = Mem_layout::Registers_map_start;
+       a < Mem_layout::Registers_map_end;
+       a += Config::SUPERPAGE_SIZE)
+    {
+      auto e = kdir->walk(Virt_addr(a), K_pte_ptr::Super_level);
+      if (e.is_valid() && phys_page == e.page_addr())
+        return (phys & ~Config::SUPERPAGE_MASK) | (a & Config::SUPERPAGE_MASK);
+    }
+
+
   Address dm = Mem_layout::Registers_map_start + ndev;
   assert(dm < Mem_layout::Registers_map_end);
 
@@ -13,7 +25,6 @@ Kmem::mmio_remap(Address phys)
   auto m = kdir->walk(Virt_addr(dm), K_pte_ptr::Super_level);
   assert (!m.is_valid());
   assert (m.page_order() == Config::SUPERPAGE_SHIFT);
-  Address phys_page = cxx::mask_lsb(phys, Config::SUPERPAGE_SHIFT);
   m.set_page(m.make_page(Phys_mem_addr(phys_page),
                          Page::Attr(Page::Rights::RW(),
                                     Page::Type::Uncached(),
