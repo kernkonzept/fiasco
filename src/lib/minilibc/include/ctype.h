44c958bfe16b68cbe9d9d1895ee32ed489fb6291
Adam Lackorzynski <adam@l4re.org>
2018-01-29 00:00:00 +0000

    libc: Remove attribute((const)) for isXXX funcs
    gcc8 warns that it ignores the statement:
    src/lib/minilibc/include/ctype.h:28:1: warning: ignoring
    attribute ‘const’ in declaration of a built-in function ‘isgraph’
    because it conflicts with attribute ‘pure’ [-Wattributes]
    
    Change-Id: Ie325dc8d88e8174a1e9dfc009913abe33f2ff5a0

--- src/lib/minilibc/include/ctype.h
+++ src/lib/minilibc/include/ctype.h
@@ -5,27 +5,27 @@
 
 __BEGIN_DECLS
 
-int isascii (int c) __attribute__ ((__const__));
-int isblank (int c) __attribute__ ((__const__));
-int isalnum (int c) __attribute__ ((__const__));
-int isalpha (int c) __attribute__ ((__const__));
-int isdigit (int c) __attribute__ ((__const__));
-int isspace (int c) __attribute__ ((__const__));
+int isascii (int c);
+int isblank (int c);
+int isalnum (int c);
+int isalpha (int c);
+int isdigit (int c);
+int isspace (int c);
 
-int isupper (int c) __attribute__ ((__const__));
-int islower (int c) __attribute__ ((__const__));
+int isupper (int c);
+int islower (int c);
 
-int tolower(int c) __attribute__ ((__const__));
-int toupper(int c) __attribute__ ((__const__));
+int tolower(int c);
+int toupper(int c);
 
-int isprint(int c) __attribute__ ((__const__));
-int ispunct(int c) __attribute__ ((__const__));
-int iscntrl(int c) __attribute__ ((__const__));
+int isprint(int c);
+int ispunct(int c);
+int iscntrl(int c);
 
 /* fscking GNU extensions! */
-int isxdigit(int c) __attribute__ ((__const__));
+int isxdigit(int c);
 
-int isgraph(int c) __attribute__ ((__const__));
+int isgraph(int c);
 
 __END_DECLS
 
