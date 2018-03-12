f23daaf09202602285556bed6820570f327e6dc4
Frank Mehnert <frank.mehnert@kernkonzept.com>
2018-03-12 00:00:00 +0000

    don't cast __libc_backend_outs() to output_func
    Fixes the following gcc-8 warning:
    
      warning: cast between incompatible function types from
      ‘int (*)(const char *, size_t)’
          {aka ‘int (*)(const char *, long unsigned int)’} to
      ‘int (*)(const char *, size_t,  void *)’
          {aka ‘int (*)(const char *, long unsigned int,  void *)’}
      [-Wcast-function-type]
    
    It is true that additional parameters are ignored by a function and the
    resulting code in fact does nothing else than just branching to the real
    function. Nevertheless gcc-8 is unhappy about the original code. Maybe
    it's too pedantic but maybe such warnings prevent users from accidents.
    
    Change-Id: I676b3f8cdf5a7a5ee7236fab3d95a8018256b682

--- src/lib/minilibc/vprintf.c
+++ src/lib/minilibc/vprintf.c
@@ -4,9 +4,15 @@
 #include "vprintf_backend.h"
 #include "libc_backend.h"
 
+static int __libc_backend_outs_wrapper(char const *s, size_t len, void *ignore)
+{
+  (void)ignore;
+  return __libc_backend_outs(s, len);
+}
+
 int vprintf(const char *format, va_list ap)
 {
-  struct output_op _ap = { 0, (output_func*) &__libc_backend_outs };
+  struct output_op _ap = { 0, &__libc_backend_outs_wrapper };
   unsigned long state = __libc_backend_printf_lock();
   int ret = __v_printf(&_ap,format,ap);
   __libc_backend_printf_unlock(state);
