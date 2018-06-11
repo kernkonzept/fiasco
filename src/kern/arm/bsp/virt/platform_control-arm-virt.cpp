3fbad1e2223dfe87db61b2d0e7bf5b408fcf687a
Sarah Hoffmann <sarah.hoffmann@kernkonzept.com>
2018-06-11 00:00:00 +0000

    arm-virt: add dummy implementation for boot_ap_cpus()
    Fixes compiling for arm-virt platform when SMP is enabled.
    
    Fixes #898.
    
    Change-Id: I6ca9b9abaddd6f96ca9306093b9d10ad29d0e9f1

---
+++ src/kern/arm/bsp/virt/platform_control-arm-virt.cpp
@@ -0,0 +1,10 @@
+IMPLEMENTATION [arm && mp && pf_arm_virt]:
+
+#include <cstdio>
+
+PUBLIC static
+void
+Platform_control::boot_ap_cpus(Address)
+{
+  printf("Platform_control::boot_ap_cpus: not implemented.\n");
+}
