cc43a2c9f36df0d5cda73d5d55bb9c147fcc75ef
Marius Melzer <marius.melzer@kernkonzept.com>
2018-06-25 00:00:00 +0000

    Fix warning bad-function-cast
    The cast to Address is unnecessary since printf automatically casts the
    returned void pointer to the given L4_PTR_FMT format.
    
    Change-Id: I3bc88f44e27bf17163d1e3596d687554b847bed3

--- src/lib/minilibc/panic.c
+++ src/lib/minilibc/panic.c
@@ -12,7 +12,7 @@ __assert_fail(const char *__assertion, const char *__file,
   printf("\nAssertion failed: '%s' [ret=%p]\n"
          "  %s:%u at " L4_PTR_FMT "\n",
          __assertion, ret, __file, __line,
-          (Address)__builtin_return_address(0));
+          __builtin_return_address(0));
 
   exit(1);
 }
