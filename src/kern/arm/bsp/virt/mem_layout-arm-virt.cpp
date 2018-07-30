f21e080297160208d26af54d6dcaee4e8dd3d9a5
Adam Lackorzynski <adam@l4re.org>
2018-07-30 00:00:00 +0000

    ARM: Fix Gic_h_phys_base of arm_virt platform
    Use the proper address.
    
    Change-Id: I7d86ff17e2b48e5d6969554ec5cbfbcb941095eb

--- src/kern/arm/bsp/virt/mem_layout-arm-virt.cpp
+++ src/kern/arm/bsp/virt/mem_layout-arm-virt.cpp
@@ -6,7 +6,6 @@ public:
   enum Phys_layout_virt : Address {
     Gic_dist_phys_base   = 0x08000000,
     Gic_cpu_phys_base    = 0x08010000,
-    Gic_h_phys_base      = 0x08200000,
+    Gic_h_phys_base      = 0x08030000,
   };
 };
-
