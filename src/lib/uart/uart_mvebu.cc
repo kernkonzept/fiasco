0d4736c0b4fd7832ddd684b8ca5600668cf5e893
Adam Lackorzynski <adam@l4re.org>
2018-08-27 00:00:00 +0000

    Uart: mvebu uart
    This change adds support for a UART used in some of the Marvell EBU SoCs
    like Armada-3700.
    
    Change-Id: Ic98d862346269df83dadc4ffe72c8cd922f53c7e

---
+++ src/lib/uart/uart_mvebu.cc
@@ -0,0 +1,91 @@
+/*
+ * (c) 2017 Adam Lackorzynski <adam@l4re.org>
+ *
+ * This file is part of L4Re and distributed under the terms of the
+ * GNU General Public License 2.
+ * Please see the COPYING-GPL-2 file for details.
+ */
+#include "uart_mvebu.h"
+#include "poll_timeout_counter.h"
+
+namespace L4
+{
+  enum
+  {
+    // use names from manual
+    RBR  = 0x00,
+    TSH  = 0x04,
+    CTRL = 0x08,
+    STAT = 0x0c,
+    BRDV = 0x10,
+
+    STAT_RX_RDY       = 0x0010,
+    STAT_TXFIFO_FULL  = 0x0800,
+
+    CTRL_RX_RDY_INT_EN = 1 <<  4,
+    CTRL_RXFIFO_RST    = 1 << 14,
+    CTRL_TXFIFO_RST    = 1 << 15,
+  };
+
+  bool Uart_mvebu::startup(Io_register_block const *regs)
+  {
+    _regs = regs;
+
+    regs->write<unsigned>(CTRL, CTRL_RXFIFO_RST | CTRL_TXFIFO_RST);
+    regs->write<unsigned>(CTRL, 0);
+
+    return true;
+  }
+
+  bool Uart_mvebu::enable_rx_irq(bool enable)
+  {
+    if (enable)
+      _regs->set<unsigned>(CTRL, CTRL_RX_RDY_INT_EN);
+    else
+      _regs->clear<unsigned>(CTRL, CTRL_RX_RDY_INT_EN);
+    return true;
+  }
+
+  void Uart_mvebu::shutdown()
+  {
+    _regs->write<unsigned>(CTRL, 0);
+  }
+
+  bool Uart_mvebu::change_mode(Transfer_mode, Baud_rate r)
+  {
+    _regs->write<unsigned>(BRDV, _baserate / r / 16);
+    return true;
+  }
+
+  int Uart_mvebu::get_char(bool blocking) const
+  {
+    while (!char_avail())
+      if (!blocking)
+        return -1;
+
+    return _regs->read<unsigned>(RBR) & 0xff;
+  }
+
+  int Uart_mvebu::char_avail() const
+  {
+    return _regs->read<unsigned>(STAT) & STAT_RX_RDY;
+  }
+
+  void Uart_mvebu::out_char(char c) const
+  {
+    Poll_timeout_counter i(3000000);
+    while (i.test(_regs->read<unsigned>(STAT) & STAT_TXFIFO_FULL))
+      ;
+
+    _regs->write<unsigned>(TSH, c);
+  }
+
+  int Uart_mvebu::write(char const *s, unsigned long count) const
+  {
+    unsigned long c = count;
+    while (c--)
+      out_char(*s++);
+
+    return count;
+  }
+}
---
+++ src/lib/uart/uart_mvebu.h
@@ -0,0 +1,29 @@
+/*
+ * (c) 2017 Adam Lackorzynski <adam@l4re.org>
+ *
+ * This file is part of L4Re and distributed under the terms of the
+ * GNU General Public License 2.
+ * Please see the COPYING-GPL-2 file for details.
+ */
+#pragma once
+
+#include "uart_base.h"
+
+namespace L4
+{
+  class Uart_mvebu : public Uart
+  {
+  public:
+    explicit Uart_mvebu(unsigned baserate) : _baserate(baserate) {}
+    bool startup(Io_register_block const *) override;
+    void shutdown() override;
+    bool enable_rx_irq(bool enable = true) override;
+    bool change_mode(Transfer_mode m, Baud_rate r) override;
+    int get_char(bool blocking = true) const override;
+    int char_avail() const override;
+    void out_char(char c) const;
+    int write(char const *s, unsigned long count) const override;
+  private:
+    unsigned _baserate;
+  };
+};
