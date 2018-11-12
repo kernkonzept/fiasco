91203bc882b90035f88a3a4152d689fa7e4b5d69
Adam Lackorzynski <adam@l4re.org>
2018-11-12 00:00:00 +0000

    Arm: rcar3: Consider different boot cores
    The boot-core is not necessarily the A57,
    so also try to boot-up this one.
    
    Change-Id: I56e8248a9335d9501af3dd578b6a346d6036bf69

--- src/kern/arm/bsp/rcar3/platform_control-arm-rcar3.cpp
+++ src/kern/arm/bsp/rcar3/platform_control-arm-rcar3.cpp
@@ -10,12 +10,13 @@ PUBLIC static
 void
 Platform_control::boot_ap_cpus(Address phys_tramp_mp_addr)
 {
-  for (int i = 1; i < 8; ++i)
+  for (int i = 0; i < 8; ++i)
     {
       unsigned coreid[8] = { 0x000, 0x001, 0x002, 0x003,
                              0x100, 0x101, 0x102, 0x103 };
       int r = cpu_on(coreid[i], phys_tramp_mp_addr);
-      if (r && r != -2) // Different SoC variants
+      if (r && r != Psci_invalid_parameters // Different SoC variants
+          && r != Psci_already_on)
         printf("CPU%d boot-up error: %d\n", i, r);
     }
 }
