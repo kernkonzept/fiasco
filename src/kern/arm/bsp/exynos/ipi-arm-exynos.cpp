1c05312c57ac0f2ea24fc8bc13a0bd3edd41e5eb
Adam Lackorzynski <adam@l4re.org>
2018-11-26 00:00:00 +0000

    Arm: Exynos: Fix external-gic
    Change-Id: Ia599769aafcd85ee362048df862487cc23ccf47b

--- src/kern/arm/bsp/exynos/ipi-arm-exynos.cpp
+++ src/kern/arm/bsp/exynos/ipi-arm-exynos.cpp
@@ -3,17 +3,24 @@ IMPLEMENTATION [mp && pf_exynos && exynos_extgic]:
 #include "pic.h"
 #include "gic.h"
 
+IMPLEMENT inline NEEDS["processor.h"]
+void
+Ipi::init(Cpu_number cpu)
+{
+  _ipi.cpu(cpu)._sgi_target = Pic::gic.cpu(cpu)->pcpu_to_sgi(Cpu::cpus.cpu(cpu).phys_id());
+}
+
 PUBLIC static inline NEEDS["pic.h"]
 void Ipi::send(Message m, Cpu_number from_cpu, Cpu_phys_id to_cpu)
 {
-  Pic::gic.cpu(from_cpu)->softint_cpu(1UL << cxx::int_value<Cpu_phys_id>(to_cpu), m);
+  Pic::gic.cpu(from_cpu)->softint_cpu(1UL << Pic::gic.cpu(from_cpu)->pcpu_to_sgi(to_cpu), m);
   stat_sent(from_cpu);
 }
 
 PUBLIC static inline NEEDS["pic.h"]
 void Ipi::send(Message m, Cpu_number from_cpu, Cpu_number to_cpu)
 {
-  Pic::gic.cpu(from_cpu)->softint_cpu(1UL << cxx::int_value<Cpu_phys_id>(_ipi.cpu(to_cpu)._phys_id), m);
+  Pic::gic.cpu(from_cpu)->softint_cpu(1UL << _ipi.cpu(to_cpu)._sgi_target, m);
   stat_sent(from_cpu);
 }
 
