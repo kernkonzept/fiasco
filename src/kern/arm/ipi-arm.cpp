0b37776b99b4aa84001c061154190bad91dead6d
Adam Lackorzynski <adam@l4re.org>
2018-11-26 00:00:00 +0000

    Arm: irregual GICs also need a different Ipi::init
    Change-Id: I6772df9a80e1532ba82ef8a6b0b297aa6fc2392f

--- src/kern/arm/ipi-arm.cpp
+++ src/kern/arm/ipi-arm.cpp
@@ -40,13 +40,6 @@ PUBLIC inline
 Ipi::Ipi() : _sgi_target(~0)
 {}
 
-IMPLEMENT inline NEEDS["processor.h"]
-void
-Ipi::init(Cpu_number cpu)
-{
-  _ipi.cpu(cpu)._sgi_target = Pic::gic->pcpu_to_sgi(Cpu::cpus.cpu(cpu).phys_id());
-}
-
 PUBLIC static
 void Ipi::ipi_call_debug_arch()
 {}
@@ -60,6 +53,13 @@ void Ipi::eoi(Message, Cpu_number on_cpu)
 
 // ---------------------------------------------------------------------------
 IMPLEMENTATION [pic_gic && mp && !irregular_gic]:
+
+IMPLEMENT inline NEEDS["processor.h"]
+void
+Ipi::init(Cpu_number cpu)
+{
+  _ipi.cpu(cpu)._sgi_target = Pic::gic->pcpu_to_sgi(Cpu::cpus.cpu(cpu).phys_id());
+}
 
 PUBLIC static inline NEEDS["pic.h"]
 void Ipi::send(Message m, Cpu_number from_cpu, Cpu_number to_cpu)
