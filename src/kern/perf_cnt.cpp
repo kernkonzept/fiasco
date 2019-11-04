0d0a4cc214612bb4de6ffeba0d2755eb29e55a2e
Marcus Hähnel <marcus.haehnel@kernkonzept.com>
2019-11-04 00:00:00 +0000

    Avoid generating code for dummy perf_cnt methods
    The methods for the generic perf_cnt implementation are mainly empty. To
    avoid generating code for them even when they are never called mark them
    inline to aid the compiler in optimizing them away.
    
    Also adjust style a bit to be more readable. Use short style when
    fitting on one line, otherwise use canonical style.
    
    Change-Id: I53be4b4d8a7952a6e11ea7d9e5cf46ebd932dfb7

--- src/kern/perf_cnt.cpp
+++ src/kern/perf_cnt.cpp
@@ -34,27 +34,29 @@ IMPLEMENTATION [!perf_cnt]:
 Perf_cnt::Perf_read_fn Perf_cnt::read_pmc[Max_slot] =
 { dummy_read_pmc, dummy_read_pmc };
 
-static Mword dummy_read_pmc() { return 0; }
+static inline Mword dummy_read_pmc() { return 0; }
 
-PUBLIC static void
+PUBLIC static inline
+void
 Perf_cnt::get_unit_mask(Mword, Unit_mask_type *, Mword *, Mword *) {}
-PUBLIC static void
+
+PUBLIC static inline
+void
 Perf_cnt::get_unit_mask_entry(Mword, Mword, Mword *, const char **) {}
-PUBLIC static void
+
+PUBLIC static inline
+void
 Perf_cnt::get_perf_event(Mword, unsigned *, const char **, const char **) {}
-PUBLIC static Mword
-Perf_cnt::get_max_perf_event() { return 0; }
-PUBLIC static void
-Perf_cnt::split_event(Mword, unsigned *, Mword *) {}
-PUBLIC static Mword
-Perf_cnt::lookup_event(Mword) { return 0; }
-PUBLIC static void
-Perf_cnt::combine_event(Mword, Mword, Mword *) {}
 
-PUBLIC static char const *
-Perf_cnt::perf_type() { return "nothing"; }
+PUBLIC static inline Mword Perf_cnt::get_max_perf_event() { return 0; }
+PUBLIC static inline void  Perf_cnt::split_event(Mword, unsigned *, Mword *) {}
+PUBLIC static inline Mword Perf_cnt::lookup_event(Mword) { return 0; }
+PUBLIC static inline void  Perf_cnt::combine_event(Mword, Mword, Mword *) {}
 
-PUBLIC static int
+PUBLIC static inline char const * Perf_cnt::perf_type() { return "nothing"; }
+
+PUBLIC static inline
+int
 Perf_cnt::mode(Mword /*slot*/, const char **mode, const char **name,
 		    Mword *event, Mword *user, Mword *kern, Mword *edge)
 {
@@ -63,33 +65,16 @@ Perf_cnt::mode(Mword /*slot*/, const char **mode, const char **name,
   return 0;
 }
 
-PUBLIC static
+PUBLIC static inline
 int
 Perf_cnt::setup_pmc(Mword, Mword, Mword, Mword, Mword)
 {
   return 0;
 }
 
-PUBLIC static inline void
-Perf_cnt::start_watchdog()
-{}
-
-PUBLIC static inline void
-Perf_cnt::stop_watchdog()
-{}
-
-PUBLIC static inline void
-Perf_cnt::touch_watchdog()
-{}
-
-PUBLIC static inline int
-Perf_cnt::have_watchdog()
-{ return 0; }
-
-PUBLIC static inline void
-Perf_cnt::setup_watchdog(Mword)
-{}
-
-PUBLIC static inline void
-Perf_cnt::init_ap()
-{}
+PUBLIC static inline void Perf_cnt::start_watchdog()      {}
+PUBLIC static inline void Perf_cnt::stop_watchdog()       {}
+PUBLIC static inline void Perf_cnt::touch_watchdog()      {}
+PUBLIC static inline int  Perf_cnt::have_watchdog()       { return 0; }
+PUBLIC static inline void Perf_cnt::setup_watchdog(Mword) {}
+PUBLIC static inline void Perf_cnt::init_ap()             {}
