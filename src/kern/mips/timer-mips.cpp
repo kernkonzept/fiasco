eb4546121cf986d7c4e9a9f486e41f225b3d4daf
Frank Mehnert <frank.mehnert@kernkonzept.com>
2019-11-11 00:00:00 +0000

    MIPS: fix potential delay of the timer interrupt
    This happens mainly when running on QEMU. The old code already had
    some detection mechanism but it did not consider the case when the
    QEMU process got preempted between reading the counter and setting
    the compare value. After setting the compare value we have to re-read
    the counter to verify that the compare register is still beyond the
    counter. If not, adapt the compare register and try again. Without
    this check we would need to wait for a complete wrap around of the
    counter to receive the next timer interrupt.
    
    Fixes ticket #CD-102.
    
    Change-Id: I29e28f91b4bb4f29642858df21406f750740fd09

--- src/kern/mips/timer-mips.cpp
+++ src/kern/mips/timer-mips.cpp
@@ -12,6 +12,14 @@ private:
   Unsigned32 _counter_high;
 
   static Per_cpu<Static_object<Timer> > _timer;
+
+  enum
+  {
+    // Chose a reasonable amount of cycles within that we will be able to
+    // reprogram the compare register without being 'caught up' by the counter
+    // register.
+    Adj_time = 8000
+  };
 };
 
 IMPLEMENTATION [mips]:
@@ -63,7 +71,16 @@ Timer::init(Cpu_number ncpu)
   if (true) // interval mode
     t->_current_cmp += t->_interval;
 
-  _set_compare(t->_current_cmp);
+  for (;;)
+    {
+      _set_compare(t->_current_cmp);
+      // See explanation in Timer::acknowledge().
+      Unsigned32 cnt = _get_counter();
+      if (EXPECT_TRUE((Signed32)(t->_current_cmp - cnt) > 0))
+        break;
+      t->_current_cmp = cnt + Adj_time;
+      t->_last_counter = t->_current_cmp;
+    }
 }
 
 PUBLIC static inline NEEDS["irq_chip.h"]
@@ -80,20 +97,34 @@ Timer::acknowledge()
   if (true) // interval mode
     t->_current_cmp += t->_interval;
 
-  Unsigned32 cc = _get_counter();
-  if (cc < t->_last_counter)
+  Unsigned32 cnt = _get_counter();
+  if (cnt < t->_last_counter)
     ++t->_counter_high;
 
-  t->_last_counter = cc;
+  t->_last_counter = cnt;
 
-  if ((Signed32)(t->_current_cmp - cc - 4000) < 0)
-    t->_current_cmp = cc + 8000;
+  Unsigned32 new_cmp = t->_current_cmp;
+  for (;;)
+    {
+      // clear TI bit and set new value if applicable
+      _set_compare(new_cmp);
+      // Now verify that the compare register was indeed set beyond the counter
+      // because otherwise it will take a complete wrap around of the counter
+      // until the next timer interrupt is generated. This could happen on QEMU
+      // or in a VM due to preemption but also on real hardware after we
+      // entered + left the kernel debugger. In the latter case we probably
+      // need several timer periods until _current_cmp got in sync with the
+      // counter again. Note that we don't update _current_cmp here because
+      // otherwise we would skip timer interrupts.
+      cnt = _get_counter();
+      if (EXPECT_TRUE((Signed32)(new_cmp - cnt) > 0))
+        break;
+      new_cmp = cnt + Adj_time;
+    }
 
-  // clear TI bit, and set new value if applicable
-  _set_compare(t->_current_cmp);
   // we don't care about CP0 hazards here as a possible IRQ
   // enable afterwards will clear those anyways
-  // printf("TI: %u %u %u\n", cc, t->_current_cmp, t->_interval);
+  // printf("TI: %u %u %u\n", cnt, t->_current_cmp, t->_interval);
 }
 
 PUBLIC static inline NEEDS[Timer::_get_counter]
