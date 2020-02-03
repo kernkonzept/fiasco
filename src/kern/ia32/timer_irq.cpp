8a9c83a7bc228f19dfa2e2e9d6c70d3519a3c275
Alexander Warg <alexander.warg@kernkonzept.com>
2020-02-03 00:00:00 +0000

    IA32: remove buggy vkey code from slow timer hack
    This change removes code from the slow timer handler that could result
    in a context switch before correctly acknowledging the timer IRQs, which
    might block any further timer IRQs for a very long or infinite time.
    
    The functionality is already handled in timer_tick.cpp.
    
    Change-Id: I0485229ea91bbd3c36426fbaee916a70fe95a281

--- src/kern/ia32/timer_irq.cpp
+++ src/kern/ia32/timer_irq.cpp
@@ -20,29 +20,6 @@ thread_timer_interrupt_slow(void)
 {
   if (current_cpu() == Cpu_number::boot_cpu())
     {
-      if (Config::esc_hack)
-	{
-	  // <ESC> timer hack: check if ESC key hit at keyboard
-          int v = Kconsole::console()->getchar(false);
-	  if (v != -1)
-	    {
-	      if (v == 27)
-		kdb_ke("ESC");
-	      else
-		Vkey::add_char(v);
-	    }
-	}
-
-      if (Config::serial_esc != Config::SERIAL_NO_ESC)
-	{
-	  // Here we have to check for serial characters because the
-	  // serial interrupt could have an lower priority than a not
-	  // acknowledged interrupt. The regular case is to stop when
-	  // receiving the serial interrupt.
-	  if (Kconsole::console()->char_avail() == 1 && !Vkey::check_())
-	    kdb_ke("SERIAL_ESC");
-	}
-
       if (Config::watchdog)
 	{
 	  // tell doggy that we are alive
