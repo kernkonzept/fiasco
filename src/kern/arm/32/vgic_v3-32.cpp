83d6bbffa68c9c442c4c2a67cf50db4181e59542
Alexander Warg <alexander.warg@kernkonzept.com>
2020-03-30 00:00:00 +0000

    Fix clang warning about array out of bounds
    Clang seems pretty stupid according to ranges of values, hence the only
    solution I found to silence a false positive warning is this. A simple
    check for n <= N_lregs is not sufficient for clang.
    
    Change-Id: I0c88c6af266c454bca9028d43d3d5291aa2b00ab

--- src/kern/arm/32/vgic_v3-32.cpp
+++ src/kern/arm/32/vgic_v3-32.cpp
@@ -111,7 +111,7 @@ Gic_h_v3::save_lrs(Gic_h::Arm_vgic::Lrs *lr, unsigned n)
   asm ("mrc p15, 4, %0, c12, " #ul", " #v : "=r"(l)); \
   asm ("mrc p15, 4, %0, c12, " #uh", " #v : "=r"(h)); \
   lr->lr64[x] = (((Unsigned64)h) << 32) | l; \
-  if (n <= x + 1) return
+  if ((x + 1 >= Gic_h::Arm_vgic::N_lregs) || (n <= x + 1)) return
 
   READ_LR(c12, c14, 0, 0);
   READ_LR(c12, c14, 1, 1);
@@ -138,7 +138,7 @@ Gic_h_v3::load_lrs(Gic_h::Arm_vgic::Lrs const *lr, unsigned n)
 #define READ_LR(ul,uh,v,x) \
   asm ("mcr p15, 4, %0, c12, " #ul", " #v : : "r"((Unsigned32)lr->lr64[x])); \
   asm ("mcr p15, 4, %0, c12, " #uh", " #v : : "r"((Unsigned32)(lr->lr64[x] >> 32))); \
-  if (n <= x + 1) return
+  if ((x + 1 >= Gic_h::Arm_vgic::N_lregs) || (n <= x + 1)) return
 
   READ_LR(c12, c14, 0, 0);
   READ_LR(c12, c14, 1, 1);
