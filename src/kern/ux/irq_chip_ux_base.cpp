366d00aaa53f2c453255af54ca265c07ddc87147
Frank Mehnert <frank.mehnert@kernkonzept.com>
2021-03-01 00:00:00 +0000

    ux: gcc-10 fix
    Change-Id: I1cdc42de9847f700ca54f729a3c6d7d9b8274b20

--- src/kern/ux/irq_chip_ux_base.cpp
+++ src/kern/ux/irq_chip_ux_base.cpp
@@ -150,7 +150,7 @@ Irq_chip_ux_base::setup_irq_prov(unsigned irq, const char *const path,
 
   switch (pids[irq] = fork())
     {
-      case -1:
+      case (unsigned int)-1:
         return false;
 
       case 0:
