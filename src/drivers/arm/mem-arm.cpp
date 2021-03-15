1a419f1af6a5240714e7a6c6e6e358cf4f1ae834
Jan Klötzke <jan.kloetzke@kernkonzept.com>
2021-03-15 00:00:00 +0000

    Optimize memcpy/memset(_mwords)
    These standard functions are called in performance-critical sections
    and are adding significant overhead because the default implementation
    copied/set only byte-by-byte.
    
    The first part of the optimization is to optimize the default
    C-implementations so that they yield decent assembly on most platforms.
    Anything should be better than copying bytes only. For the
    implementation we can also exploit the fact that unaligned accesses are
    practically non-existent in Fiasco and can spare all the special
    handling. This affects all platforms except memcpy on IA32 which has a
    dedicated inline assembly version already.
    
    For the arm 32-bit platform the memcpy_mwords function is additionally
    optimized because the compiler is not smart enough to use ldm/stm by
    itself. As almost all call sites use a constant length, the code is
    especially optimized for this case.
    
    The memset_mwords function on arm stores 64 bit words to nudge the
    compiler to exploit the strd instructions. Even though not strictly
    required by the architecture, aligning the pointer to 64 bit will
    generate more efficient bus access on write-through caches. On arm64
    memset_mwords stores multiple 64 bit words at once to make sure the
    compiler uses stp where possible.
    
    Change-Id: I583c3d6c103f1110f1f34787d5fb05ce7e168bbc

--- src/drivers/arm/mem-arm.cpp
+++ src/drivers/arm/mem-arm.cpp
@@ -9,20 +9,103 @@ public:
 //-----------------------------------------------------------------------------
 IMPLEMENTATION [arm]:
 
+IMPLEMENT_DEFAULT inline void Mem::prefetch_w(void *) {}
+
+//-----------------------------------------------------------------------------
+IMPLEMENTATION [arm && 32bit]:
+
 IMPLEMENT static inline
 void
 Mem::memset_mwords (void *dst, const unsigned long value, unsigned long nr_of_mwords)
 {
-  unsigned long *d = (unsigned long *)dst;
-  for (; nr_of_mwords--; d++)
-    *d = value;
+  if (!nr_of_mwords)
+    return;
+
+  typedef unsigned long __attribute__((may_alias)) U32;
+  typedef unsigned long long __attribute__((may_alias)) U64;
+
+  U32 *d32 = (U32 *)dst;
+  if ((unsigned long)d32 & 0x4U)
+    {
+      *d32++ = value;
+      nr_of_mwords--;
+    }
+
+  U64 v64 = ((U64)value << 32) | value;
+  for (; nr_of_mwords >= 4; d32 += 4, nr_of_mwords -= 4)
+    {
+      ((U64 *)d32)[0] = v64;
+      ((U64 *)d32)[1] = v64;
+    }
+
+  if (nr_of_mwords & 0x2U)
+    {
+      *((U64 *)d32) = v64;
+      d32 += 2;
+    }
+
+  if (nr_of_mwords & 0x1U)
+    *d32 = value;
 }
 
 IMPLEMENT static inline
 void
 Mem::memcpy_mwords (void *dst, void const *src, unsigned long nr_of_mwords)
 {
-  __builtin_memcpy(dst, src, nr_of_mwords * sizeof(unsigned long));
+  unsigned long __attribute__((may_alias)) *s = (unsigned long *)src;
+  unsigned long __attribute__((may_alias)) *d = (unsigned long *)dst;
+  unsigned long tmp;
+
+  if (__builtin_constant_p(nr_of_mwords))
+    {
+      // Exploit the fact that the length is a compile time constant to pick
+      // the best inline assembly.
+      switch (nr_of_mwords & 0x03U)
+        {
+        case 0:
+          break;
+        case 1:
+          asm volatile("ldr %0, [%1], #4\n"
+                       "str %0, [%2], #4\n"
+                       : "=&r"(tmp), "+r"(s), "+r"(d)
+                       : : "memory");
+          break;
+        case 2:
+          asm volatile("ldm %0!, {r0, r1}\n"
+                       "stm %1!, {r0, r1}\n"
+                       : "+&r"(s), "+&r"(d)
+                       : : "r0", "r1", "memory");
+          break;
+        case 3:
+          asm volatile("ldm %0!, {r0, r1, r2}\n"
+                       "stm %1!, {r0, r1, r2}\n"
+                       : "+&r"(s), "+&r"(d)
+                       : : "r0", "r1", "r2", "memory");
+          break;
+        }
+      // nr_of_mwords &= ~0x03UL; not necessary because code below shifts out
+      // the two LSBs anyway
+    }
+  else
+    while (nr_of_mwords & 0x03U)
+      {
+        *d++ = *s++;
+        nr_of_mwords--;
+      }
+
+  // Need to use inline assembly here because the compiler is not smart enough
+  // to use ldm/stm. We could let the compiler choose the scratch registers but
+  // it results in assembler warnings because the order of the registers is
+  // sometimes not ascending. Using the caller-saved registers emitted the best
+  // code overall.
+  nr_of_mwords >>= 2;
+  while (nr_of_mwords--)
+    {
+      asm volatile("ldm %0!, {r0, r1, r2, r3}\n"
+                   "stm %1!, {r0, r1, r2, r3}\n"
+                   : "+&r"(s), "+&r"(d)
+                   : : "r0", "r1", "r2", "r3", "memory");
+    }
 }
 
 IMPLEMENT static inline
@@ -32,7 +115,50 @@ Mem::memcpy_bytes(void *dst, void const *src, unsigned long nr_of_bytes)
   __builtin_memcpy(dst, src, nr_of_bytes);
 }
 
-IMPLEMENT_DEFAULT inline void Mem::prefetch_w(void *) {}
+//-----------------------------------------------------------------------------
+IMPLEMENTATION [arm && 64bit]:
+
+IMPLEMENT static inline
+void
+Mem::memset_mwords (void *dst, const unsigned long value, unsigned long nr_of_mwords)
+{
+  if (!nr_of_mwords)
+    return;
+
+  unsigned long __attribute__((may_alias)) *d = (unsigned long *)dst;
+  for (; nr_of_mwords >= 4; d += 4, nr_of_mwords -= 4U)
+    {
+      d[0] = value;
+      d[1] = value;
+      d[2] = value;
+      d[3] = value;
+    }
+
+  if (nr_of_mwords & 0x2U)
+    {
+      d[0] = value;
+      d[1] = value;
+      d += 2;
+    }
+
+  if (nr_of_mwords & 0x1U)
+    *d = value;
+}
+
+IMPLEMENT static inline
+void
+Mem::memcpy_mwords (void *dst, void const *src, unsigned long nr_of_mwords)
+{
+  // The C-version is good enough.
+  __builtin_memcpy(dst, src, nr_of_mwords * sizeof(unsigned long));
+}
+
+IMPLEMENT static inline
+void
+Mem::memcpy_bytes(void *dst, void const *src, unsigned long nr_of_bytes)
+{
+  __builtin_memcpy(dst, src, nr_of_bytes);
+}
 
 //-----------------------------------------------------------------------------
 IMPLEMENTATION [arm_v5]:
