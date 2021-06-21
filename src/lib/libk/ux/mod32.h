90f66ae14c3ddf51d1e5bf190453a4d3ba77326d
Frank Mehnert <frank.mehnert@kernkonzept.com>
2021-06-21 00:00:00 +0000

    IA32/mod32.h: fix modulo for dividend >= 1<<32
    In I17c27b20651e7036c4d6ac36349ca6d0f845b871 we changed the code for
    mod32.h to perform only one division. This is wrong if the dividend
    is a real 64-bit value (upper word not 0) and the divisor is very
    small. Fix this case.
    
    Change-Id: Ia32588e6d086d0862587bdd0013af556db5815b3

--- src/lib/libk/ux/mod32.h
+++ src/lib/libk/ux/mod32.h
@@ -9,9 +9,13 @@ unsigned long
 mod32(unsigned long long dividend, unsigned long divisor)
 {
   unsigned long ret, dummy;
-  asm ("divl    %[divisor]      \n\t"
+  asm ("divl    %[divisor]              \n\t"
+       "movl    %[dividend_lo32], %%eax \n\t"
+       "divl    %[divisor]              \n\t"
      : "=d"(ret), "=a"(dummy)
-     : "A"(dividend), [divisor]"rm"(divisor));
+     : "a"((unsigned long)(dividend >> 32)), "d"(0),
+       [dividend_lo32]"irm"((unsigned long)(dividend & 0xffffffff)),
+       [divisor]"rm"(divisor));
   return ret;
 }
 
