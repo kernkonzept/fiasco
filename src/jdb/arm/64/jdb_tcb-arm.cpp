d4dc5fe04f9af355236a69dcb87d1588a563dfa8
Frank Mehnert <frank.mehnert@kernkonzept.com>
2021-09-06 00:00:00 +0000

    jdb_tcb/ARM64: name 'ULR'
    The userspace LR register becomes more important when using the KIP
    syscall code.
    
    Change-Id: I41f5b0b3c65fe2ebc27537436045cbfeda25e6c3

--- src/jdb/arm/64/jdb_tcb-arm.cpp
+++ src/jdb/arm/64/jdb_tcb-arm.cpp
@@ -80,14 +80,14 @@ IMPLEMENT inline
 bool
 Jdb_tcb_ptr::is_user_value() const
 {
-  return _offs >= Context::Size - 6 * sizeof(Mword);
+  return _offs >= Context::Size - 7 * sizeof(Mword);
 }
 
 IMPLEMENT inline
 const char *
 Jdb_tcb_ptr::user_value_desc() const
 {
-  const char *desc[] = { "PSR", "PC", "USP", "PFA", "ESR", "KSP" };
+  const char *desc[] = { "PSR", "PC", "USP", "PFA", "ESR", "KSP", "ULR" };
   return desc[(Context::Size - _offs) / sizeof(Mword) - 1];
 }
 
