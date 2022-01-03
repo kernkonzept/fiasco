37b21dddb47bab9c4136148d4057fd3ad9884ce3
Jan Klötzke <jan.kloetzke@kernkonzept.com>
2022-01-03 00:00:00 +0000

    libk: make min/max constexpr
    Make them usable as compile time constant expressions.
    
    Change-Id: Icadfd28524368d46a3cfcda20e129451bb7df88c

--- src/lib/libk/minmax.h
+++ src/lib/libk/minmax.h
@@ -4,13 +4,13 @@
 
 // trivial, used to terminate the variadic recursion
 template<typename A>
-inline A const &
+constexpr A const &
 min(A const &a)
 { return a; }
 
 // matches with automatic argument type deduction
 template<typename A, typename ...ARGS>
-inline A const &
+constexpr A const &
 min(A const &a1, A const &a2, ARGS const &...a)
 {
   return min((a1 <= a2) ? a1 : a2, a...);
@@ -18,7 +18,7 @@ min(A const &a1, A const &a2, ARGS const &...a)
 
 // matches with explicit template type A
 template<typename A, typename ...ARGS>
-inline A const &
+constexpr A const &
 min(typename cxx::identity<A>::type const &a1,
     typename cxx::identity<A>::type const &a2,
     ARGS const &...a)
@@ -28,19 +28,19 @@ min(typename cxx::identity<A>::type const &a1,
 
 // trivial, used to terminate the variadic recursion
 template<typename A>
-inline A const &
+constexpr A const &
 max(A const &a)
 { return a; }
 
 // matches with automatic argument type deduction
 template<typename A, typename ...ARGS>
-inline A const &
+constexpr A const &
 max(A const &a1, A const &a2, ARGS const &...a)
 { return max((a1 >= a2) ? a1 : a2, a...); }
 
 // matches with explicit template type A
 template<typename A, typename ...ARGS>
-inline A const &
+constexpr A const &
 max(typename cxx::identity<A>::type const &a1,
     typename cxx::identity<A>::type const &a2,
     ARGS const &...a)
