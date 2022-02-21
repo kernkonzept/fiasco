433057f77d2f0ed76a982bbd728bcd6614274c03
Adam Lackorzynski <adam@l4re.org>
2022-02-21 09:00:00 +0000

    Arm: Generalize PSCI-based core booting
    Put the loop for booting up cores via PSCI into a common loop and use
    it, avoiding copying the code for each BSPs where it is applicable.
    
    Change-Id: I972cbcf1cca09ed6e084bee671ad16bf49fc921f

--- src/kern/arm/bsp/zynqmp/platform_control-arm-zynqmp.cpp
+++ src/kern/arm/bsp/zynqmp/platform_control-arm-zynqmp.cpp
@@ -13,24 +13,8 @@ PUBLIC static
 void
 Platform_control::boot_ap_cpus(Address phys_tramp_mp_addr)
 {
-  for (int i = 0; i < min<unsigned>(4, Config::Max_num_cpus); ++i)
-    {
-      unsigned coreid[4] = { 0x0, 0x1, 0x2, 0x3 };
-      int r = cpu_on(coreid[i], phys_tramp_mp_addr);
-      if (r)
-        {
-          if (r != Psci::Psci_already_on)
-            printf("CPU%d boot-up error: %d\n", i, r);
-          continue;
-        }
-
-      // The Zynq-MP firmware will not boot all CPUs if we fire CPU_ON
-      // events too fast, thus wait for each CPU to appear so that we do not
-      // overburden the firmware.
-      while (!Cpu::online(Cpu_number(i)))
-        {
-          Mem::barrier();
-          Proc::pause();
-        }
-    }
+  // The Zynq-MP firmware will not boot all CPUs if we fire CPU_ON
+  // events too fast, thus boot sequentially waiting for each core to appear
+  // so that we do not overburden the firmware.
+  boot_ap_cpus_psci(phys_tramp_mp_addr, { 0x0, 0x1, 0x2, 0x3 }, true);
 }
--- src/kern/arm/platform_control-arm-psci.cpp
+++ src/kern/arm/platform_control-arm-psci.cpp
@@ -1,12 +1,61 @@
 IMPLEMENTATION [arm && arm_psci]:
 
+#include "config.h"
+#include "cpu.h"
+#include "kip.h"
+#include "mem.h"
+#include "minmax.h"
+#include "processor.h"
 #include "psci.h"
+
+#include <cstdio>
 
 PUBLIC static
 int
 Platform_control::cpu_on(unsigned long target, Address phys_tramp_mp_addr)
 {
   return Psci::cpu_on(target, phys_tramp_mp_addr);
+}
+
+PUBLIC template<size_t NUM>
+static void
+Platform_control::boot_ap_cpus_psci(Address phys_tramp_mp_addr,
+                                    const unsigned long (&physid_list)[NUM],
+                                    bool /* sequential */ = false)
+{
+  /* The current implementation is booting cores sequentially only */
+
+  unsigned seq = 1;
+  for (unsigned long physid : physid_list)
+    {
+      if (seq >= Config::Max_num_cpus)
+        break;
+
+      int r = Psci::cpu_on(physid, phys_tramp_mp_addr);
+      if (r)
+        {
+          if (r != Psci::Psci_already_on)
+            printf("CPU%d/%lx boot-up error: %d\n", seq, physid, r);
+          continue;
+        }
+
+      Unsigned64 timeout = Kip::k()->clock() + 500 * 1000;
+      while (1)
+        {
+          if (Cpu::online(Cpu_number(seq)))
+            break;
+
+          if (Kip::k()->clock() > timeout)
+            {
+              printf("CPU%d/%lx did not come online.\n", seq, physid);
+              break;
+            }
+
+          Mem::barrier();
+          Proc::pause();
+        }
+      ++seq;
+    }
 }
 
 PUBLIC static
