02d0559be99a7a01eaaa0d4bebc90c1ee92b2425
Georg Kotheimer <georg.kotheimer@kernkonzept.com>
2022-04-04 00:00:00 +0000

    drivers: Consolidate keycode constants
    For one thing, this completes the change
    I15b5d9a3d8c69ea25e0227a85218a05f6745024e, which claimed to
    "Reuse keycodes-arch.h for all but UX", but missed the actual
    removal of the architecture specific keycodes-arch.h files.
    
    Furthermore, with the introduction of KEY_BACKSPACE_2 in change
    I7bf13f74c6e9465d0055a49e91e68cbb674a2cff the speciality of the
    UX's backspace key is no longer special, so their is no need for
    any architecture specific keycodes anymore.
    
    While at it, add some documentation to the keycodes.
    
    Change-Id: I7223048fd92b68dd0d10529c0ed078b0eb3bf530

--- src/drivers/keycodes.h
+++ src/drivers/keycodes.h
@@ -3,10 +3,17 @@
 #ifndef KEYCODES_H__
 #define KEYCODES_H__
 
-#include "keycodes-arch.h"
-
 enum
 {
+  // Keycodes which correspond to their respective ASCII values.
+  KEY_BACKSPACE         = 0x08,
+  KEY_BACKSPACE_2       = 0x7F,
+  KEY_TAB               = 0x09,
+  KEY_ESC               = 0x1b,
+  KEY_RETURN            = 0x0d,
+  KEY_RETURN_2          = 0x0a,
+
+  // Internal keycodes, do not correspond to any official encoding.
   KEY_CURSOR_UP		= 0xb8,
   KEY_CURSOR_DOWN	= 0xb2,
   KEY_CURSOR_LEFT	= 0xb4,
@@ -18,7 +25,8 @@ enum
   KEY_INSERT		= 0xb0,
   KEY_DELETE		= 0xae,
   KEY_F1		= 0xc0,
-  KEY_SINGLE_ESC	= 0x101,
+  KEY_SINGLE_ESC        = 0x101, ///< Explicit "single-ESC" keycode, indicating that
+                                 ///  the ESC is not part of an ESC sequence.
 };
 
 #endif // KEYCODES_H__
