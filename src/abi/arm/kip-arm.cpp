370d2b07ff62881b5ce4296234d91297ff31b7f5
Adam Lackorzynski <adam@l4re.org>
2022-04-18 00:00:00 +0000

    arm: Simplify Kip platform_infos
    Do not have all the details of the Kip platform info in the kernel but
    only have the place-holder to know the structure sizes. The kernel does
    not use those fields.
    
    Change-Id: I18e18c2858ef201de690d1f8c55fdc433fe774ca

--- src/abi/arm/kip-arm.cpp
+++ src/abi/arm/kip-arm.cpp
@@ -13,15 +13,7 @@ public:
   {
     char name[16];
     Unsigned32 is_mp;
-    struct
-    {
-      struct
-      {
-        Unsigned32 MIDR, CTR, TCMTR, TLBTR, MPIDR, REVIDR;
-        Unsigned32 ID_PFR[2], ID_DFR0, ID_AFR0, ID_MMFR[4], ID_ISAR[6];
-      } cpuinfo;
-    } arch;
-    Unsigned32 pad[3];
+    char arch_cpuinfo[252]; // Just placeholders
   };
 
   /* 0xF0 */
