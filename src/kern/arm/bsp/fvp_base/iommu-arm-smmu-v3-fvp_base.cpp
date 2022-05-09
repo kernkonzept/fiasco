6e26a7c61eb5497ee5e29967903f57edad226085
Georg Kotheimer <georg.kotheimer@kernkonzept.com>
2022-05-09 00:00:00 +0000

    arm-fvp: Add support for SMMUv3
    Requires-Change-Id: I157aa7d2308b7e5b5fdec4b62971e9f58d9e6e86
    Change-Id: I1b63c889a8aa1ca5e2efd3b4de13b26f09d74fc8

---
+++ src/kern/arm/bsp/fvp_base/iommu-arm-smmu-v3-fvp_base.cpp
@@ -0,0 +1,26 @@
+IMPLEMENTATION [arm && iommu && pf_fvp_base]:
+
+#include "kmem.h"
+
+IMPLEMENT
+bool
+Iommu::init_platform()
+{
+  static_assert(Num_iommus == 1, "Unexpected number of IOMMUs.");
+  Address base_addr = 0x002b400000;
+
+  // 103  71  SMMUv3 non-secure combined interrupt.
+  // 104  72  SMMUv3 secure combined interrupt. Unused because there is no secure side.
+  // 105  73  SMMUv3 secure event queue. Unused because there is no secure side.
+  // 106  74  SMMUv3 non-secure event queue.
+  // 107  75  SMMUv3 PRI queue. Unused because no PCIe device supports PRI.
+  // 108  76  SMMUv3 secure command queue sync. Unused because there is no secure side.
+  // 109  77  SMMUv3 non-secure command queue sync.
+  // 110  78  SMMUv3 secure GERROR. Unused because there is no secure side.
+  // 111  79  SMMUv3 non-secure GERROR.
+
+  Address v = Kmem::mmio_remap(base_addr, 0x100000);
+  iommus()[0].setup(v, 106, 111);
+
+  return true;
+}
