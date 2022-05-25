df0baaa6b56b982df2a5706da9f6ee06a7d6ffe1
Jan Klötzke <jan.kloetzke@kernkonzept.com>
2022-05-25 11:39:20 +0200

    Make sigma0 and root task optional
    Do not start sigma0 and/or the root task if their entry point is not
    set. This enables future setups where they may not be required to be
    started.
    
    Change-Id: I2e88bbc93d32ce58a4be2a8ad83a2b8eed2b3b3b

--- src/kern/kernel_thread-std.cpp
+++ src/kern/kernel_thread-std.cpp
@@ -27,6 +27,10 @@ Kernel_thread::init_workload()
   Cap_index const C_pager   = Cap_index(Initial_kobjects::Pager);
 
   unsigned node = Platform_control::node_id();
+
+  if (!Kip::k()->sigma0[node])
+    return;
+
   auto g = lock_guard(*cpu_lock);
 
   //
@@ -69,53 +73,59 @@ Kernel_thread::init_workload()
   check (sigma0_thread->control(Thread_ptr(Thread_ptr::Null), Thread_ptr(Thread_ptr::Null)) == 0);
   check (sigma0_thread->bind(sigma0, User<Utcb>::Ptr((Utcb*)Virt_addr::val(sigma0_utcb.mem_address()))));
   check (sigma0_thread->ex_regs(Kip::k()->sigma0[node], 0));
+  sigma0_thread->set_home_cpu(Cpu_number::boot_cpu());
 
   //
   // create the boot task
   //
 
-  Task *boot_task = Task::create<Task>(*Ram_quota::root, L4_msg_tag(), 0, 0, &err, 0);
+  Thread_object *boot_thread = nullptr;
 
-  assert_opt (boot_task);
-  L4_fpage root_utcb = L4_fpage::mem(Mem_layout::Utcb_addr,
-                                     Config::PAGE_SHIFT);
-  check(boot_task->alloc_ku_mem(root_utcb)
-        >= 0);
-  boot_task->map_all_segs(Mem_desc::Bootloader, node);
-
-  // prevent deletion of this thing
-  boot_task->inc_ref();
-
-  Thread_object *boot_thread = new (*Ram_quota::root) Thread_object(*Ram_quota::root);
-
-  assert (boot_thread);
-
-  // prevent deletion of this thing
-  boot_thread->inc_ref();
-
-  check (map(boot_task,   boot_task, boot_task, C_task, 0));
-  check (map(boot_thread, boot_task, boot_task, C_thread, 0));
-
-  check (boot_thread->control(Thread_ptr(C_pager), Thread_ptr(Thread_ptr::Null)) == 0);
-  check (boot_thread->bind(boot_task, User<Utcb>::Ptr((Utcb*)Virt_addr::val(root_utcb.mem_address()))));
-  check (boot_thread->ex_regs(Kip::k()->root[node], 0));
-
-  Ipc_gate *s0_b_gate = Ipc_gate::create(*Ram_quota::root, sigma0_thread, 4 << 4);
-
-  check (s0_b_gate);
-  check (map(s0_b_gate, boot_task, boot_task, C_pager, 0));
-
-  sigma0_thread->set_home_cpu(Cpu_number::boot_cpu());
-  boot_thread->set_home_cpu(Cpu_number::boot_cpu());
-
-  sigma0_thread->activate();
-  check (obj_map(sigma0, C_factory, 1, boot_task, C_factory, 0).error() == 0);
-  for (Cap_index c = Initial_kobjects::first(); c < Initial_kobjects::end(); ++c)
+  if (Kip::k()->root[node])
     {
-      Kobject_iface *o = initial_kobjects->obj(c);
-      if (o)
-	check(obj_map(sigma0, c, 1, boot_task, c, 0).error() == 0);
+      Task *boot_task = Task::create<Task>(*Ram_quota::root, L4_msg_tag(), 0, 0, &err, 0);
+
+      assert_opt (boot_task);
+      L4_fpage root_utcb = L4_fpage::mem(Mem_layout::Utcb_addr,
+                                         Config::PAGE_SHIFT);
+      check(boot_task->alloc_ku_mem(root_utcb)
+            >= 0);
+      boot_task->map_all_segs(Mem_desc::Bootloader, node);
+
+      // prevent deletion of this thing
+      boot_task->inc_ref();
+
+      boot_thread = new (*Ram_quota::root) Thread_object(*Ram_quota::root);
+
+      assert (boot_thread);
+
+      // prevent deletion of this thing
+      boot_thread->inc_ref();
+
+      check (map(boot_task,   boot_task, boot_task, C_task, 0));
+      check (map(boot_thread, boot_task, boot_task, C_thread, 0));
+
+      check (boot_thread->control(Thread_ptr(C_pager), Thread_ptr(Thread_ptr::Null)) == 0);
+      check (boot_thread->bind(boot_task, User<Utcb>::Ptr((Utcb*)Virt_addr::val(root_utcb.mem_address()))));
+      check (boot_thread->ex_regs(Kip::k()->root[node], 0));
+
+      Ipc_gate *s0_b_gate = Ipc_gate::create(*Ram_quota::root, sigma0_thread, 4 << 4);
+
+      check (s0_b_gate);
+      check (map(s0_b_gate, boot_task, boot_task, C_pager, 0));
+
+      boot_thread->set_home_cpu(Cpu_number::boot_cpu());
+
+      check (obj_map(sigma0, C_factory, 1, boot_task, C_factory, 0).error() == 0);
+      for (Cap_index c = Initial_kobjects::first(); c < Initial_kobjects::end(); ++c)
+        {
+          Kobject_iface *o = initial_kobjects->obj(c);
+          if (o)
+            check(obj_map(sigma0, c, 1, boot_task, c, 0).error() == 0);
+        }
     }
 
-  boot_thread->activate();
+  sigma0_thread->activate();
+  if (boot_thread)
+    boot_thread->activate();
 }
