d36a06c9796e78b98feb84c07d57eedd066da28f
Frank Mehnert <frank.mehnert@kernkonzept.com>
2022-12-12 00:00:00 +0000

    add support for second uart
    Code for a second Uart instance is useful if the platform-provided Uart
    is not known at build time.
    
    In that case, add
    - `CXXFLAGS_uart-libuart += $(call LIBUART_UART2, foo)`
      (basically very similar to existing `CXXFLAGS_uart-libuart` lines)
    - `PREPROCESS_PARTS += second_uart`
    - `OBJECTS_LIBUART += uart_foo.o`
    
    to the platform Modules file to add L4::Uart_foo which is declared in
    the header file uart_foo.h. By default, the Uart which is declared by
    `$(call LIBUART_UART)` is initialized. To use the second Uart instead,
    call
    
      Uart::set_uart_instance(instance)
    
    with `instance > 0`. This has to be done before Uart::startup() is
    executed (called from Kernel_uart::init() which is called from
    Startup::stage1()).
    
    Change-Id: Ida6eb7abf2bee9a5652e56a83bb78e6f64a9ae5f

--- src/drivers/uart-libuart.cpp
+++ src/drivers/uart-libuart.cpp
@@ -6,13 +6,19 @@ INTERFACE [libuart]:
 EXTENSION class Uart
 {
 private:
+  static void init_uart_instance(Unsigned32 base_baud);
+  void bsp_init();
+
   int _irq = -1;
+  static L4::Uart *_uart;
 };
 
 //------------------------------------------------------
 IMPLEMENTATION [libuart]:
 
 Static_object<FIASCO_UART_TYPE> _the_uart;
+
+L4::Uart *Uart::_uart;
 
 IMPLEMENT
 Uart::Uart() : Console(DISABLED) {}
@@ -23,13 +29,21 @@ int Uart::irq() const
   return _irq;
 }
 
+IMPLEMENT_DEFAULT
+void
+Uart::bsp_init()
+{}
+
 PROTECTED bool
 Uart::startup(L4::Io_register_block const *reg, int irq, Unsigned32 base_baud)
 {
   _irq = irq;
-  _the_uart.construct(base_baud);
 
-  if (!_the_uart->startup(reg))
+  bsp_init();
+
+  init_uart_instance(base_baud);
+
+  if (!_uart->startup(reg))
     return false;
 
   add_state(ENABLED);
@@ -39,5 +53,44 @@ Uart::startup(L4::Io_register_block const *reg, int irq, Unsigned32 base_baud)
 IMPLEMENT L4::Uart *
 Uart::uart()
 {
-  return _the_uart.get();
+  return _uart;
+}
+
+//------------------------------------------------------
+IMPLEMENTATION [!second_uart]:
+
+IMPLEMENT void inline
+Uart::init_uart_instance(Unsigned32 base_baud)
+{
+  _uart = _the_uart.construct(base_baud);
+}
+
+//------------------------------------------------------
+IMPLEMENTATION [second_uart]:
+
+EXTENSION class Uart
+{
+  static unsigned _instance;
+};
+
+unsigned Uart::_instance;
+Static_object<FIASCO_UART2_TYPE> _the_uart_2;
+
+IMPLEMENT void inline
+Uart::init_uart_instance(Unsigned32 base_baud)
+{
+  if (_instance)
+    _uart = _the_uart_2.construct(base_baud);
+  else
+    _uart = _the_uart.construct(base_baud);
+}
+
+PUBLIC bool
+Uart::set_uart_instance(unsigned instance)
+{
+  if (state() & ENABLED)
+    return false; // too late -- Uart::startup() already called
+
+  _instance = instance;
+  return true;
 }
