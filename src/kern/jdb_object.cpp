a39cbe6532e64a5fccad9d739a02eb5bd4c6833a
Diego Dias <diego.dias@kernkonzept.com>
2023-01-09 00:00:00 +0000

    update copyright years
    Change-Id: I5608af8a7a10df6842cc7bcb92ec6cd1c9d1153e

--- src/kern/jdb_object.cpp
+++ src/kern/jdb_object.cpp
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2016 Kernkonzept GmbH.
+ * Copyright (C) 2016, 2020-2021 Kernkonzept GmbH.
  * Author: Steffen Liebergeld <steffen.liebergeld@kernkonzept.com>
  *
  * This file is distributed under the terms of the GNU General Public
