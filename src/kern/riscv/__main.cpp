a784e653044c40e9eed22180febcf2cac3dcb9dc
Georg Kotheimer <georg.kotheimer@kernkonzept.com>
2023-07-17 00:00:00 +0000

    riscv: mark kernel_main() / bootstrap() as "noreturn"
    This function does never return on any architecture so mark it as such.
    
    Addendum to I633369ea84addad87ca9b3c2e784de5f8c1ebd9b.
    
    Change-Id: I81a39f9380cdae81326503a1100a2c680bb55b71

--- src/kern/riscv/__main.cpp
+++ src/kern/riscv/__main.cpp
@@ -11,7 +11,7 @@ IMPLEMENTATION [riscv]:
 #include "processor.h"
 #include "terminate.h"
 
-void kernel_main(void);
+void kernel_main(void) FIASCO_NORETURN;
 
 extern "C"
 void __main(Mword hart_id)
