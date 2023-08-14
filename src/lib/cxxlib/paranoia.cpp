63f6e4cb121754517059ef332c81b52c88dc2b12
Frank Mehnert <frank.mehnert@kernkonzept.com>
2023-08-14 00:00:00 +0000

    provide dummy operator delete(void *, std::align_val_t) for C++17
    According to C++17 the compiler will generate code to use
    
      operator delete(void *, std::align_val_t)
    
    rather than
    
      operator delete(void *)
    
    for deleting objects if the alignment of the object exceeds the value of
    __STDCPP_DEFAULT_NEW_ALIGNMENT__. So far this was only seen on SPARC
    with __STDCPP_DEFAULT_NEW_ALIGNMENT__==8 and alignof(Context)==16.
    
    As we don't free runtime objects this way, the default implementation of
    the delete operator is a dummy triggering panic(). However, we shall
    provide a dummy for the C++17 delete operator to be on the safe side.
    
    Even Clang-17 doesn't provide std::align_val_t, so this is gcc-only.
    
    Change-Id: I4920b277906677acf5ac31a7436dbf8f0c131895

--- src/lib/cxxlib/paranoia.cpp
+++ src/lib/cxxlib/paranoia.cpp
@@ -2,6 +2,9 @@ INTERFACE:
 
 #include <cstddef>
 
+// Required for Clang which lacks a builtin definition of std::align_val_t.
+namespace std { enum class align_val_t : size_t {}; }
+
 IMPLEMENTATION:
 
 #include <cstdio>
@@ -25,11 +28,27 @@ extern "C" void __pure_virtual()
 
 void operator delete(void *) throw()
 {
-  // This must not happen: We never delete an object of the abstract
-  // class slab_cache_anon.  If the compiler was clever, it wouldn't
-  // generate a call to this function (because all destructors of
-  // abstract base classes have been marked abstract virtual), and
-  // we wouldn't need to define this.
+  // This must not happen: We never delete an object of the abstract class
+  // slab_cache_anon.  If the compiler was clever, it wouldn't generate a call
+  // to this function (because all destructors of abstract base classes have
+  // been marked abstract virtual), and we wouldn't need to define this.
+  panic("operator delete (aka __builtin_delete) called from " L4_PTR_FMT,
+      L4_PTR_ARG(__builtin_return_address(0)));
+}
+
+void operator delete(void *, std::align_val_t)
+{
+  // This must not happen -- same as above.
+  //
+  // According to C++17, the compiler will call this function instead of the
+  // function above if the alignment of the object to be deleted exceeds the
+  // value of __STDCPP_DEFAULT_NEW_ALIGNMENT__.
+  //
+  // So far this was only seen on SPARC with
+  //  * __STDCPP_DEFAULT_NEW_ALIGNMENT__ == 8 and
+  //  * alignof(Context) == 16.
+  //
+  // Clang doesn't provide std::align_val_t.
   panic("operator delete (aka __builtin_delete) called from " L4_PTR_FMT,
       L4_PTR_ARG(__builtin_return_address(0)));
 }
