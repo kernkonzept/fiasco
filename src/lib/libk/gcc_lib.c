3d8715b345d6c9c4962bbb1777366d3482c4e7e5
Frank Mehnert <frank.mehnert@kernkonzept.com>
2023-08-21 00:00:00 +0000

    implement __aeabi_uidivmod and __aeabi_uidiv
    These functions are required by ARM 32-bit platforms lacking the `udiv`
    Assembler instruction (ARM < v7).
    
    See also ticket #CD-1086.
    
    Change-Id: I65c0a5607bc4a09d45348a2ac59e80d28ac00269

--- src/lib/libk/gcc_lib.c
+++ src/lib/libk/gcc_lib.c
@@ -10,6 +10,16 @@ struct ullmoddiv_t
   unsigned long long mod;
 };
 
+/**
+ * 64-bit unsigned integer division + modulo for 32-bit machines.
+ * Used by the wrapper functions __umoddi3(), __udivdi3(), __udivmoddi4(), and
+ * __aeabi_uldivmod().
+ *
+ * \param div  Dividend.
+ * \param s    Divisor.
+ * \returns ullmoddiv_t structure containing the divisor and modulo result from
+ *          the 64-bit unsigned division.
+ */
 static struct ullmoddiv_t ullmoddiv(unsigned long long div, unsigned long long s)
 {
   unsigned long long i;
@@ -22,14 +32,13 @@ static struct ullmoddiv_t ullmoddiv(unsigned long long div, unsigned long long s
     return (struct ullmoddiv_t){.div = 0, .mod = div};
 
   tmp = 1;
-  while (!(s & (1ULL<<63)) && s < div)
+  while (!(s & (1ULL << 63)) && s < div)
     {
       s <<= 1;
       tmp <<= 1;
     }
 
   i = 0;
-
   while (1)
     {
       if (div >= s)
@@ -48,6 +57,60 @@ static struct ullmoddiv_t ullmoddiv(unsigned long long div, unsigned long long s
   return (struct ullmoddiv_t){.div = i, .mod = div};
 }
 
+struct umoddiv_t
+{
+  unsigned div;
+  unsigned mod;
+};
+
+/**
+ * 32-bit unsigned integer division + modulo for 32-bit machines lacking the
+ * `udiv` Assembler instruction.
+ * Implemented analogously to ullmoddiv().
+ * Used by the wrapper functions __aeabi_uidivmod() and __aeabi_uidiv().
+ *
+ * \param div  Dividend.
+ * \param s    Divisor.
+ * \returns umoddiv_t structure containing the divisor and modulo result from
+ *          the 32-bit unsigned division.
+ */
+struct umoddiv_t umoddiv(unsigned div, unsigned s);
+struct umoddiv_t umoddiv(unsigned div, unsigned s)
+{
+  unsigned i;
+  unsigned tmp;
+
+  if (s == 0)
+    return (struct umoddiv_t){.div = 0, .mod = 0};
+
+  if (s > div)
+    return (struct umoddiv_t){.div = 0, .mod = div};
+
+  tmp = 1;
+  while (!(s & (1U << 31)) && s < div)
+    {
+      s <<= 1;
+      tmp <<= 1;
+    }
+
+  i = 0;
+  while (1)
+    {
+      if (div >= s)
+        {
+          div -= s;
+          i |= tmp;
+        }
+      if (div == 0)
+        break;
+      tmp >>= 1;
+      if (!tmp)
+        break;
+      s >>= 1;
+    }
+
+  return (struct umoddiv_t){.div = i, .mod = div};
+}
 
 /**
  * 64-bit unsigned modulo for 32-bit machines.
