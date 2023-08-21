d3b9184ed55ac6aef505bfbb80b73f580ca9ba13
Frank Mehnert <frank.mehnert@kernkonzept.com>
2023-08-21 00:00:00 +0000

    jdb_tbuf/arm: remove workaround for Clang<9
    Clang<9 is not supported anymore, hence remove a corresponding
    workaround.
    
    Change-Id: I27594deaf55c2c86e5888c355397a0cd53a13a8f

--- src/jdb/arm/32/jdb_tbuf-arm.cpp
+++ src/jdb/arm/32/jdb_tbuf-arm.cpp
@@ -1,29 +1,6 @@
 INTERFACE [arm && jdb_logging]:
 
-// Clang < 9 does not accept the "c" (constant) modifier.
-// https://bugs.llvm.org/show_bug.cgi?id=40959
-// gcc requires the "c" modifier.
-
-#if defined(__clang__) && (__clang_major__ < 9)
-# define BEGIN_LOG_EVENT(name, sc, fmt)				\
-  do								\
-    {								\
-      Mword __do_log__;						\
-      asm volatile ("1:  mov    %0, #0		\n\t"		\
-		    ".pushsection \".debug.jdb.log_table\" \n\t" \
-		    "3: .long 2f		\n\t"		\
-		    "   .long 1b		\n\t"		\
-		    "   .long %[xfmt]		\n\t"		\
-		    ".section \".rodata.log.str\" \n\t"		\
-		    "2: .asciz "#name"		\n\t"           \
-		    "   .asciz "#sc"		\n\t"		\
-		    ".popsection		\n\t"		\
-		    : "=r"(__do_log__)                          \
-                    : [xfmt] "i" (&Tb_entry_formatter_t<fmt>::singleton)); \
-      if (EXPECT_FALSE( __do_log__ ))				\
-	{
-#else
-# define BEGIN_LOG_EVENT(name, sc, fmt)				\
+#define BEGIN_LOG_EVENT(name, sc, fmt)				\
   do								\
     {								\
       Mword __do_log__;						\
@@ -40,4 +17,3 @@ INTERFACE [arm && jdb_logging]:
                     : [xfmt] "i" (&Tb_entry_formatter_t<fmt>::singleton)); \
       if (EXPECT_FALSE( __do_log__ ))				\
 	{
-#endif
