9588e75bbf5938e56ecab0ee2f6117b3708b6c65
Jan Klötzke <jan.kloetzke@kernkonzept.com>
2023-09-01 10:52:02 +0200

    jdb: fix tbuf entry patching on Arm Thumb2
    When the kernel is compiled with Thumb2 instructions, the "mov
    absolute" instructions have a different encoding.
    
    Change-Id: I8d8ccb9299f7833766fad04e7293f5471530f0f4

--- src/jdb/arm/jdb_tbuf_fe-arm.cpp
+++ src/jdb/arm/jdb_tbuf_fe-arm.cpp
@@ -23,3 +23,25 @@ Jdb_tbuf_fe::set_entry_status(Tb_log_table_entry const *e, unsigned char value)
   check(!Jdb::poke_task(Jdb_address::kmem_addr(e->patch), &insn, sizeof(insn)));
 }
 
+IMPLEMENTATION [arm && thumb2 && jdb_logging]:
+
+#include "globals.h"
+#include "jdb.h"
+#include "jdb_types.h"
+
+IMPLEMENT_OVERRIDE
+unsigned char
+Jdb_tbuf_fe::get_entry_status(Tb_log_table_entry const *e)
+{
+  unsigned char val;
+  check(!Jdb::peek_task(Jdb_address::kmem_addr(e->patch + 2), &val, sizeof(val)));
+  return val;
+}
+
+IMPLEMENT_OVERRIDE
+void
+Jdb_tbuf_fe::set_entry_status(Tb_log_table_entry const *e, unsigned char value)
+{
+  check(Jdb::poke(Jdb_addr<unsigned char>::kmem_addr(e->patch + 2), value));
+}
+
