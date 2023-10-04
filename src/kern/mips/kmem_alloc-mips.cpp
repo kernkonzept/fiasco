83b9c3ce8957b46d0bacac7070c5e2ce8db4ecae
Frank Mehnert <frank.mehnert@kernkonzept.com>
2023-10-04 10:28:01 +0200

    kmem_alloc-mips: coding style and cosmetic fixes
    Also bring this code closer to the other architectures.
    Also place the "panic.h" header at the correct place.
    
    Change-Id: I203624908668213e77f4dfb9c83d4803e3155072

--- src/kern/mips/kmem_alloc-mips.cpp
+++ src/kern/mips/kmem_alloc-mips.cpp
@@ -12,6 +12,7 @@ IMPLEMENTATION [mips]:
 #include "kmem.h"
 #include "koptions.h"
 #include "paging_bits.h"
+#include "panic.h"
 
 #include <cstdio>
 
@@ -23,15 +24,13 @@ IMPLEMENT
 Kmem_alloc::Kmem_alloc()
 {
   Mem_region_map<64> map;
-  Mem_region f;
   unsigned long available_size = create_free_map(Kip::k(), &map);
 
   printf("Available physical memory: %#lx\n", available_size);
 
   // sanity check whether the KIP has been filled out, number is arbitrary
   if (available_size < (1 << 18))
-    panic("Kmem_alloc: No kernel memory available (%ld)\n",
-          available_size);
+    panic("Kmem_alloc: No kernel memory available (%ld)\n", available_size);
 
   unsigned long alloc_size = Koptions::o()->kmemsize << 10;
   if (!alloc_size)
@@ -48,27 +47,29 @@ Kmem_alloc::Kmem_alloc()
   a->init(Mem_layout::phys_to_pmem(0)); //Mem_layout::phys_to_pmem(f.start));
   a->setup_free_map(_freemap, Freemap_size);
 
-  _orig_free = alloc_size;
-  for (unsigned i = 0; alloc_size && (i < map.length()); ++i) {
-    f = map[i];
+  for (unsigned i = 0; alloc_size && i < map.length(); ++i)
+    {
+      Mem_region f = map[i];
 
-    if (f.start >= max_addr)
-      continue;
+      if (f.start >= max_addr)
+        continue;
 
-    unsigned long sz = f.size() > alloc_size ? alloc_size : f.size();
+      if (f.size() > alloc_size)
+        f.end -= f.size() - alloc_size;
 
-    if ((f.start + sz) >= max_addr)
-      sz = max_addr - f.start;
+      if (f.end >= max_addr)
+        f.end = max_addr - 1;
 
-    if (sz < Config::PAGE_SIZE)
-      continue;
+      if (f.size() < Config::PAGE_SIZE)
+        continue;
 
-    Kip::k()->add_mem_region(Mem_desc(f.start, f.start + sz - 1, Mem_desc::Reserved));
-    printf("Add KMEM memory @ %#lx, size %#lx\n", Mem_layout::phys_to_pmem(f.start), sz);
-    a->add_mem((void *)Mem_layout::phys_to_pmem(f.start), sz);
-
-    alloc_size -= sz;
-  }
+      _orig_free += f.size();
+      Kip::k()->add_mem_region(Mem_desc(f.start, f.end, Mem_desc::Reserved));
+      printf("Add KMEM memory @ %#lx, size %#lx\n",
+             Mem_layout::phys_to_pmem(f.start), f.size());
+      a->add_mem((void *)Mem_layout::phys_to_pmem(f.start), f.size());
+      alloc_size -= f.size();
+    }
 
   if (alloc_size)
     panic("Kmem_alloc: cannot allocate sufficient kernel memory");
@@ -80,7 +81,6 @@ IMPLEMENTATION [mips && debug]:
 #include <cstdio>
 
 #include "kip_init.h"
-#include "panic.h"
 
 PUBLIC
 void Kmem_alloc::debug_dump()
