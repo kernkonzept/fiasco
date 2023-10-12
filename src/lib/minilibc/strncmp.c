8908fa67cceeb0790e7f87bb5dc6c0c0c08b086a
Marcus Hähnel <marcus.haehnel@kernkonzept.com>
2023-10-12 11:17:46 +0200

    minilibc: Adapt coding style
    Clean up the coding style of some more minilibc functions. This improves
    readability.
    
    Change-Id: I727fc1f7327d090501a17b55c3ab1e8ac19e0e76

--- src/lib/minilibc/strncmp.c
+++ src/lib/minilibc/strncmp.c
@@ -2,14 +2,20 @@
 #include <string.h>
 
 int strncmp(const char *s1, const char *s2, size_t n) {
-  register const char* a=s1;
-  register const char* b=s2;
-  register const char* fini=a+n;
-  while (a<fini) {
-    register int res=*a-*b;
-    if (res) return res;
-    if (!*a) return 0;
-    ++a; ++b;
-  }
+  register const char* a = s1;
+  register const char* b = s2;
+  register const char* fini = a + n;
+
+  while (a < fini)
+    {
+      register int res = *a - *b;
+      if (res)
+        return res;
+      if (!*a)
+        return 0;
+      ++a;
+      ++b;
+    }
+
   return 0;
 }
--- src/lib/minilibc/vsnprintf.c
+++ src/lib/minilibc/vsnprintf.c
@@ -12,21 +12,24 @@ struct str_data {
   size_t size;
 };
 
-static int swrite(void*ptr, size_t nmemb, struct str_data* sd) {
-  size_t tmp=sd->size-sd->len;
-  if (tmp>0) {
-    size_t len=nmemb;
-    if (len>tmp) len=tmp;
-    memcpy(&sd->str[sd->len],ptr,len);
-    sd->len+=len;
-    sd->str[sd->len]=0;
-  }
+static int swrite(void *ptr, size_t nmemb, struct str_data *sd) {
+  size_t tmp = sd->size-sd->len;
+  if (tmp > 0)
+    {
+      size_t len = nmemb;
+      if (len > tmp)
+        len = tmp;
+      memcpy(&sd->str[sd->len], ptr, len);
+      sd->len += len;
+      sd->str[sd->len] = 0;
+    }
   return nmemb;
 }
 
-int vsnprintf(char* str, size_t size, const char *format, va_list arg_ptr) {
+int vsnprintf(char *str, size_t size, const char *format, va_list arg_ptr) {
   struct str_data sd = { str, 0, size };
   struct output_op ap = { &sd, (output_func*)&swrite };
-  if (size) --sd.size;
-  return __v_printf(&ap,format,arg_ptr);
+  if (size)
+    --sd.size;
+  return __v_printf(&ap, format, arg_ptr);
 }
