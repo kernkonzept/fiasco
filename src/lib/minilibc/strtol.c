d5a051d8bc63ecdd0e619158f9a8d0933a8590b2
Marcus Hähnel <marcus.haehnel@kernkonzept.com>
2023-10-18 16:58:18 +0200

    minilibc: Document strtol
    The behavior of the strtol function deviates from the behavior defined
    by POSIX. So document the function to avoid any false hopes of users of
    the function.
    
    Change-Id: I99e6feb10ca18846f932798d49a6363d2896eade

--- src/lib/minilibc/strtol.c
+++ src/lib/minilibc/strtol.c
@@ -3,7 +3,23 @@
 
 #define ABS_LONG_MIN 2147483648UL
 
-long int strtol (const char *nptr, char **endptr, int base) {
+/**
+ * A function to convert strings to 32-bit integers.
+ *
+ * This function mimics the functionality of POSIX strtol but is limited to
+ * converting to 32-bit values. Contrary to POSIX strtol it does not clamp the
+ * returned value to the target value range but returns 0 in cases where the
+ * number represented by nptr exceeds the 32-bit signed value range.
+ *
+ * \param nptr    Pointer to the string to be converted.
+ * \param endptr  Pointer to the location where to store the address of the
+ *                character after the last valid parsed character. May be NULL.
+ * \param base    The base of the number encoded by the string in nptr.
+ *
+ * \ret  The converted number or 0 if the number exceeded the allowed value
+ *       range.
+ */
+long int strtol(const char *nptr, char **endptr, int base) {
 
   int neg = 0;
   unsigned long int v;
@@ -15,15 +31,15 @@ long int strtol (const char *nptr, char **endptr, int base) {
     neg = -1;
     ++nptr;
   }
-  
+
   v = strtoul (nptr, endptr, base);
-  
+
   if (v >= ABS_LONG_MIN) {
     if (v == ABS_LONG_MIN && neg) {
       return v;
     }
     return 0; //(neg ? LONG_MIN : LONG_MAX);
   }
-  
+
   return (neg ? -v : v);
 }
