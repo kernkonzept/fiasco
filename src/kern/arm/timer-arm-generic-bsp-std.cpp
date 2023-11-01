a019556b0db6a7377a8bed3737d7ba112e370c7c
Frank Mehnert <frank.mehnert@kernkonzept.com>
2023-11-01 17:51:17 +0100

    timer-arm-generic-bsp-std: add static assertion
    Assert on the valid cases. This shall improve coverage.
    
    Change-Id: I85c6015760af1208959371ab69569ba03894cf02

--- src/kern/arm/timer-arm-generic-bsp-std.cpp
+++ src/kern/arm/timer-arm-generic-bsp-std.cpp
@@ -3,12 +3,18 @@ IMPLEMENTATION [arm_generic_timer]:
 PUBLIC static
 unsigned Timer::irq()
 {
+  using T = Generic_timer::Timer_type;
+  static_assert(   static_cast<T>(Gtimer::Type) == Generic_timer::Physical
+                || static_cast<T>(Gtimer::Type) == Generic_timer::Virtual
+                || static_cast<T>(Gtimer::Type) == Generic_timer::Hyp);
+
   switch (Gtimer::Type)
     {
     case Generic_timer::Physical: return 29;
     case Generic_timer::Virtual:  return 27;
     case Generic_timer::Hyp:      return 26;
     };
+  __builtin_unreachable();
 }
 
 IMPLEMENT
