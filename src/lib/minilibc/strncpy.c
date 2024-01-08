56f4e5edc0320702bfea20c1e91328b432e9bebe
Frank Mehnert <frank.mehnert@kernkonzept.com>
2024-01-08 00:00:00 +0000

    strncpy(): add comment about non-POSIX compliance
    Change-Id: Ib071189cca1b2bebea0fa7e78fa13a628054e12d

--- src/lib/minilibc/strncpy.c
+++ src/lib/minilibc/strncpy.c
@@ -4,6 +4,7 @@
 
 char *strncpy(char *dest, const char *src, size_t n)
 {
+  // In contrast to POSIX strncpy(), don't zero-pad the remaining target buffer.
   memccpy(dest,src,0,n);
   return dest;
 }
