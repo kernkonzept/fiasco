527e9effe2eb85c0f042b73983d7b613dc1d60d0
Martin Decky <martin.decky@kernkonzept.com>
2024-01-15 00:00:00 +0000

    Unify coding style of macro definitions
    No change in functionality.
    
    Change-Id: Ibc82a32524a697a3c73dddab6e2f0257e61b9e4b

--- src/kern/initcalls.h
+++ src/kern/initcalls.h
@@ -3,8 +3,8 @@
 
 #include "globalconfig.h"
 
-#define FIASCO_INIT		__attribute__ ((__section__ (".initcall.text")))
-#define FIASCO_INITDATA		__attribute__ ((__section__ (".initcall.data")))
+#define FIASCO_INIT     __attribute__((__section__(".initcall.text")))
+#define FIASCO_INITDATA __attribute__((__section__(".initcall.data")))
 
 #define FIASCO_INIT_AND_PM
 #define FIASCO_INIT_AND_PM_DATA
@@ -13,11 +13,15 @@
 #define FIASCO_INIT_CPU_AND_PM_DATA
 
 #ifdef CONFIG_MP
-# define FIASCO_INIT_CPU
-# define FIASCO_INITDATA_CPU
+
+#define FIASCO_INIT_CPU
+#define FIASCO_INITDATA_CPU
+
 #else
-# define FIASCO_INIT_CPU FIASCO_INIT
-# define FIASCO_INITDATA_CPU FIASCO_INITDATA
+
+#define FIASCO_INIT_CPU     FIASCO_INIT
+#define FIASCO_INITDATA_CPU FIASCO_INITDATA
+
 #endif
 
 #endif // INITCALLS_H__
--- src/lib/libk/std_macros.cpp
+++ src/lib/libk/std_macros.cpp
@@ -1,8 +1,8 @@
 INTERFACE:
 
-#define BUILTIN_EXPECT(exp,c)	__builtin_expect((exp),(c))
-#define EXPECT_TRUE(exp)	__builtin_expect((exp),true)
-#define EXPECT_FALSE(exp)	__builtin_expect((exp),false)
+#define BUILTIN_EXPECT(exp, c) __builtin_expect((exp), (c))
+#define EXPECT_TRUE(exp)       __builtin_expect((exp), true)
+#define EXPECT_FALSE(exp)      __builtin_expect((exp), false)
 
 // Use this for functions which do not examine any values except their
 // arguments and have no effects except the return value. Note that a
@@ -10,36 +10,40 @@ INTERFACE:
 // must _not_ be declared `const'.  Likewise, a function that calls a
 // non-`const' function usually must not be `const'.  It does not make
 // sense for a `const' function to return `void'.
-#define FIASCO_CONST		__attribute__ ((__const__))
+#define FIASCO_CONST __attribute__((__const__))
 
 #ifdef __i386__
-#define FIASCO_FASTCALL		__attribute__ ((__regparm__(3)))
+#define FIASCO_FASTCALL __attribute__((__regparm__(3)))
 #else
 #define FIASCO_FASTCALL
 #endif
 
-#define MARK_AS_DEPRECATED	__attribute__ ((__deprecated__))
-#define ALWAYS_INLINE		__attribute__ ((__always_inline__))
-#define FIASCO_NOINLINE         __attribute__ ((__noinline__))
-#define FIASCO_WARN_RESULT      __attribute__ ((warn_unused_result))
+#define MARK_AS_DEPRECATED __attribute__((__deprecated__))
+#define ALWAYS_INLINE      __attribute__((__always_inline__))
+#define FIASCO_NOINLINE    __attribute__((__noinline__))
+#define FIASCO_WARN_RESULT __attribute__((warn_unused_result))
+#define FIASCO_NORETURN    __attribute__((__noreturn__))
 
-#define FIASCO_NORETURN         __attribute__ ((__noreturn__))
 #ifdef CONFIG_OPTIMIZE_FOR_SIZE
 #define FIASCO_FLATTEN
 #else
-#define FIASCO_FLATTEN          __attribute__((__flatten__))
+#define FIASCO_FLATTEN __attribute__((__flatten__))
 #endif
 
 #ifdef __clang__
-# define FIASCO_NO_UNROLL_LOOPS
-# define FIASCO_LONGCALL
+
+#define FIASCO_NO_UNROLL_LOOPS
+#define FIASCO_LONGCALL
+
 #else
-# define FIASCO_NO_UNROLL_LOOPS __attribute__((optimize("no-unroll-loops")))
-# define FIASCO_LONGCALL        __attribute__((long_call))
+
+#define FIASCO_NO_UNROLL_LOOPS __attribute__((optimize("no-unroll-loops")))
+#define FIASCO_LONGCALL        __attribute__((long_call))
+
 #endif
 
 #define FIASCO_STRINGIFY_(x) #x
-#define FIASCO_STRINGIFY(x) FIASCO_STRINGIFY_(x)
+#define FIASCO_STRINGIFY(x)  FIASCO_STRINGIFY_(x)
 
 IMPLEMENTATION:
 //-
