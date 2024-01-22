83e1afde7216fdee6dcc092f3b6951d0e0cb8297
Jan Klötzke <jan.kloetzke@kernkonzept.com>
2024-01-22 00:00:00 +0000

    arm: add mmio_remap() for MPU based systems
    In addition to mmio_remap() there is an mmio_unmap() call too. The
    number of MPU regions is just too small to waste them for temporary
    kernel mappings.
    
    Change-Id: I0544103fce33a56c01c7a89d9d6a2adabbc0bad0

--- src/kern/arm/kmem-arm.cpp
+++ src/kern/arm/kmem-arm.cpp
@@ -18,3 +18,46 @@ Kmem::is_kmem_page_fault(Mword, Mword ec)
 {
   return !PF::is_usermode_error(ec);
 }
+
+// -----------------------------------------------------------------
+IMPLEMENTATION [arm && mpu]:
+
+#include "kmem_space.h"
+#include "mem.h"
+
+PUBLIC static
+Address
+Kmem::mmio_remap(Address phys, Address size)
+{
+  // Arm MPU regions must be aligned to 64 bytes
+  Address start = phys & ~63UL;
+  Address end = (phys + size - 1U) | 63U;
+
+  // Attention: this only works before the first user space task is created.
+  // Otherwise the mpu regions will collide!
+  auto diff = kdir->add(
+    start, end,
+    Mpu_region_attr::make_attr(L4_fpage::Rights::RW(),
+                               L4_msg_item::Memory_type::Uncached()));
+  assert(diff);
+  Mpu::sync(*kdir, diff.value());
+  Mem::isb();
+
+  return phys;
+}
+
+PUBLIC static
+void
+Kmem::mmio_unmap(Address virt, Address size)
+{
+  Mem::dsb();
+
+  // Arm MPU regions must be aligned to 64 bytes
+  Address start = virt & ~63UL;
+  Address end = (virt + size - 1U) | 63U;
+
+  auto diff = kdir->del(start, end, nullptr);
+  if (diff)
+    Mpu::sync(*kdir, diff.value());
+  Mem::isb();
+}
