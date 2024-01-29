362a880dca1c1084380734fae30e150ba6b354c4
Frank Mehnert <frank.mehnert@kernkonzept.com>
2024-01-29 00:00:00 +0000

    config_tcbsize.h: abort build if no context size is defined
    This is a paranoid check for invalid configurations.
    
    Change-Id: I4537d73fec1ad4fdc1b7d7c591f263206a26874f

--- src/kern/config_tcbsize.h
+++ src/kern/config_tcbsize.h
@@ -3,11 +3,13 @@
 
 #include <globalconfig.h>
 #if defined CONFIG_CONTEXT_8K
-#define THREAD_BLOCK_SIZE (0x2000)
+# define THREAD_BLOCK_SIZE (0x2000)
 #elif defined CONFIG_CONTEXT_4K
-#define THREAD_BLOCK_SIZE (0x1000)
+# define THREAD_BLOCK_SIZE (0x1000)
+#elif defines CONFIG_CONTEXT_2K
+# define THREAD_BLOCK_SIZE (0x800)
 #else
-#define THREAD_BLOCK_SIZE (0x800)
+# error "Which context size?"
 #endif
 
 #endif
