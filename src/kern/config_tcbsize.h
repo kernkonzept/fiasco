0978cb2ec0947da77648cb6fd2d69724c91e3fce
Frank Mehnert <frank.mehnert@kernkonzept.com>
2024-01-29 00:00:00 +0000

    config_tcbsize: fix typo
    The typo was introduced in I4537d73fec1ad4fdc1b7d7c591f263206a26874f.
    
    Change-Id: I98a23d3d54b6ee307aa757edb6abf83720fdaf4d

--- src/kern/config_tcbsize.h
+++ src/kern/config_tcbsize.h
@@ -6,7 +6,7 @@
 # define THREAD_BLOCK_SIZE (0x2000)
 #elif defined CONFIG_CONTEXT_4K
 # define THREAD_BLOCK_SIZE (0x1000)
-#elif defines CONFIG_CONTEXT_2K
+#elif defined CONFIG_CONTEXT_2K
 # define THREAD_BLOCK_SIZE (0x800)
 #else
 # error "Which context size?"
