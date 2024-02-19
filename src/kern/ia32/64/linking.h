3ea03338340a9fed428d1f53c7a986faf949565b
Manuel Kalettka <manuel.kalettka@kernkonzept.com>
2024-02-19 00:00:00 +0000

    Add COV option for amd64
    Change-Id: I725d622963ceb3d1aa15e2091d2ea0ee64e9949b

--- src/kern/ia32/64/linking.h
+++ src/kern/ia32/64/linking.h
@@ -6,9 +6,14 @@
 #define FIASCO_IMAGE_PHYS_START  0x400000
 //#define FIASCO_IMAGE_PHYS_START  0x2000
 #define FIASCO_IMAGE_VIRT_START  0xfffffffff0000000
-#ifndef CONFIG_KERNEL_NX
-#define FIASCO_IMAGE_VIRT_SIZE   0x400000 // must be superpage-aligned
+
+// must be superpage-aligned
+#if !defined(CONFIG_KERNEL_NX) && !defined(CONFIG_COV)
+#define FIASCO_IMAGE_VIRT_SIZE   0x400000
+#elif !defined(CONFIG_COV)
+#define FIASCO_IMAGE_VIRT_SIZE   0x600000
 #else
-#define FIASCO_IMAGE_VIRT_SIZE   0x600000 // must be superpage-aligned
+#define FIASCO_IMAGE_VIRT_SIZE   0x800000
 #endif
+
 #define FIASCO_IMAGE_PHYS_OFFSET (FIASCO_IMAGE_VIRT_START - (FIASCO_IMAGE_PHYS_START & 0xffffffffffc00000))
