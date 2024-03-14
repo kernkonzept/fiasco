b6efb88f4e8f49f0d80b54f311f519d498763477
Jan Klötzke <jan.kloetzke@kernkonzept.com>
2024-03-14 16:25:29 +0100

    koptions: use own instance per AMP node
    Change-Id: I439f79f138c2205cb9757408d8138e888c6c1ad8

--- src/kern/koptions.cpp
+++ src/kern/koptions.cpp
@@ -8,7 +8,8 @@ namespace Koptions {
   Options const *o();
 };
 
-IMPLEMENTATION:
+//---------------------------------------------------------------------------
+IMPLEMENTATION[!amp]:
 
 namespace Koptions_ns
 {
@@ -29,4 +30,45 @@ namespace Koptions_ns
 
 namespace Koptions {
   Options const *o() { return &Koptions_ns::o; }
+};
+
+//---------------------------------------------------------------------------
+IMPLEMENTATION[amp]:
+
+#include "amp_node.h"
+
+namespace Koptions_ns
+{
+  template<unsigned NODES>
+  struct Amp_koptions : Amp_koptions<NODES - 1>
+  {
+    Koptions::Options options =
+      {
+        Koptions::Magic,
+        Koptions::Version_current,
+        0,              // flags
+        0,              // kmemsize
+        { 0, { 0 }, 0, 0, 0, 0, 0, 0 }, // uart
+        0,                              // core_spin_addr
+        "",                             // jdb_cmd
+        0,                              // tbuf_entries
+        0,                              // out_buf
+        cxx::int_value<Amp_phys_id>(Amp_node::phys_id(NODES - 1)),
+      };
+  };
+
+  template<>
+  struct Amp_koptions<0>
+  {};
+
+  Amp_koptions<Amp_node::Max_num_nodes> const
+  o __attribute__((section(".koptions")));
+}
+
+namespace Koptions {
+  Options const *o()
+  {
+    Options const *first = reinterpret_cast<Options const *>(&Koptions_ns::o);
+    return first + Amp_node::id();
+  }
 };
