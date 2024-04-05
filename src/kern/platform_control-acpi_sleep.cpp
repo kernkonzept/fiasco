2ab5675265df82b0481e75f45f01c1e3a7c1c5e4
Georg Kotheimer <georg.kotheimer@kernkonzept.com>
2024-04-05 12:33:57 +0200

    [API] platform_control/ia32: Fix incorrect usage of cpu_call_many
    In preparation for upcoming fix for Cpu_call::cpu_call_many(async=true),
    which then will require also for asynchronous calls that the cpu lock is
    not held.
    
    The existing implementation had two incorrect cpu call usages, which
    could result in deadlocks:
    
    1. Platform_control::system_suspend() makes an asynchronous cpu call to
       the boot CPU, but passes state via the captured extra pointer and
       expects a return value, also via extra. Therefore the cpu call would
       have to be synchronous. Another problem is that the CPU lock is held
       during the cpu call.
    
    2. suspend_ap_cpus() makes an asynchronous cpu call with the CPU lock
       held. Since the code is executed in a DRQ handler, it is not possible
       to release the CPU lock.
    
    It was therefore necessary to rework the system suspend handling. The
    system suspend handling is now executed directly by the calling thread.
    
    This implies introducing a new restriction for the user-space API:
    The Platform_control::system_suspend() operation must only be invoked by
    a thread running on the boot CPU. Furthermore, user-space is responsible
    that the thread is not migrated to a different CPU during the suspend,
    otherwise the system might get stuck.
    
    The restriction seems reasonable, as a system suspend affects the entire
    system, so must only be accessible to an actor that has control over
    the entire system.
    
    See ticket #CD-1244.
    
    Change-Id: Iaa7a044890678c3c817b0e287f982e2d99b344ef

--- src/kern/platform_control-acpi_sleep.cpp
+++ src/kern/platform_control-acpi_sleep.cpp
@@ -12,6 +12,10 @@ IMPLEMENTATION:
 static bool _system_suspend_enabled = false;
 static void (*_system_resume_handler)() = nullptr;
 
+// Ensures that only one system suspend is done at a time, effectively a bool,
+// but stored as an Mword since accessed with cas.
+static Mword _system_suspend_pending;
+
 // Values cached from ACPI FADT, initialized in Platform_control::init
 static Unsigned32 _pm1a, _pm1b, _pm1a_sts, _pm1b_sts;
 static Unsigned32 _fadt_reset_value, _fadt_reset_regs_addr;
@@ -90,6 +94,13 @@ IMPLEMENTATION [mp]:
 
 static Cpu_mask _cpus_to_suspend;
 
+/**
+ * Suspend all AP CPUs and take the boot CPU offline.
+ *
+ * \note Must be executed on the boot CPU, i.e. the calling thread must be and
+ *       stay on the boot CPU.
+ * \note Releases the CPU lock temporarily to make cross-CPU call to AP CPUs.
+ */
 static void
 suspend_ap_cpus()
 {
@@ -97,7 +108,7 @@ suspend_ap_cpus()
   _cpus_to_suspend = Cpu::online_mask();
   _cpus_to_suspend.clear(Cpu_number::boot_cpu());
 
-  Cpu_call::cpu_call_many(_cpus_to_suspend, [](Cpu_number cpu)
+  auto suspend_ap_cpu = [](Cpu_number cpu)
     {
       Context::spill_current_fpu(cpu);
       current()->kernel_context_drq([](Context::Drq *, Context *, void *)
@@ -118,7 +129,13 @@ suspend_ap_cpus()
           return Context::Drq::no_answer_resched();
         }, 0);
       return false;
-    }, true);
+    };
+
+    {
+      // If the thread is migrated here, the suspend will not finish.
+      auto guard = lock_guard<Lock_guard_inverse_policy>(cpu_lock);
+      Cpu_call::cpu_call_many(_cpus_to_suspend, suspend_ap_cpu, true);
+    }
 
   // Wind up pending Rcu and Drq changes together with all _cpus_to_suspend
   check (Context::take_cpu_offline(current_cpu(), true));
@@ -141,11 +158,10 @@ IMPLEMENTATION [!mp]:
 static void suspend_ap_cpus() {}
 static void take_boot_cpu_online() {}
 
-
 IMPLEMENTATION:
 
-#include "cpu_call.h"
 #include "io.h"
+#include "thread_state.h"
 
 PUBLIC static
 void
@@ -155,22 +171,31 @@ Platform_control::set_system_resume_handler(void (*system_resume_handler)())
 }
 
 /**
- * \brief Initiate a full system suspend to RAM.
- * \pre must run on the boot CPU
+ * Initiate a full system suspend to RAM.
+ *
+ * \pre CPU lock must be held.
+ *
+ * \note Must be executed on the boot CPU, i.e. the calling thread must be and
+ *       stay on the boot CPU.
  */
-static Context::Drq::Result
-do_system_suspend(Context::Drq *, Context *, void *data)
+static Mword
+do_system_suspend(Mword sleep_type)
 {
-  assert (current_cpu() == Cpu_number::boot_cpu());
-  Context::spill_current_fpu(current_cpu());
+  assert(cpu_lock.test());
+  assert(current_cpu() == Cpu_number::boot_cpu());
+
+  Mword result = 0;
+
+  // First suspend all the other CPUs in the system (temporarily releases CPU
+  // lock).
   suspend_ap_cpus();
+
+  // Then enter system suspend via ACPI.
+  Context::spill_current_fpu(current_cpu());
 
   facs->fw_wake_vector = phys_wake_vector;
   if (facs->len > 32 && facs->version >= 1)
     facs->x_fw_wake_vector = 0;
-
-  Mword sleep_type = *static_cast<Mword *>(data);
-  *reinterpret_cast<Mword*>(data) = 0;
 
   Pm_object::run_on_suspend_hooks(current_cpu());
 
@@ -181,7 +206,7 @@ do_system_suspend(Context::Drq *, Context *, void *data)
   if (acpi_save_cpu_and_suspend(sleep_type,
                                 (_pm1b << 16) | _pm1a,
                                 (_pm1b_sts << 16) | _pm1a_sts))
-    *reinterpret_cast<Mword *>(data) = -L4_err::EInval;
+    result = -L4_err::EInval;
 
   Mem_unit::tlb_flush();
 
@@ -199,28 +224,34 @@ do_system_suspend(Context::Drq *, Context *, void *data)
   if (_system_resume_handler)
     _system_resume_handler();
 
-  return Context::Drq::no_answer_resched();
+  return result;
 }
 
 IMPLEMENT_OVERRIDE
 static int
 Platform_control::system_suspend(Mword extra)
 {
-  auto guard = lock_guard(cpu_lock);
-
   if (!_system_suspend_enabled)
     return -L4_err::ENodev;
 
-  Cpu_mask cpus;
-  cpus.set(Cpu_number::boot_cpu());
+  auto guard = lock_guard(cpu_lock);
 
-  Cpu_call::cpu_call_many(cpus, [&extra](Cpu_number)
-    {
-      current()->kernel_context_drq(do_system_suspend, &extra);
-      return false;
-    }, true);
+  // Must only be invoked from boot CPU and must not be migrated.
+  if (   current()->home_cpu() != Cpu_number::boot_cpu()
+      || current_cpu() != Cpu_number::boot_cpu())
+    return -L4_err::EInval;
 
-  return extra;
+  // Mark systems suspend as pending, fails if already in progress.
+  if (!cas(&_system_suspend_pending, Mword{false}, Mword{true}))
+    return -L4_err::EBusy;
+
+  Mem::mp_mb();
+  Mword result = do_system_suspend(extra);
+  Mem::mp_mb();
+
+  write_now(&_system_suspend_pending, false);
+
+  return result;
 }
 
 IMPLEMENT_OVERRIDE
--- src/kern/platform_control.cpp
+++ src/kern/platform_control.cpp
@@ -8,6 +8,21 @@ public:
   static void init(Cpu_number);
   static bool cpu_shutdown_available();
   static int cpu_allow_shutdown(Cpu_number cpu, bool allow);
+
+  /**
+   * Initate a system suspend.
+   *
+   * Blocks the calling thread until the system resumes from the suspend.
+   *
+   * \pre Must only be called from thread on the boot CPU, which must not be
+   *      migrated to a different CPU during the suspend, otherwise the system
+   *      might get stuck.
+   *
+   * \param extra  Platform-specific additional information to pass to the
+   *               system suspend implementation.
+   *
+   * \return 0 on success, otherwise error code.
+   */
   static int system_suspend(Mword extra);
   static void system_off();
   static void system_reboot();
