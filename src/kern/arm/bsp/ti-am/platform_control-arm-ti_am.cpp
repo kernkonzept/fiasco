13a1259e2b76e4776c4d48c0d11698a2e10f6dfa
Adam Lackorzynski <adam@l4re.org>
2024-04-16 00:17:05 +0200

    arm/ti_am: Remove unneeded headers
    Change-Id: I7387367aecb94a0f59e45fb316736998adf72ee4

--- src/kern/arm/bsp/ti-am/platform_control-arm-ti_am.cpp
+++ src/kern/arm/bsp/ti-am/platform_control-arm-ti_am.cpp
@@ -1,13 +1,5 @@
 IMPLEMENTATION [arm && mp && pf_ti_am]:
 
-#include "psci.h"
-#include "cpu.h"
-#include "panic.h"
-#include "reset.h"
-#include "poll_timeout_counter.h"
-
-#include <cstdio>
-
 PUBLIC static
 void
 Platform_control::boot_ap_cpus(Address phys_tramp_mp_addr)
