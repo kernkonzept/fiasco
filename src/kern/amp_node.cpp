0a30c3c9cd905ec1fb6f0064195d1ebf40df980e
Jan Klötzke <jan.kloetzke@kernkonzept.com>
2024-05-29 10:02:25 +0200

    amp_node: introduce physical- and logical-id distinction
    The physical ID of an AMP node is an opaque number. No assumption about
    the bit representation should be made. For indexing other data
    structures, the logical ID should be used instead. It starts at 0 for
    the primary AMP node and is always monotonically increasing.
    
    Change-Id: I24c2c232b77a84123eb93236509cc14825b15ae3

--- src/kern/amp_node.cpp
+++ src/kern/amp_node.cpp
@@ -1,16 +1,50 @@
 INTERFACE:
 
+#include <cxx/cxx_int>
+#include "std_macros.h"
+
+/**
+ * AMP node physical ID.
+ *
+ * The physical ID is an opaque number. They still have a defined difference
+ * between each other, so that the range
+ *
+ *   [Amp_node::First_node, Amp_node::First_node + Amp_node::Max_num_nodes - 1]
+ *
+ * is properly defined.
+ */
+struct Amp_phys_id
+: cxx::int_type_base<unsigned, Amp_phys_id, unsigned>
+{
+  Amp_phys_id() = default;
+  constexpr explicit Amp_phys_id(unsigned id)
+  : cxx::int_type_base<unsigned, Amp_phys_id, unsigned>(id)
+  {}
+};
+
 class Amp_node
 {
 public:
   /**
-   * Get node-id of current CPU.
+   * Get physical node-id of current CPU.
    *
    * *Must* be an inline function that is completely inlineable. It's used
    * by Platform_control::amp_boot_info_idx() which is called without a
    * stack!
    */
-  static inline unsigned current_node();
+  static inline Amp_phys_id phys_id();
+
+  /**
+   * Get logical node-id of current CPU.
+   *
+   * The boot node logical id is 0. Other nodes start at 1, up to Max_num_nodes
+   * - 1.
+   *
+   * *Must* be an inline function that is completely inlineable. It's used
+   * by Platform_control::amp_boot_info_idx() which is called without a
+   * stack!
+   */
+  static inline unsigned id();
 };
 
 //---------------------------------------------------------------------------
@@ -22,8 +56,26 @@ EXTENSION class Amp_node
 {
 public:
   // The platform is supposed to define `First_node`.
-  enum : unsigned { Max_num_nodes = CONFIG_MP_MAX_CPUS };
+  static constexpr unsigned Max_num_nodes = CONFIG_MP_MAX_CPUS;
 };
+
+//---------------------------------------------------------------------------
+IMPLEMENTATION:
+
+IMPLEMENT inline ALWAYS_INLINE
+unsigned
+Amp_node::id()
+{
+  return phys_id() - First_node;
+}
+
+/**
+ * Convert logical node-ID to physical node-ID.
+ */
+PUBLIC static constexpr
+Amp_phys_id
+Amp_node::phys_id(unsigned node)
+{ return First_node + node; }
 
 //---------------------------------------------------------------------------
 IMPLEMENTATION[!amp]:
@@ -31,10 +83,11 @@ IMPLEMENTATION[!amp]:
 EXTENSION class Amp_node
 {
 public:
-  enum : unsigned { First_node = 0, Max_num_nodes = 1 };
+  static constexpr Amp_phys_id First_node = Amp_phys_id(0);
+  static constexpr unsigned Max_num_nodes = 1;
 };
 
-IMPLEMENT inline
-unsigned
-Amp_node::current_node()
+IMPLEMENT inline ALWAYS_INLINE
+Amp_phys_id
+Amp_node::phys_id()
 { return First_node; }
