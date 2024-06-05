60e793190a6adf7ea4586e96f2168c0dfb271b1d
Georg Kotheimer <georg.kotheimer@kernkonzept.com>
2024-06-05 12:52:01 +0200

    fvp_base_r: Mark Amp_node::phys_id() function as pure
    Bring it in sync with the other BSPs.
    
    Change-Id: I25c43541d2cc9efcfd0a49df99ab030b28c35b8d

--- src/kern/arm/bsp/fvp_base_r/amp_node-arm-fvp_base_r.cpp
+++ src/kern/arm/bsp/fvp_base_r/amp_node-arm-fvp_base_r.cpp
@@ -13,7 +13,7 @@ IMPLEMENTATION [arm && pf_fvp_base_r && amp]:
 #include "processor.h"
 
 IMPLEMENT inline ALWAYS_INLINE NEEDS["processor.h"]
-Amp_phys_id
+FIASCO_PURE Amp_phys_id
 Amp_node::phys_id()
 {
   return Amp_phys_id(cxx::int_value<Cpu_phys_id>(Proc::cpu_id()) & 0xffU);
