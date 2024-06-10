b07cd22d0be131e73af7824683d9929334499f6d
Jan Klötzke <jan.kloetzke@kernkonzept.com>
2024-06-10 00:00:00 +0000

    koptions: unify amp and !amp implementations
    There is no reason to have a dedicated implementation for !amp
    configurations. The Amp_node::id() function is inline and will always
    return 0. This is visible to the compiler so that there should be no
    overhead if AMP is disabled.
    
    Change-Id: I91f07969509eb267bd84b5fb5e170cfdcbb032d8

--- src/kern/koptions.cpp
+++ src/kern/koptions.cpp
@@ -9,32 +9,7 @@ namespace Koptions {
   Options const *o() FIASCO_CONST;
 };
 
-//---------------------------------------------------------------------------
-IMPLEMENTATION[!amp]:
-
-namespace Koptions_ns
-{
-  Koptions::Options const o __attribute__((section(".koptions"))) =
-    {
-      Koptions::Magic,
-      Koptions::Version_current,
-      0,              // flags
-      0,              // kmemsize
-      { 0, { 0 }, 0, 0, 0, 0, 0, 0 }, // uart
-      0,              // core_spin_addr
-      "",             // jdb_cmd
-      0,              // tbuf_entries
-      0,              // out_buf
-      0,              // node
-    };
-}
-
-namespace Koptions {
-  Options const *o() { return &Koptions_ns::o; }
-};
-
-//---------------------------------------------------------------------------
-IMPLEMENTATION[amp]:
+IMPLEMENTATION:
 
 #include "amp_node.h"
 
