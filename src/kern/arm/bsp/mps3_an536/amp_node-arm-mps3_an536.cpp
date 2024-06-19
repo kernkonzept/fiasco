0b7e543a69c2548e6917468636a2158de2b57a7b
Jan Klötzke <jan.kloetzke@kernkonzept.com>
2024-06-19 11:24:29 +0200

    mps3_an536: fix amp build
    Change-Id: I3e73ceb0d08b092074ff1534215dd012ae0bbc27

--- src/kern/arm/bsp/mps3_an536/amp_node-arm-mps3_an536.cpp
+++ src/kern/arm/bsp/mps3_an536/amp_node-arm-mps3_an536.cpp
@@ -7,5 +7,5 @@ IMPLEMENT inline ALWAYS_INLINE NEEDS["processor.h"]
 FIASCO_PURE Amp_phys_id
 Amp_node::phys_id()
 {
-  return Amp_phys_id(cxx::int_value<Cpu_phys_id>(Proc::cpu_id() & 0xffU));
+  return Amp_phys_id(cxx::int_value<Cpu_phys_id>(Proc::cpu_id()) & 0xffU);
 }
