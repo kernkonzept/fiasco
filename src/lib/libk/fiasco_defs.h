84a0a3343fb50ea5e0d70327b18368aa0143bee3
Frank Mehnert <frank.mehnert@kernkonzept.com>
2024-06-21 13:13:31 +0200

    Add FIASCO_HAS_BUILTIN macro
    The __has_builtin operator was only introduced with gcc-10, so we
    cannot use it with older compilers. Therefore introduce the helper macro
    FIASCO_HAS_BUILTIN, which forwards to __has_builtin if it is defined and
    evaluates to 0 otherwise.
    
    Change-Id: Ib8ffe24ca5db92adb10ae589edfb98d4475125e6

--- src/lib/libk/fiasco_defs.h
+++ src/lib/libk/fiasco_defs.h
@@ -8,3 +8,9 @@
 #define FIASCO_U_CONST(value)   FIASCO_MACRO_JOIN(value, U)
 #define FIASCO_UL_CONST(value)  FIASCO_MACRO_JOIN(value, UL)
 #define FIASCO_ULL_CONST(value) FIASCO_MACRO_JOIN(value, ULL)
+
+#ifdef __has_builtin
+#define FIASCO_HAS_BUILTIN(def) __has_builtin(def)
+#else
+#define FIASCO_HAS_BUILTIN(def) 0
+#endif
