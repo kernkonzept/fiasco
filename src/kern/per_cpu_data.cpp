506e3691d0c453eb65d0280dea788fd4d362bc68
Jan Klötzke <jan.kloetzke@kernkonzept.com>
2024-06-24 09:41:14 +0200

    per_cpu_data: fix Per_cpu_ptr with Static_object
    Internally, the Per_cpu objects use Global_data members. This class has
    a specialization for Static_object to make the Global_data wrapping as
    transparent as possible. But this overload lacks the address-of
    operator. Hence we have to unwrap the Global_data object to get a
    consistent behaviour, regardless of the underlying type.
    
    Change-Id: I5c0ee70ef13a2b9c282df5a0a2d388e4686c4b89

--- src/kern/per_cpu_data.cpp
+++ src/kern/per_cpu_data.cpp
@@ -118,10 +118,10 @@ public:
     Per_cpu<cxx::remove_cv_t<T>> > Per_cpu_type;
 
   Per_cpu_ptr() {}
-  Per_cpu_ptr(Per_cpu_type *o) : _p(&o->_d) {}
+  Per_cpu_ptr(Per_cpu_type *o) : _p(&o->_d.unwrap()) {}
   Per_cpu_ptr &operator = (Per_cpu_type *o)
   {
-    _p = &o->_d;
+    _p = &o->_d.unwrap();
     return *this;
   }
 
