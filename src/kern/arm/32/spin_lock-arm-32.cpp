0479b04581e686a9a391640163b2891c981431cc
Georg Kotheimer <georg.kotheimer@kernkonzept.com>
2024-07-03 12:01:11 +0200

    spin_lock: Optimize implementation for ARMv8 AArch32
    ARMv8 makes a few modifications to the architecture, which allow for an
    improved spin lock implementation:
    - Clearing the global monitor results in a wakeup event being generated,
      thereby removing the need for a DSB barrier after unlock.
    - Introduction of load instructions with acquire semantics and store
      instructions with release semantics.
    
    Change-Id: I5ff9018b36bc8ca29f5b1a3c08e3fd12deef4cbd

--- src/kern/arm/32/spin_lock-arm-32.cpp
+++ src/kern/arm/32/spin_lock-arm-32.cpp
@@ -7,7 +7,7 @@ EXTENSION class Spin_lock
                 "unsupported spin-lock type for ARM");
 };
 
-IMPLEMENTATION [arm && mp]:
+IMPLEMENTATION [arm && mp && !arm_v8plus]:
 
 #include "processor.h"
 
@@ -58,6 +58,64 @@ Spin_lock<Lock_t>::unlock_arch()
       : [lock] "+m" (_lock), [tmp] "=&r" (tmp)); \
   Mem::dsb(); \
   __asm__ __volatile__("sev")
+
+  switch (sizeof(Lock_t))
+    {
+    case 1: UNLOCK_ARCH(b); break;
+    case 2: UNLOCK_ARCH(h); break;
+    case 4: UNLOCK_ARCH(); break;
+    }
+
+#undef UNLOCK_ARCH
+}
+
+IMPLEMENTATION [arm && mp && arm_v8plus]:
+
+#include "processor.h"
+
+IMPLEMENT template<typename Lock_t> inline NEEDS["processor.h"]
+void
+Spin_lock<Lock_t>::lock_arch()
+{
+  Lock_t dummy, tmp;
+
+#define LOCK_ARCH(z) \
+  __asm__ __volatile__ ( \
+      "1: ldaex"#z"   %[d], %[lock]          \n" \
+      "   tst     %[d], #2                   \n" \
+      "   wfene                              \n" \
+      "   orreq   %[tmp], %[d], #2           \n" \
+      "   strex"#z"eq %[d], %[tmp], %[lock]  \n" \
+      "   teqeq   %[d], #0                   \n" \
+      "   bne     1b                         \n" \
+      : [d] "=&r" (dummy), [tmp] "=&r"(tmp), [lock] "+Q" (_lock) \
+      : : "cc", "memory" \
+      )
+
+  switch(sizeof(Lock_t))
+    {
+    case 1: LOCK_ARCH(b); break;
+    case 2: LOCK_ARCH(h); break;
+    case 4: LOCK_ARCH(); break;
+    }
+
+#undef LOCK_ARCH
+}
+
+IMPLEMENT template<typename Lock_t> inline
+void
+Spin_lock<Lock_t>::unlock_arch()
+{
+  Lock_t tmp;
+
+#define UNLOCK_ARCH(z) \
+  __asm__ __volatile__( \
+      "ldr"#z " %[tmp], %[lock]  \n" \
+      "bic %[tmp], %[tmp], #2    \n" /* Arch_lock == #2 */ \
+      "stl"#z " %[tmp], %[lock]  \n" \
+      : [lock] "+Q" (_lock), [tmp] "=&r" (tmp) \
+      : : "memory" \
+      )
 
   switch (sizeof(Lock_t))
     {
