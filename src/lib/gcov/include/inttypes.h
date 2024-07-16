96cd821ec00a22913d54de82b12c1f07ee46fa3b
Valentin Gehrke <valentin.gehrke@kernkonzept.com>
2024-07-16 04:38:04 +0200

    lib/gcov: define necessary int types
    Required in
    ./src/lib/gcov/contrib/llvm_compiler-rt_profile/profile/InstrProfData.inc
    
    Change-Id: Ib25e72840b0b19df5a62cd5fe6b74cb597315be6

--- src/lib/gcov/include/inttypes.h
+++ src/lib/gcov/include/inttypes.h
@@ -1,3 +1,8 @@
 #pragma once
 
 #include <stdint.h>
+
+typedef unsigned long long uint64_t;
+typedef unsigned int       uint32_t;
+typedef unsigned short     uint16_t;
+typedef unsigned char      uint8_t;
