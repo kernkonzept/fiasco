2753542c0a6eb571dcedbb48b502cf400961b07e
Frank Mehnert <frank.mehnert@kernkonzept.com>
2024-08-22 20:35:15 +0200

    merge remaining ia32-amd64 modules
    Modules containing code for x86/32 as well as code for x86/64 are
    usually named module_name-ia32.
    
    Change-Id: Id81987b63ff4b51ca36a999e0e8c917b3e2c7e36

--- src/jdb/ia32/jdb_bp-ia32.cpp
+++ src/jdb/ia32/jdb_bp-ia32.cpp
@@ -61,14 +61,9 @@ private:
 
 class Jdb_bp
 {
-public:
-  static int		global_breakpoints();
-  static void		init_arch();
-
 private:
-  static void		at_jdb_enter();
-  static void		at_jdb_leave();
   static Breakpoint	bps[4];
+  static Mword          dr7;
 };
 
 
@@ -116,6 +111,7 @@ private:
 };
 
 Breakpoint Jdb_bp::bps[4];
+Mword Jdb_bp::dr7;
 
 char const * const Breakpoint::mode_names[4] =
 {
@@ -406,6 +402,16 @@ Breakpoint::test_log(Thread *t)
     }
 }
 
+PUBLIC inline
+void
+Breakpoint::set(Jdb_address _addr, Mword _len, Mode _mode, Log _log)
+{
+  addr = _addr;
+  mode = _mode;
+  log  = _log;
+  len  = _len;
+}
+
 
 STATIC_INITIALIZE_P(Jdb_bp, JDB_MODULE_INIT_PRIO);
 
@@ -420,6 +426,22 @@ Jdb_bp::init()
   Jdb::jdb_leave.add(&leave);
 
   init_arch();
+}
+
+PUBLIC static inline ALWAYS_INLINE
+void
+Jdb_bp::write_debug_register(unsigned num, Mword val)
+{
+  asm volatile("mov %0, %%db%c1" :: "r" (val), "i"(num));
+}
+
+PUBLIC static inline ALWAYS_INLINE
+Mword
+Jdb_bp::read_debug_register(unsigned num)
+{
+  Mword val;
+  asm volatile("mov %%db%c1, %0" : "=r"(val) : "i"(num));
+  return val;
 }
 
 static inline
@@ -593,6 +615,146 @@ Jdb_bp::list()
   putchar('\n');
 }
 
+PUBLIC static inline ALWAYS_INLINE NEEDS[Jdb_bp::read_debug_register]
+Mword
+Jdb_bp::get_dr(Mword i)
+{
+  switch (i)
+    {
+    case 0: return read_debug_register(0);
+    case 1: return read_debug_register(1);
+    case 2: return read_debug_register(2);
+    case 3: return read_debug_register(3);
+    case 6: return read_debug_register(6);
+    case 7: return dr7;
+    default: return 0;
+    }
+}
+
+PRIVATE static
+int
+Jdb_bp::global_breakpoints()
+{
+  return 1;
+}
+
+static
+int
+Jdb_bp::set_debug_address_register(int num, Jdb_address addr, Mword len,
+                                   Breakpoint::Mode mode)
+{
+  clr_dr7(num, dr7);
+  set_dr7(num, len, mode, dr7);
+  switch (num)
+    {
+    case 0: write_debug_register(0, addr.addr()); break;
+    case 1: write_debug_register(1, addr.addr()); break;
+    case 2: write_debug_register(2, addr.addr()); break;
+    case 3: write_debug_register(3, addr.addr()); break;
+    default:;
+    }
+  return 1;
+}
+
+static
+void
+Jdb_bp::clr_debug_address_register(int num)
+{
+  clr_dr7(num, dr7);
+}
+
+PRIVATE static
+void
+Jdb_bp::at_jdb_enter()
+{
+  dr7 = read_debug_register(7);
+  // disable breakpoints while we are in kernel debugger
+  write_debug_register(7, dr7 & Val_enter);
+}
+
+PRIVATE static
+void
+Jdb_bp::at_jdb_leave()
+{
+  write_debug_register(6, Val_leave);
+  write_debug_register(7, dr7);
+}
+
+/** @return 1 if single step occurred */
+PRIVATE static
+int
+Jdb_bp::test_sstep(Cpu_number, Jdb_entry_frame *)
+{
+  Mword dr6 = read_debug_register(6);
+  if (!(dr6 & Val_test_sstep))
+    return 0;
+
+  // single step has highest priority, don't consider other conditions
+  write_debug_register(6, Val_leave);
+  return 1;
+}
+
+/** @return 1 if breakpoint occurred */
+PRIVATE static
+int
+Jdb_bp::test_break(Cpu_number cpu, Jdb_entry_frame *ef, String_buffer *buf)
+{
+  Mword dr6 = read_debug_register(6);
+  if (!(dr6 & Val_test))
+    return 0;
+
+  int ret = test_break(cpu, ef, buf, dr6);
+  write_debug_register(6, dr6 & ~Val_test);
+  return ret;
+}
+
+/** @return 1 if other debug exception occurred */
+PRIVATE static
+int
+Jdb_bp::test_other(Cpu_number, Jdb_entry_frame *, String_buffer *buf)
+{
+  Mword dr6 = read_debug_register(6);
+  if (!(dr6 & Val_test_other))
+    return 0;
+
+  buf->printf("unknown trap 1 (dr6=" L4_PTR_FMT ")", dr6);
+  write_debug_register(6, Val_leave);
+  return 1;
+}
+
+/** @return 1 if only breakpoints were logged and jdb should not be entered */
+PRIVATE static
+int
+Jdb_bp::test_log_only(Cpu_number, Jdb_entry_frame *)
+{
+  Mword dr6 = read_debug_register(6);
+
+  if (dr6 & Val_test)
+    {
+      dr7 = read_debug_register(7);
+      // disable breakpoints -- we might trigger a r/w breakpoint again
+      write_debug_register(7, dr7 & Val_enter);
+      test_log(dr6);
+      write_debug_register(6, dr6);
+      write_debug_register(7, dr7);
+      if (!(dr6 & Val_test_other))
+        // don't enter jdb, breakpoints only logged
+        return 1;
+    }
+
+  // enter jdb
+  return 0;
+}
+
+PRIVATE static
+void
+Jdb_bp::init_arch()
+{
+  Jdb::bp_test_log_only = test_log_only;
+  Jdb::bp_test_break    = test_break;
+  Jdb::bp_test_sstep    = test_sstep;
+  Jdb::bp_test_other    = test_other;
+}
 
 //---------------------------------------------------------------------------//
 
--- src/jdb/ia32/jdb_kern_info-ia32.cpp
+++ src/jdb/ia32/jdb_kern_info-ia32.cpp
@@ -1,9 +1,315 @@
-IMPLEMENTATION[ia32,amd64]:
+IMPLEMENTATION[ia32 || amd64]:
 
 #include <cstdio>
 #include <cstring>
 #include "simpleio.h"
+
+#include "config.h"
+#include "cpu.h"
+#include "gdt.h"
+#include "idt.h"
+#include "io.h"
+#include "i8259.h"
 #include "jdb_screen.h"
+#include "perf_cnt.h"
+#include "pic.h"
+#include "space.h"
+#include "tss.h"
+
+
+class Jdb_kern_info_idt : public Jdb_kern_info_module
+{
+};
+
+static Jdb_kern_info_idt k_I INIT_PRIORITY(JDB_MODULE_INIT_PRIO + 1);
+
+PUBLIC
+Jdb_kern_info_idt::Jdb_kern_info_idt()
+  : Jdb_kern_info_module('I', "Interrupt Descriptor Table (IDT)")
+{
+  Jdb_kern_info::register_subcmd(this);
+}
+
+PUBLIC
+void
+Jdb_kern_info_idt::show() override
+{
+  Pseudo_descriptor idt_pseudo;
+  unsigned line = 0;
+
+  Idt::get (&idt_pseudo);
+
+  printf("idt base=" L4_PTR_FMT "  limit=%04x (%04x bytes)\n",
+         idt_pseudo.base(),
+         static_cast<unsigned>((idt_pseudo.limit() + 1) / sizeof(Idt_entry)),
+         static_cast<unsigned>(idt_pseudo.limit()) + 1);
+  if (!Jdb_core::new_line(line))
+    return;
+
+  Idt_entry *ie = reinterpret_cast<Idt_entry*>(idt_pseudo.base());
+  for (unsigned i=0; i<(idt_pseudo.limit()+1)/sizeof(Idt_entry); i++)
+    {
+      printf("%3x: ",i);
+      ie[i].show();
+      if (!Jdb_core::new_line(line))
+        return;
+    }
+}
+
+class Jdb_kern_info_test_tsc_scaler : public Jdb_kern_info_module
+{
+};
+
+static Jdb_kern_info_test_tsc_scaler k_tts INIT_PRIORITY(JDB_MODULE_INIT_PRIO+1);
+
+PUBLIC
+Jdb_kern_info_test_tsc_scaler::Jdb_kern_info_test_tsc_scaler()
+  : Jdb_kern_info_module('T', "Test TSC scaler")
+{
+  Jdb_kern_info::register_subcmd(this);
+}
+
+PUBLIC
+void
+Jdb_kern_info_test_tsc_scaler::show() override
+{
+  while (Kconsole::console()->getchar(false) == -1)
+    {
+      Unsigned64 t;
+      t = Cpu::boot_cpu()->ns_to_tsc(Cpu::boot_cpu()->tsc_to_ns(Cpu::rdtsc()));
+      printf("Diff (press any key to stop): %llu\n", Cpu::rdtsc() - t);
+    }
+}
+
+class Jdb_kern_info_pic_state : public Jdb_kern_info_module
+{
+};
+
+static Jdb_kern_info_pic_state k_p INIT_PRIORITY(JDB_MODULE_INIT_PRIO+1);
+
+PUBLIC
+Jdb_kern_info_pic_state::Jdb_kern_info_pic_state()
+  : Jdb_kern_info_module('p', "PIC ports")
+{
+  Jdb_kern_info::register_subcmd(this);
+}
+
+void
+Jdb_kern_info_pic_state::show() override
+{
+  typedef Irq_chip_i8259<Io> I8259;
+  int i;
+  static char const hex[] = "0123456789ABCDEF";
+
+  // show important I/O ports
+  Io::out8_p(I8259::OCW_TEMPLATE | I8259::READ_NEXT_RD | I8259::READ_IS_ONRD,
+             Pic::MASTER_ICW );
+  unsigned in_service = Io::in8(Pic::MASTER_ICW);
+  Io::out8_p(I8259::OCW_TEMPLATE | I8259::READ_NEXT_RD | I8259::READ_IR_ONRD,
+             Pic::MASTER_ICW);
+  unsigned requested = Io::in8(Pic::MASTER_ICW);
+  unsigned mask = Jdb::pic_status & 0x0ff;
+  printf("master PIC: in service:");
+  for (i=7; i>=0; i--)
+    putchar((in_service & (1<<i)) ? hex[i] : '-');
+  printf(", request:");
+  for (i=7; i>=0; i--)
+    putchar((requested & (1<<i)) ? hex[i] : '-');
+  printf(", mask:");
+  for (i=7; i>=0; i--)
+    putchar((mask & (1<<i)) ? hex[i] : '-');
+  putchar('\n');
+
+  Io::out8_p( I8259::OCW_TEMPLATE | I8259::READ_NEXT_RD | I8259::READ_IS_ONRD,
+              Pic::SLAVES_ICW);
+  in_service = Io::in8(Pic::SLAVES_ICW);
+  Io::out8_p( I8259::OCW_TEMPLATE | I8259::READ_NEXT_RD | I8259::READ_IR_ONRD,
+              Pic::SLAVES_ICW);
+  requested = Io::in8(Pic::SLAVES_ICW);
+  mask = Jdb::pic_status >> 8;
+  printf(" slave PIC: in service:");
+  for (i=7; i>=0; i--)
+    putchar((in_service & (1<<i)) ? hex[i+8] : '-');
+  printf(", request:");
+  for (i=7; i>=0; i--)
+    putchar((requested & (1<<i)) ? hex[i+8] : '-');
+  printf(", mask:");
+  for (i=7; i>=0; i--)
+    putchar((mask & (1<<i)) ? hex[i+8] : '-');
+  putchar('\n');
+}
+
+
+class Jdb_kern_info_misc : public Jdb_kern_info_module
+{
+};
+
+static Jdb_kern_info_misc k_i INIT_PRIORITY(JDB_MODULE_INIT_PRIO + 1);
+
+PUBLIC
+Jdb_kern_info_misc::Jdb_kern_info_misc()
+  : Jdb_kern_info_module('i', "Miscellaneous info")
+{
+  Jdb_kern_info::register_subcmd(this);
+}
+
+PUBLIC
+void
+Jdb_kern_info_misc::show() override
+{
+  Cpu_time clock = Jdb::system_clock_on_enter();
+  printf("clck: %08llx.%08llx\n", clock >> 32, clock & 0xffffffffU);
+
+  show_pdir();
+
+  Pseudo_descriptor gdt_pseudo, idt_pseudo;
+  Gdt::get (&gdt_pseudo);
+  Idt::get (&idt_pseudo);
+  printf("idt : base=" L4_PTR_FMT "  limit=%04x\n"
+         "gdt : base=" L4_PTR_FMT "  limit=%04x\n",
+         idt_pseudo.base(), (idt_pseudo.limit() + 1) / 8,
+         gdt_pseudo.base(), (gdt_pseudo.limit() + 1) / 8);
+
+  // print LDT
+  printf("ldt : %04x", Cpu::get_ldt());
+  if (Cpu::get_ldt() != 0)
+    {
+      Gdt_entry *e = Cpu::boot_cpu()->get_gdt()->entries()
+                     + (Cpu::boot_cpu()->get_ldt() >> 3);
+      printf(": " L4_PTR_FMT "-" L4_PTR_FMT,
+             e->base(), e->base()+ e->size());
+    }
+
+  // print TSS
+  printf("\n"
+         "tr  : %04x", Cpu::boot_cpu()->get_tr());
+
+  if (Cpu::get_tr() != 0)
+    {
+      Gdt_entry *e = Cpu::boot_cpu()->get_gdt()->entries()
+                     + (Cpu::boot_cpu()->get_tr() >> 3);
+      printf(": " L4_PTR_FMT "-" L4_PTR_FMT ", iobitmap at " L4_PTR_FMT,
+             e->base(), e->base() + e->size(),
+             e->base() + (reinterpret_cast<Tss *>(e->base())->_hw.ctx.iopb));
+    }
+
+  printf("\n"
+         "cr0 : " L4_PTR_FMT "\n"
+         "cr4 : " L4_PTR_FMT "\n",
+         Cpu::get_cr0(), Cpu::get_cr4());
+}
+
+PRIVATE inline NEEDS["jdb_screen.h"]
+void
+Jdb_kern_info_misc::show_pdir()
+{
+  Mem_space *s = Mem_space::current_mem_space(Cpu_number::boot_cpu());
+  printf("%s" L4_PTR_FMT "\n",
+         Jdb_screen::Root_page_table, reinterpret_cast<Address>(s->dir()));
+}
+
+
+class Jdb_kern_info_cpu : public Jdb_kern_info_module
+{
+};
+
+static Jdb_kern_info_cpu k_c INIT_PRIORITY(JDB_MODULE_INIT_PRIO + 1);
+
+PUBLIC
+Jdb_kern_info_cpu::Jdb_kern_info_cpu()
+  : Jdb_kern_info_module('c', "CPU features")
+{
+  Jdb_kern_info::register_subcmd(this);
+}
+
+PUBLIC
+void
+Jdb_kern_info_cpu::show() override
+{
+  const char *perf_type = Perf_cnt::perf_type();
+  char cpu_mhz[32];
+  char time[32];
+  unsigned hz;
+  constexpr char const * const scheduler_mode[]
+    = { "PIT", "RTC", "APIC", "HPET" };
+
+  cpu_mhz[0] = '\0';
+  if ((hz = Cpu::boot_cpu()->frequency()))
+    {
+      unsigned mhz = hz / 1000000;
+      hz -= mhz * 1000000;
+      unsigned khz = hz / 1000;
+      snprintf(cpu_mhz, sizeof(cpu_mhz), "%u.%03u MHz", mhz, khz);
+    }
+
+  printf ("CPU: %s %s (%s)\n",
+          Cpu::boot_cpu()->model_str(), cpu_mhz,
+          Config::found_vmware ? "vmware" : "native");
+  Cpu::boot_cpu()->show_cache_tlb_info("     ");
+  show_features();
+  show_feature_ia32_tsc_adjust();
+
+  if (Cpu::boot_cpu()->tsc())
+    {
+      Unsigned32 hour, min, sec, ns;
+      Cpu::boot_cpu()->tsc_to_s_and_ns(Cpu::rdtsc(), &sec, &ns);
+      hour = sec  / 3600;
+      sec -= hour * 3600;
+      min  = sec  / 60;
+      sec -= min  * 60;
+      snprintf(time, sizeof(time), "%02u:%02u:%02u.%06u",
+               hour, min, sec, ns/1000);
+    }
+  else
+    strcpy(time, "not available");
+
+  printf("\nTimer interrupt source: %s (irq vector 0x%02x)"
+         "\nPerformance counters: %s"
+         "\nLast branch recording: %s"
+         "\nDebug store to memory: %s"
+         "\nTime stamp counter: %s"
+         "\n",
+         scheduler_mode[Config::Scheduler_mode],
+         Config::scheduler_irq_vector,
+         perf_type ? perf_type : "no",
+         Cpu::boot_cpu()->lbr_type() != Cpu::Lbr_unsupported
+            ? Cpu::boot_cpu()->lbr_type() == Cpu::Lbr_pentium_4 ? "P4" : "P6"
+            : "no",
+         Cpu::boot_cpu()->bts_type() != Cpu::Bts_unsupported
+            ? Cpu::boot_cpu()->bts_type() == Cpu::Bts_pentium_4 ? "P4" : "Pentium-M"
+            : "no",
+         time
+         );
+}
+
+PRIVATE
+void
+Jdb_kern_info_cpu::show_feature_ia32_tsc_adjust()
+{
+  if (Cpu::cpuid_eax(0) >= 7 && (Cpu::cpuid_ebx(7) & 2))
+    {
+      printf("\nIA32_TSC_ADJUST values:\n");
+      for (Cpu_number u = Cpu_number::first(); u < Config::max_num_cpus(); ++u)
+        if (Cpu::online(u))
+          {
+            printf("     CPU[%2u]: ", cxx::int_value<Cpu_number>(u));
+
+            auto get_tsc_adj = [](Cpu_number)
+              {
+                Unsigned64 v = ~0ULL;
+                Jdb::rdmsr(MSR_IA32_TSC_ADJUST, &v);
+                printf("%lld\n", v);
+              };
+
+            if (u == Cpu_number::boot_cpu())
+              get_tsc_adj(u);
+            else
+              Jdb::remote_work(u, get_tsc_adj, true);
+
+            Mem::barrier();
+          }
+    }
+}
 
 static char const *end_of_table = reinterpret_cast<char const *>(-1);
 
@@ -71,6 +377,7 @@ Jdb_kern_info_cpu::show_features()
     "pbe (pending break enable)",
     end_of_table
   };
+
   static const char *const extended[] =
   {
     "pni (prescott new instructions)",
@@ -97,6 +404,7 @@ Jdb_kern_info_cpu::show_features()
     "avx", "f16c",
     end_of_table
   };
+
   static const char *const ext_81_ecx[] =
   {
     NULL, NULL, "svm (secure virtual machine)", NULL, NULL,
@@ -106,6 +414,7 @@ Jdb_kern_info_cpu::show_features()
     "lwp", "fmaa", NULL, NULL, "nodeid", NULL, "tbm", "topext",
     end_of_table
   };
+
   static const char *const ext_81_edx[] =
   {
     NULL, NULL, NULL, NULL,
@@ -153,11 +462,137 @@ Jdb_kern_info_cpu::show_features()
     }
 }
 
-PRIVATE inline NEEDS["jdb_screen.h"]
-void
-Jdb_kern_info_misc::show_pdir()
+
+class Jdb_kern_info_gdt : public Jdb_kern_info_module
 {
-  Mem_space *s = Mem_space::current_mem_space(Cpu_number::boot_cpu());
-  printf("%s" L4_PTR_FMT "\n",
-         Jdb_screen::Root_page_table, reinterpret_cast<Address>(s->dir()));
+private:
+  static unsigned line;
+};
+
+static Jdb_kern_info_gdt k_g INIT_PRIORITY(JDB_MODULE_INIT_PRIO + 1);
+
+unsigned Jdb_kern_info_gdt::line;
+
+PUBLIC
+Jdb_kern_info_gdt::Jdb_kern_info_gdt()
+  : Jdb_kern_info_module('g', "Global Descriptor Table (GDT)")
+{
+  Jdb_kern_info::register_subcmd(this);
+}
+
+PRIVATE static
+void
+Jdb_kern_info_gdt::show_gdt(Cpu_number cpu)
+{
+  Gdt *gdt = Cpu::cpus.cpu(cpu).get_gdt();
+  unsigned entries = Gdt::gdt_max / 8;
+
+  if (Config::Max_num_cpus > 1)
+    printf("CPU%u: GDT base=" L4_PTR_FMT "  limit=%04x (%04x bytes)\n",
+           cxx::int_value<Cpu_number>(cpu), reinterpret_cast<Mword>(gdt),
+           entries, Gdt::gdt_max);
+  else
+    printf("GDT base=" L4_PTR_FMT "  limit=%04x (%04x bytes)\n",
+           reinterpret_cast<Mword>(gdt), entries, Gdt::gdt_max);
+
+  if (!Jdb_core::new_line(line))
+    return;
+
+  for (unsigned i = 0; i < entries; i++)
+    {
+      printf(" %02x: ", i * 8);
+      if (i == 0)
+        printf("(ignored)\n");
+      else
+        (*gdt)[i].show();
+      if (!Jdb_core::new_line(line))
+        return;
+      if (i != 0 && (*gdt)[i].desc_size() == 16)
+        ++i;
+    }
+}
+
+PUBLIC
+void
+Jdb_kern_info_gdt::show() override
+{
+  line = 0;
+  Jdb::foreach_cpu(&show_gdt);
+}
+
+// ------------------------------------------------------------------------
+IMPLEMENTATION [(ia32 || amd64) && hpet_timer]:
+
+#include "hpet.h"
+
+class Jdb_kern_info_hpet_smm : public Jdb_kern_info_module
+{};
+
+static Jdb_kern_info_hpet_smm ki_smm INIT_PRIORITY(JDB_MODULE_INIT_PRIO + 1);
+
+PUBLIC
+Jdb_kern_info_hpet_smm::Jdb_kern_info_hpet_smm()
+  : Jdb_kern_info_module('S', "SMM loop using HPET")
+{
+  Jdb_kern_info::register_subcmd(this);
+}
+
+PUBLIC
+void
+Jdb_kern_info_hpet_smm::show() override
+{
+  const unsigned config_spin_loops = 10000;
+  const unsigned config_hist_loops = 60;
+  unsigned delta = 1;
+  Mword counter_good = 0;
+  Mword histsum = 0;
+  Mword hist_loops = config_hist_loops;
+
+  printf("HPET SMM Check: Press key to stop.\n");
+  printf("HPET SMM Check Loop testing (loops=%d)\n", config_spin_loops);
+
+  Hpet::hpet()->dump();
+  Hpet::hpet()->enable();
+  while (1)
+    {
+      Unsigned64 x1 = Hpet::hpet()->counter_val;
+
+      int i = config_spin_loops;
+      while (i--)
+        asm volatile("" : : : "memory");
+
+      Unsigned64 diff = Hpet::hpet()->counter_val - x1;
+
+      if (hist_loops)
+        {
+          histsum += diff;
+          --hist_loops;
+
+          if (hist_loops == 0)
+            {
+              delta = (histsum + histsum / 9) / config_hist_loops;
+              printf("HPET SMM Check threshold=%dhpet-clks %lldus\n",
+                     delta,
+                     (delta * Hpet::hpet()->counter_clk_period()) / 1000000000ULL);
+            }
+        }
+      else
+        {
+          if (diff > delta && diff < (~0UL - delta * 2))
+            {
+              printf("%lld  %lldus (before %ld good iterations)\n", diff,
+                     (diff * Hpet::hpet()->counter_clk_period()) / 1000000000ULL,
+                     counter_good);
+              counter_good = 0;
+              if (Kconsole::console()->getchar(false) != -1)
+                break;
+            }
+          else
+            ++counter_good;
+
+          if (counter_good % 30000 == 2)
+            if (Kconsole::console()->getchar(false) != -1)
+              break;
+        }
+    }
 }
