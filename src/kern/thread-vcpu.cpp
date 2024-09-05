c2a9f39c9905d822a6dee4a2fa95e09fdf255ee9
Georg Kotheimer <georg.kotheimer@kernkonzept.com>
2024-09-05 21:11:56 +0200

    Revert "Remove unsed Thread::arch_vcpu_set_user_space()"
    This reverts commit 2f33bce1090ceac5ff892473a1ce6f1a2882234d.
    
    Reason for revert: The hook was introduced for and is used by RISC-V.
    
    Change-Id: I0273334d93ed48d6cd877a174e9eebe423de7e3b

--- src/kern/thread-vcpu.cpp
+++ src/kern/thread-vcpu.cpp
@@ -34,6 +34,7 @@ protected:
    *                            to be initialized.
    */
   void arch_init_vcpu_state(Vcpu_state *vcpu_state, bool ext);
+  void arch_vcpu_set_user_space();
 };
 
 IMPLEMENTATION:
@@ -52,6 +53,10 @@ void Thread::arch_init_vcpu_state(Vcpu_state *vcpu, bool /*ext*/)
   vcpu->version = Vcpu_arch_version;
 }
 
+IMPLEMENT_DEFAULT inline
+void Thread::arch_vcpu_set_user_space()
+{}
+
 PUBLIC inline NEEDS["task.h"]
 void
 Thread::vcpu_set_user_space(Task *t)
@@ -62,6 +67,8 @@ Thread::vcpu_set_user_space(Task *t)
 
   Task *old = static_cast<Task*>(_space.vcpu_user());
   _space.vcpu_user(t);
+
+  arch_vcpu_set_user_space();
 
   if (old && !old->dec_ref())
     delete old;
