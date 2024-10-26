f03ef4c40e898de8f1ea1f53f373239dc28392b7
Adam Lackorzynski <adam@l4re.org>
2024-10-26 06:50:34 +0200

    arm/imx8: Use generic core bootup function
    instead of the code duplicated one.
    
    Change-Id: Ic893224c2801393260addd51742256d267b02d4c

--- src/kern/arm/bsp/imx/platform_control-arm-imx.cpp
+++ src/kern/arm/bsp/imx/platform_control-arm-imx.cpp
@@ -114,36 +114,11 @@ Platform_control::boot_ap_cpus(Address phys_tramp_mp_addr)
 // ------------------------------------------------------------------------
 IMPLEMENTATION [arm && mp && arm_v8 && arm_psci]:
 
-#include "cpu.h"
-#include "psci.h"
-#include "minmax.h"
-
-#include <cstdio>
-
 PUBLIC static
 void
 Platform_control::boot_ap_cpus(Address phys_tramp_mp_addr)
 {
-  int seq = 1;
-  unsigned coreid[] = { 0x000, 0x001, 0x002, 0x003,
-                        0x100, 0x101 };
-
-  for (int i = 0; i < min<int>(Num_cores, Config::Max_num_cpus); ++i)
-    {
-      int r = Psci::cpu_on(coreid[i], phys_tramp_mp_addr);
-      if (r)
-        {
-          if (r != Psci::Psci_already_on)
-            printf("KERNEL: CPU%d boot-up error: %d\n", i, r);
-          continue;
-        }
-
-      while (!Cpu::online(Cpu_number(seq)))
-        {
-          Mem::barrier();
-          Proc::pause();
-        }
-      ++seq;
-    }
+  boot_ap_cpus_psci(phys_tramp_mp_addr,
+                    { 0x000, 0x001, 0x002, 0x003, 0x100, 0x101 }, true);
 }
 
