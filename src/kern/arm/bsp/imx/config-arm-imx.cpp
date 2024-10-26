8c53ba14276268ea34f577f07a0c236e940f7771
Adam Lackorzynski <adam@l4re.org>
2024-10-26 07:02:10 +0200

    arm/imx: Add missing names for SoCs
    Change-Id: Ia3dfbdf4fa2944f749e8be0af9bc656831c66df1

--- src/kern/arm/bsp/imx/config-arm-imx.cpp
+++ src/kern/arm/bsp/imx/config-arm-imx.cpp
@@ -21,3 +21,23 @@ INTERFACE [arm && pf_imx_53]:
 INTERFACE [arm && pf_imx_6]:
 
 #define TARGET_NAME "i.MX6"
+
+INTERFACE [arm && pf_imx_6ul]:
+
+#define TARGET_NAME "i.MX6UL"
+
+INTERFACE [arm && pf_imx_7]:
+
+#define TARGET_NAME "i.MX7"
+
+INTERFACE [arm && pf_imx_8m]:
+
+#define TARGET_NAME "i.MX8M"
+
+INTERFACE [arm && pf_imx_8mp]:
+
+#define TARGET_NAME "i.MX8MP"
+
+INTERFACE [arm && pf_imx_8xq]:
+
+#define TARGET_NAME "i.MX8XQ"
