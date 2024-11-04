f73f8859c6733760eed3f010ff2939e1dcb7eeb7
Frank Mehnert <frank.mehnert@kernkonzept.com>
2024-11-04 00:00:00 +0000

    ARM/32/generic_timer: document system register names
    Change-Id: I531b9c45f28c98d404bcaad855750842ba7c48dd

--- src/kern/arm/32/generic_timer.cpp
+++ src/kern/arm/32/generic_timer.cpp
@@ -20,31 +20,53 @@ namespace Generic_timer {
      * virtual timer
      */
     static Unsigned64 counter() // use the virtual counter
-    { Unsigned64 v; asm volatile("mrrc p15, 1, %Q0, %R0, c14" : "=r" (v)); return v; }
+    {
+      Unsigned64 v;
+      asm volatile("mrrc p15, 1, %Q0, %R0, c14" : "=r" (v)); // CNTVCT
+      return v;
+    }
 
     static Unsigned64 compare() // use virtual compare
-    { Unsigned64 v; asm volatile("mrrc p15, 3, %Q0, %R0, c14" : "=r" (v)); return v; }
+    {
+      Unsigned64 v;
+      asm volatile("mrrc p15, 3, %Q0, %R0, c14" : "=r" (v)); // CNTV_CVAL
+      return v;
+    }
 
     static void compare(Unsigned64 v)
-    { asm volatile("mcrr p15, 3, %Q0, %R0, c14" : : "r" (v)); }
+    {
+      asm volatile("mcrr p15, 3, %Q0, %R0, c14" : : "r" (v)); // CNTV_CVAL
+    }
 
     static Unsigned32 control()
-    { Unsigned32 v; asm volatile("mrc p15, 0, %0, c14, c3, 1" : "=r" (v)); return v; }
+    {
+      Unsigned32 v;
+      asm volatile("mrc p15, 0, %0, c14, c3, 1" : "=r" (v)); // CNTV_CTL
+      return v;
+    }
 
     static void control(Unsigned32 v)
-    { asm volatile("mcr p15, 0, %0, c14, c3, 1" : : "r" (v)); }
+    {
+      asm volatile("mcr p15, 0, %0, c14, c3, 1" : : "r" (v)); // CNTV_CTL
+    }
 
     static void setup_timer_access()
     {
       // CNTKCTL: allow access to virtual counter from PL0
-      asm volatile("mcr p15, 0, %0, c14, c1, 0" : : "r"(0x2));
+      asm volatile("mcr p15, 0, %0, c14, c1, 0" : : "r"(0x2)); // CNTKCTL
     }
 
     static Unsigned32 frequency()
-    { Unsigned32 v; asm volatile ("mrc p15, 0, %0, c14, c0, 0": "=r" (v)); return v; }
+    {
+      Unsigned32 v;
+      asm volatile ("mrc p15, 0, %0, c14, c0, 0": "=r" (v)); // CNTFRQ
+      return v;
+    }
 
     static void frequency(Unsigned32 v)
-    { asm volatile ("mcr p15, 0, %0, c14, c0, 0": :"r" (v)); }
+    {
+      asm volatile ("mcr p15, 0, %0, c14, c0, 0": :"r" (v)); // CNTFRQ
+    }
   };
 
   template<> struct T<Physical>
@@ -55,31 +77,53 @@ namespace Generic_timer {
      * virtual timer
      */
     static Unsigned64 counter() // use the physical counter
-    { Unsigned64 v; asm volatile("mrrc p15, 0, %Q0, %R0, c14" : "=r" (v)); return v; }
+    {
+      Unsigned64 v;
+      asm volatile("mrrc p15, 0, %Q0, %R0, c14" : "=r" (v)); // CNTPCT
+      return v;
+    }
 
     static Unsigned64 compare() // use PL1 physical compare
-    { Unsigned64 v; asm volatile("mrrc p15, 2, %Q0, %R0, c14" : "=r" (v)); return v; }
+    {
+      Unsigned64 v;
+      asm volatile("mrrc p15, 2, %Q0, %R0, c14" : "=r" (v)); // CNTP_CVAL
+      return v;
+    }
 
     static void compare(Unsigned64 v)
-    { asm volatile("mcrr p15, 2, %Q0, %R0, c14" : : "r" (v)); }
+    {
+      asm volatile("mcrr p15, 2, %Q0, %R0, c14" : : "r" (v)); // CNTP_CVAL
+    }
 
     static Unsigned32 control()
-    { Unsigned32 v; asm volatile("mrc p15, 0, %0, c14, c2, 1" : "=r" (v)); return v; }
+    {
+      Unsigned32 v;
+      asm volatile("mrc p15, 0, %0, c14, c2, 1" : "=r" (v)); // CNTP_CTL
+      return v;
+    }
 
     static void control(Unsigned32 v)
-    { asm volatile("mcr p15, 0, %0, c14, c2, 1" : : "r" (v)); }
+    {
+      asm volatile("mcr p15, 0, %0, c14, c2, 1" : : "r" (v)); // CNTP_CTL
+    }
 
     static void setup_timer_access()
     {
        // CNTKCTL: allow access to virtual and physical counter from PL0
-      asm volatile("mcr p15, 0, %0, c14, c1, 0" : : "r"(0x3));
+      asm volatile("mcr p15, 0, %0, c14, c1, 0" : : "r"(0x3)); // CNTKCTL
     }
 
     static Unsigned32 frequency()
-    { Unsigned32 v; asm volatile ("mrc p15, 0, %0, c14, c0, 0": "=r" (v)); return v; }
+    {
+      Unsigned32 v;
+      asm volatile ("mrc p15, 0, %0, c14, c0, 0": "=r" (v)); // CNTFRQ
+      return v;
+    }
 
     static void frequency(Unsigned32 v)
-    { asm volatile ("mcr p15, 0, %0, c14, c0, 0": :"r" (v)); }
+    {
+      asm volatile ("mcr p15, 0, %0, c14, c0, 0": :"r" (v)); // CNTFRQ
+    }
   };
 
   template<> struct T<Hyp>
@@ -90,19 +134,35 @@ namespace Generic_timer {
      * HYP mode timer
      */
     static Unsigned64 counter() // use the physical counter
-    { Unsigned64 v; asm volatile("mrrc p15, 0, %Q0, %R0, c14" : "=r" (v)); return v; }
+    {
+      Unsigned64 v;
+      asm volatile("mrrc p15, 0, %Q0, %R0, c14" : "=r" (v)); // CNTPCT
+      return v;
+    }
 
     static Unsigned64 compare() // use PL2 physical compare
-    { Unsigned64 v; asm volatile("mrrc p15, 6, %Q0, %R0, c14" : "=r" (v)); return v; }
+    {
+      Unsigned64 v;
+      asm volatile("mrrc p15, 6, %Q0, %R0, c14" : "=r" (v)); // CNTHP_CVAL
+      return v;
+    }
 
     static void compare(Unsigned64 v)
-    { asm volatile("mcrr p15, 6, %Q0, %R0, c14" : : "r" (v)); }
+    {
+      asm volatile("mcrr p15, 6, %Q0, %R0, c14" : : "r" (v)); // CNTHP_CVAL
+    }
 
     static Unsigned32 control()
-    { Unsigned32 v; asm volatile("mrc p15, 4, %0, c14, c2, 1" : "=r" (v)); return v; }
+    {
+      Unsigned32 v;
+      asm volatile("mrc p15, 4, %0, c14, c2, 1" : "=r" (v)); // CNTP_CTL
+      return v;
+    }
 
     static void control(Unsigned32 v)
-    { asm volatile("mcr p15, 4, %0, c14, c2, 1" : : "r" (v)); }
+    {
+      asm volatile("mcr p15, 4, %0, c14, c2, 1" : : "r" (v)); // CNTP_CTL
+    }
 
     static void setup_timer_access()
     {
@@ -115,10 +175,16 @@ namespace Generic_timer {
     }
 
     static Unsigned32 frequency()
-    { Unsigned32 v; asm volatile ("mrc p15, 0, %0, c14, c0, 0": "=r" (v)); return v; }
+    {
+      Unsigned32 v;
+      asm volatile ("mrc p15, 0, %0, c14, c0, 0": "=r" (v)); // CNTFRQ
+      return v;
+    }
 
     static void frequency(Unsigned32 v)
-    { asm volatile ("mcr p15, 0, %0, c14, c0, 0": :"r" (v)); }
+    {
+      asm volatile ("mcr p15, 0, %0, c14, c0, 0": :"r" (v)); // CNTFRQ
+    }
   };
 
   template<> struct T<Secure_hyp>
@@ -129,19 +195,35 @@ namespace Generic_timer {
      * secure HYP mode timer
      */
     static Unsigned64 counter() // use the physical counter
-    { Unsigned64 v; asm volatile("mrrc p15, 0, %Q0, %R0, c14" : "=r" (v)); return v; }
+    {
+      Unsigned64 v;
+      asm volatile("mrrc p15, 0, %Q0, %R0, c14" : "=r" (v)); // CNTPCT
+      return v;
+    }
 
     static Unsigned64 compare() // use CNTHPS_CVAL - secure PL2 physical compare
-    { Unsigned64 v; asm volatile("mrrc p15, 2, %Q0, %R0, c14" : "=r" (v)); return v; }
+    {
+      Unsigned64 v;
+      asm volatile("mrrc p15, 2, %Q0, %R0, c14" : "=r" (v)); // CNTHPS_CVAL
+      return v;
+    }
 
     static void compare(Unsigned64 v)
-    { asm volatile("mcrr p15, 2, %Q0, %R0, c14" : : "r" (v)); }
+    {
+      asm volatile("mcrr p15, 2, %Q0, %R0, c14" : : "r" (v)); // CNTHPS_CVAL
+    }
 
     static Unsigned32 control() // use CNTHPS_CTL
-    { Unsigned32 v; asm volatile("mrc p15, 0, %0, c14, c2, 1" : "=r" (v)); return v; }
+    {
+      Unsigned32 v;
+      asm volatile("mrc p15, 0, %0, c14, c2, 1" : "=r" (v)); // CNTHPS_CTL
+      return v;
+    }
 
     static void control(Unsigned32 v)
-    { asm volatile("mcr p15, 0, %0, c14, c2, 1" : : "r" (v)); }
+    {
+      asm volatile("mcr p15, 0, %0, c14, c2, 1" : : "r" (v)); // CNTHPS_CTL
+    }
 
     static void setup_timer_access()
     {
@@ -154,10 +236,16 @@ namespace Generic_timer {
     }
 
     static Unsigned32 frequency()
-    { Unsigned32 v; asm volatile ("mrc p15, 0, %0, c14, c0, 0": "=r" (v)); return v; }
+    {
+      Unsigned32 v;
+      asm volatile ("mrc p15, 0, %0, c14, c0, 0": "=r" (v)); // CNTFRQ
+      return v;
+    }
 
     static void frequency(Unsigned32 v)
-    { asm volatile ("mcr p15, 0, %0, c14, c0, 0": :"r" (v)); }
+    {
+      asm volatile ("mcr p15, 0, %0, c14, c0, 0": :"r" (v)); // CNTFRQ
+    }
   };
 }
 
