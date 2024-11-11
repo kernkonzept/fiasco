2574caf8226c6acd509db4ffd214c20c088524dc
Adam Lackorzynski <adam@l4re.org>
2024-11-11 00:00:00 +0000

    arm: sbsa: improve redistributor probing
    There are two ways to find the GIC redistributor of a CPU on Arm ACPI
    platforms: via a "GIC Redistributor (GICR) Structure" or from the "GIC
    CPU Interface (GICC) Structure" directly.
    
    So far, Fiasco only supported a single "GIC Redistributor (GICR)
    Structure". In fact, there can be more than one as per specification.
    
    More importantly, the GICR structure is not usable on all platforms.
    Only if all redistributors are in the always-on power domain, such a
    structure can be used. On all other platforms, the GICC structure hold
    the pointer to the redistributor. So if no GICR structure is present,
    we use the GICC redistributor field.
    
    Change-Id: I6d5e63f626666397829e6e3fde05fe3da80a6084

--- src/kern/arm/bsp/sbsa/pic-arm-sbsa.cpp
+++ src/kern/arm/bsp/sbsa/pic-arm-sbsa.cpp
@@ -9,7 +9,62 @@ IMPLEMENTATION [arm && pic_gic && pf_sbsa]:
 #include "acpi.h"
 #include "irq_mgr_msi.h"
 #include "gic_v3.h"
+#include "gic_redist.h"
 #include "kmem_mmio.h"
+
+class Gic_redist_find_acpi : public Gic_redist_find
+{
+public:
+  Gic_redist_find_acpi(const Acpi_madt *madt)
+  : _madt(madt)
+  {}
+
+  Mmio_register_block get_redist_mmio(Unsigned64 mpid) override
+  {
+    bool region = false;
+    for (auto const *redist : _madt->iterate<Acpi_madt::Gic_redistributor_if>())
+      {
+        region = true;
+        // Assumes Kmem_mmio::map is smart and reuses existing mappings
+        auto *r = Kmem_mmio::map(redist->base, redist->length);
+        if (!r)
+          return Mmio_register_block();
+        auto b = scan_range(r, mpid);
+        if (b.valid())
+          return b;
+      }
+
+    // if there was a Acpi_madt::Gic_redistributor_if, other options do not
+    // exist per ACPI spec
+    if (region)
+      return Mmio_register_block();
+
+    Unsigned64 mpid_pure = mpid & 0xff00ffffff;
+    // Think: The loop gets longer and longer the more cores we boot.
+    // Assuming the ACPI table is MPID ordered (which often seems the case)
+    // we could remember the loop iterator and continue there...
+    for (auto const *gicc : _madt->iterate<Acpi_madt::Gic_cpu_if>())
+      {
+        if ((gicc->flags & Acpi_madt::Gic_cpu_if::Enabled)
+            && gicc->mpidr == mpid_pure)
+          {
+            /* The size is just the maximum that could be there, to know better
+             * we'd need to map it to check Gic_redist::GICR_TYPER.vlpis and
+             * then map again with the potentially bigger size */
+            if (auto *r = Kmem_mmio::map(gicc->gicr_base,
+                                         Gic_redist::GICR_frame_size * 4))
+              return Mmio_register_block(r);
+
+            break;
+          }
+      }
+
+    return Mmio_register_block();
+  }
+
+private:
+  const Acpi_madt *_madt;
+};
 
 PUBLIC static FIASCO_INIT
 void
@@ -28,14 +83,11 @@ Pic::init()
   if (dist->version < Acpi_madt::Gic_distributor_if::V3)
     panic("SBSA: GICv3 or higher expected!\n");
 
-  // Should we support more than one redistributor range?
-  auto *redist = madt->find<Acpi_madt::Gic_redistributor_if>(0);
-  if (!redist)
-    panic("SBSA: no redistributor defined in MADT\n");
+  auto *redist_find = new Boot_object<Gic_redist_find_acpi>(madt);
 
   auto *g =
     new Boot_object<Gic_v3>(Kmem_mmio::map(dist->base, Gic_dist::Size),
-                            Kmem_mmio::map(redist->base, redist->length));
+                            redist_find);
 
   int i = 0;
   while (auto *its = madt->find<Acpi_madt::Gic_its_if>(i++))
