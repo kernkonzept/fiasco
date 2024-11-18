3acb976f08292078d2619d16edf22672dce4878f
Frank Mehnert <frank.mehnert@kernkonzept.com>
2024-11-18 00:00:00 +0000

    timeslice_timeout: fix spelling typo in comment
    Change-Id: If86b8515d77e53e60c980c9f46c62713ec352425

--- src/kern/timeslice_timeout.cpp
+++ src/kern/timeslice_timeout.cpp
@@ -15,7 +15,7 @@ IMPLEMENTATION:
 #include "sched_context.h"
 #include "std_macros.h"
 
-/* Initialize global valiable timeslice_timeout */
+/* Initialize global variable timeslice_timeout */
 DEFINE_PER_CPU Per_cpu<Timeout *> timeslice_timeout;
 DEFINE_PER_CPU static Per_cpu<Timeslice_timeout> the_timeslice_timeout(Per_cpu_data::Cpu_num);
 
