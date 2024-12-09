75d9f38e723fc709c470ef249db288ef99567370
Toni Dietze <toni.dietze@kernkonzept.com>
2024-12-09 00:00:00 +0000

    documentation: unify spelling of flexpage
    Change-Id: I59c85b39f57a4a984c7141a6a241c68b30f7e13d

--- src/abi/l4_fpage.cpp
+++ src/abi/l4_fpage.cpp
@@ -14,12 +14,12 @@ typedef cxx::int_type_full<Mword, struct Cap_index_t, Cap_diff, Order> Cap_index
 typedef cxx::int_type_order<unsigned long, struct Port_number_t, Order> Port_number;
 
 /**
- * A L4 flex page.
+ * An L4 flexpage.
  *
- * A flex page represents a naturally aligned area of mappable space,
+ * A flexpage represents a naturally aligned area of mappable space,
  * such as memory, I/O-ports, and capabilities (kernel objects).
- * There is also a representation for describing a flex page that represents
- * the whole of all these address spaces. The size of a flex page is given
+ * There is also a representation for describing a flexpage that represents
+ * the whole of all these address spaces. The size of a flexpage is given
  * as a power of two.
  *
  *
@@ -33,8 +33,8 @@ typedef cxx::int_type_order<unsigned long, struct Port_number_t, Order> Port_num
  *
  *   - The rights bits (0-3) denote the access rights to an object, see
  *     L4_fpage::Rights.
- *   - The \a type of a flex page is denotes the address space that is
- *     referenced by that flex page (see L4_fpage::Type).
+ *   - The \a type of a flexpage is denotes the address space that is
+ *     referenced by that flexpage (see L4_fpage::Type).
  *   - The order is the exponent for the size calculation (size = 2^order).
  *   - The page number denotes the page address within the address space
  *     denoted by \a type. For example when \a type is #Memory, the \a page
@@ -49,19 +49,19 @@ class L4_fpage
 {
 public:
   /**
-   * Data type to represent the binary representation of a flex page.
+   * Data type to represent the binary representation of a flexpage.
    */
   typedef Mword Raw;
 
   /**
-   * Address space type of a flex page.
+   * Address space type of a flexpage.
    */
   enum Type
   {
-    Special = 0, ///< Special flex pages, either invalid or all spaces.
-    Memory,      ///< Memory flex page
-    Io,          ///< I/O-port flex page
-    Obj          ///< Object flex page (capabilities)
+    Special = 0, ///< Special flexpages, either invalid or all spaces.
+    Memory,      ///< Memory flexpage
+    Io,          ///< I/O-port flexpage
+    Obj          ///< Object flexpage (capabilities)
   };
 
   enum { Addr_shift = 12 };
@@ -88,16 +88,16 @@ public:
 
     constexpr Rights apply(Rights r) const { return *this & r; }
 
-    /// Memory flex page is user accessible
+    /// Memory flexpage is user accessible
     static constexpr Rights U() { return Rights(8); }
 
-    /// Memory flex page is readable
+    /// Memory flexpage is readable
     static constexpr Rights R() { return Rights(4); }
 
-    /// Memory flex page is writable
+    /// Memory flexpage is writable
     static constexpr Rights W() { return Rights(2); }
 
-    /// Memory flex page is executable (often equal to #R)
+    /// Memory flexpage is executable (often equal to #R)
     static constexpr Rights X() { return Rights(1); }
 
     static constexpr Rights UR() { return U() | R(); }
@@ -105,49 +105,49 @@ public:
     static constexpr Rights URWX() { return U() | R() | W() | X(); }
     static constexpr Rights URW() { return U() | R() | W(); }
 
-    /// Memory flex page is readable and executable
+    /// Memory flexpage is readable and executable
     static constexpr Rights RX() { return R() | X(); }
 
-    /// Memory flex page is readable, writeable, and executable
+    /// Memory flexpage is readable, writeable, and executable
     static constexpr Rights RWX() { return R() | W() | X(); }
 
-    /// Memory flex page is readable and writable
+    /// Memory flexpage is readable and writable
     static constexpr Rights RW() { return R() | W(); }
 
-    ///< Memory flex page is writable and executable
+    ///< Memory flexpage is writable and executable
     static constexpr Rights WX() { return W() | X(); }
 
 
-    /// Object flex page: delete rights
+    /// Object flexpage: delete rights
     static constexpr Rights CD() { return Rights(0x8); }
 
-    /// Object flex page: read rights (w/o this the mapping is not present)
+    /// Object flexpage: read rights (w/o this the mapping is not present)
     static constexpr Rights CR() { return Rights(0x4); }
 
-    /** Object flex page: strong semantics (object specific, i.e. not having
+    /** Object flexpage: strong semantics (object specific, i.e. not having
      *  this right on an IPC gate demotes all capabilities transferred via this
      *  IPC gate to also suffer this right. */
     static constexpr Rights CS() { return Rights(0x2); }
 
-    /// Object flex page: write rights (purely object specific)
+    /// Object flexpage: write rights (purely object specific)
     static constexpr Rights CW() { return Rights(0x1); }
 
-    /// Object flex page: combine #CR and #CW
+    /// Object flexpage: combine #CR and #CW
     static constexpr Rights CRW() { return CR() | CW(); }
 
-    /// Object flex page: combine #CR and #CS
+    /// Object flexpage: combine #CR and #CS
     static constexpr Rights CRS() { return CR() | CS(); }
 
-    /// Object flex page: combine #CR, #CW, and #CS
+    /// Object flexpage: combine #CR, #CW, and #CS
     static constexpr Rights CRWS() { return CRW() | CS(); }
 
-    /// Object flex page: combine #CS and #CW
+    /// Object flexpage: combine #CS and #CW
     static constexpr Rights CWS() { return CW() | CS(); }
 
-    /// Object flex page: combine #CS, #CW, and #CD
+    /// Object flexpage: combine #CS, #CW, and #CD
     static constexpr Rights CWSD() { return CW() | CS() | CD(); }
 
-    /// Object flex page: combine #CR, #CW, #CS, and #CD
+    /// Object flexpage: combine #CR, #CW, #CS, and #CD
     static constexpr Rights CRWSD() { return CRWS() | CD(); }
 
     /// All rights shall be transferred, independent of the type
@@ -156,7 +156,7 @@ public:
 
 private:
   /**
-   * Create a flex page with the given parameters.
+   * Create a flexpage with the given parameters.
    */
   constexpr L4_fpage(Type type, Mword address,
                      unsigned char order, Rights rights)
@@ -172,46 +172,46 @@ public:
   };
 
   /**
-   * Create an I/O flex page.
+   * Create an I/O flexpage.
    *
-   * IO flex pages do not support access rights other than RW or nothing.
+   * IO flexpages do not support access rights other than RW or nothing.
    * \param port base I/O-port number (0..65535), must be aligned to
    *             2^\a order. The value is shifted by #Addr_shift bits to the
    *             left.
-   * \param order the order of the I/O flex page, size is 2^\a order ports.
+   * \param order the order of the I/O flexpage, size is 2^\a order ports.
    */
   static constexpr L4_fpage io(Mword port, unsigned char order, Rights rights = Rights(0))
   { return L4_fpage(Io, adr_bfm_t::val_dirty(port), order, rights); }
 
   /**
-   * Create an object flex page.
+   * Create an object flexpage.
    *
    * \param idx capability index, note capability indexes are multiples of
    *            0x1000. (hence \a idx is not shifted)
-   * \param order The size of the flex page is 2^\a order. The value in \a idx
+   * \param order The size of the flexpage is 2^\a order. The value in \a idx
    *              must be aligned to 2^(\a order + #Addr_shift.
    */
   static constexpr L4_fpage obj(Mword idx, unsigned char order, Rights rights = Rights(0))
   { return L4_fpage(Obj, idx & adr_bfm_t::Mask, order, rights); }
 
   /**
-   * Create a memory flex page.
+   * Create a memory flexpage.
    *
    * \param addr The virtual address. Only the most significant bits are
    *             considered, bits from 0 to \a order-1 are dropped.
-   * \param order The size of the flex page is 2^\a order in bytes.
+   * \param order The size of the flexpage is 2^\a order in bytes.
    */
   static constexpr L4_fpage mem(Mword addr, unsigned char order, Rights rights = Rights(0))
   { return L4_fpage(Memory, addr & adr_bfm_t::Mask, order, rights); }
 
   /**
-   * Create a nil (invalid) flex page.
+   * Create a nil (invalid) flexpage.
    */
   static constexpr L4_fpage nil()
   { return L4_fpage(0); }
 
   /**
-   * Create a special receive flex page representing
+   * Create a special receive flexpage representing
    * all available address spaces at once. This is used
    * for page-fault and exception IPC.
    */
@@ -219,7 +219,7 @@ public:
   { return L4_fpage(Special, 0, Whole_space, rights); }
 
   /**
-   * Create a flex page from the raw value.
+   * Create a flexpage from the raw value.
    */
   explicit constexpr L4_fpage(Raw raw) : _raw(raw) {}
 
@@ -229,94 +229,94 @@ public:
   typedef Virt_addr Mem_addr;
 
   /**
-   * Get the virtual address of a memory flex page.
+   * Get the virtual address of a memory flexpage.
    *
    * \pre type() must return #Memory to return a valid value.
-   * \return The virtual memory base address of the flex page.
+   * \return The virtual memory base address of the flexpage.
    */
   constexpr Virt_addr mem_address() const
   { return Virt_addr(adr_bfm_t::get_unshifted(_raw)); }
 
   /**
-   * Get the capability address of an object flex page.
+   * Get the capability address of an object flexpage.
    *
    * \pre type() must return #Obj to return a valid value.
-   * \return The capability value (index) of this flex page.
+   * \return The capability value (index) of this flexpage.
    *         This value is not shifted, so it is a multiple of 0x1000.
    *         See obj_index() for reference.
    */
   constexpr Mword obj_address() const { return adr_bfm_t::get_unshifted(_raw); }
 
   /**
-   * Get the I/O-port number of an I/O flex page.
+   * Get the I/O-port number of an I/O flexpage.
    * \pre type() must return #Io to return a valid value.
-   * \return The I/O-port index of this flex page.
+   * \return The I/O-port index of this flexpage.
    */
   constexpr Port_number io_address() const { return Port_number{adr()}; }
 
   /**
-   * Get the capability index of an object flex page.
+   * Get the capability index of an object flexpage.
    *
    * \pre type() must return #Obj to return a valid value.
-   * \return The index into the capability table provided by this flex page.
+   * \return The index into the capability table provided by this flexpage.
    *         This value is shifted #Addr_shift to be a real index
    *         (opposed to obj_address()).
    */
   constexpr Cap_index obj_index() const { return Cap_index{adr()}; }
 
   /**
-   * Test for memory flex page (if type() is #Memory).
+   * Test for memory flexpage (if type() is #Memory).
    * \return true if type() is #Memory.
    */
   constexpr bool is_mempage() const { return type() == Memory; }
 
   /**
-   * Test for I/O flex page (if type() is #Io).
+   * Test for I/O flexpage (if type() is #Io).
    * \return true if type() is #Io.
    */
   constexpr bool is_iopage() const { return type() == Io; }
 
   /**
-   * Test for object flex page (if type() is #Obj).
+   * Test for object flexpage (if type() is #Obj).
    * \return true if type() is #Obj.
    */
   constexpr bool is_objpage() const { return type() == Obj; }
 
 
   /**
-   * Is the flex page the whole address space?
-   * @return not zero, if the flex page covers the
+   * Is the flexpage the whole address space?
+   * @return not zero, if the flexpage covers the
    *   whole address space.
    */
   constexpr Mword is_all_spaces() const
   { return (_raw & (type_bfm_t::Mask | order_bfm_t::Mask)) == order_bfm_t::val(Whole_space); }
 
   /**
-   * Is the flex page valid?
-   * \return not zero if the flex page
+   * Is the flexpage valid?
+   * \return not zero if the flexpage
    *    contains a value other than 0.
    */
   constexpr Mword is_valid() const { return _raw; }
 
   /**
-   * Get the binary representation of the flex page.
-   * \return this flex page in binary representation.
+   * Get the binary representation of the flexpage.
+   * \return this flexpage in binary representation.
    */
   constexpr Raw raw() const { return _raw; }
 
 private:
   Raw _raw;
 
-  /** \name Rights of the flex page */
+  /** \name Rights of the flexpage */
   CXX_BITFIELD_MEMBER( 0,  3, _rights, _raw);
 
 public:
-  /** \name Type of the flex page */
+  /** \name Type of the flexpage */
   CXX_BITFIELD_MEMBER( 4,  5, type, _raw);
-  /** \name Size (as power of 2) of the flex page */
+  /** \name Size (as power of 2) of the flexpage */
   CXX_BITFIELD_MEMBER( 6, 11, order, _raw);
 private:
-  /** \name Address encoded in the flex page */
+  /** \name Address encoded in the flexpage */
   CXX_BITFIELD_MEMBER(12, MWORD_BITS-1, adr, _raw);
 
 public:
@@ -324,21 +324,22 @@ public:
   constexpr Rights rights() const { return Rights(_rights()); }
 
   /**
-   * Rights bits for flex pages.
+   * Rights bits for flexpages.
    *
-   * The particular semantics of the rights bits in a flex page differ depending on the
-   * type of the flex page. For memory there are #R, #W, and #X rights. For
-   * I/O-ports there must be #R and #W, to get access. For object (capabilities)
-   * there are #CD, #CR, #CS, and #CW rights on the object and additional
-   * rights in the map control value of the map operation (see L4_fpage::Obj_map_ctl).
+   * The particular semantics of the rights bits in a flexpage differ depending
+   * on the type of the flexpage. For memory there are #R, #W, and #X rights.
+   * For I/O-ports there must be #R and #W, to get access. For object
+   * (capabilities) there are #CD, #CR, #CS, and #CW rights on the object and
+   * additional rights in the map control value of the map operation (see
+   * L4_fpage::Obj_map_ctl).
    */
 
 
   /**
-   * Mask the flex page with the given rights.
+   * Mask the flexpage with the given rights.
    *
    * \param r Rights mask. All rights missing in the mask are removed.
-   *          The semantics depend on the type (type()) of the flex page.
+   *          The semantics depend on the type (type()) of the flexpage.
    */
   void mask_rights(Rights r) { _raw &= (Mword{cxx::int_value<Rights>(r)} | ~_rights_bfm_t::Mask); }
 };
