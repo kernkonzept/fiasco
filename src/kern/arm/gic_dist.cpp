c8dcd10c6b77f357695519572b205da336a149cb
Adam Lackorzynski <adam@l4re.org>
2024-12-23 00:00:00 +0000

    arm/gic: Add all GICD_TYPER (v3) fields
    Change-Id: I46365627e932c490426e67646a8131bb13bdfcb5

--- src/kern/arm/gic_dist.cpp
+++ src/kern/arm/gic_dist.cpp
@@ -64,9 +64,18 @@ public:
   struct Typer_v3 : public Typer
   {
     explicit Typer_v3(Unsigned32 v) : Typer(v) {}
+    CXX_BITFIELD_MEMBER_RO( 5,  7, cpu_number, raw);
+    CXX_BITFIELD_MEMBER_RO( 8,  8, espi, raw);
+    CXX_BITFIELD_MEMBER_RO( 9,  9, nmi, raw);
     CXX_BITFIELD_MEMBER_RO(11, 15, num_lpis, raw);
+    CXX_BITFIELD_MEMBER_RO(16, 16, mbis, raw);
     CXX_BITFIELD_MEMBER_RO(17, 17, lpis, raw);
+    CXX_BITFIELD_MEMBER_RO(18, 18, dvis, raw);
     CXX_BITFIELD_MEMBER_RO(19, 23, id_bits, raw);
+    CXX_BITFIELD_MEMBER_RO(24, 24, a3v, raw);
+    CXX_BITFIELD_MEMBER_RO(25, 25, no1n, raw);
+    CXX_BITFIELD_MEMBER_RO(26, 26, rss, raw);
+    CXX_BITFIELD_MEMBER_RO(27, 31, espi_range, raw);
   };
 };
 
