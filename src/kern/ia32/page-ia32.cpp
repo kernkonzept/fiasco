08d595072791a2e8756e1fe94c6da35ba1b8f6ed
Frank Mehnert <frank.mehnert@kernkonzept.com>
2025-01-27 00:00:00 +0000

    ia32: document some page table entry bits
    Change-Id: I086f30d92ddc710b626985b3a4cc4cfdcaf68e64

--- src/kern/ia32/page-ia32.cpp
+++ src/kern/ia32/page-ia32.cpp
@@ -5,6 +5,8 @@ EXTENSION class Page
 public:
   enum Attribs_enum : Mword
   {
+    // PAT:7 Dirty:6 Accessed:5 PCD:4 PWT:3 U/S:2 R/W:1 Present:0
+
     MAX_ATTRIBS   = 0x00000006,
     Cache_mask    = 0x00000018, ///< Cache attribute mask
     CACHEABLE     = 0x00000000, ///< PAT=0, PCD=0, PWT=0: PAT0 (WB)
