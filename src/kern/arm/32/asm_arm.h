c0d456b68b17d2e42dbef91859dd6ac90e4a3134
Jan Klötzke <jan.kloetzke@kernkonzept.com>
2025-03-03 00:00:00 +0000

    arm: add some more 32 bit register names
    Change-Id: I8f080d61bf9f17fdb493f5862509bf1f667e8131

--- src/kern/arm/32/asm_arm.h
+++ src/kern/arm/32/asm_arm.h
@@ -2,10 +2,10 @@
 
 #if !defined(__ARM_ARCH) || (__ARM_ARCH < 7)
 .macro dsb scope
-	mcr	p15, 0, r0, c7, c10, 4
+	mcr	p15, 0, r0, c7, c10, 4  // CP15DSB
 .endm
 .macro isb scope
-	mcr	p15, 0, r0, c7, c5, 4
+	mcr	p15, 0, r0, c7, c5, 4   // CP15ISB
 .endm
 
 #endif
