bcf60fbab378533f43abf4cfa5f6c2ecd6173949
Martin Decky <martin.decky@kernkonzept.com>
2025-05-08 14:49:10 +0200

    x86: IRQ entry point code cleanup
    Improve and modernize coding style. No change in functionality.
    
    Change-Id: Ic28deeba3d24ec64d212154a7b555c65c3642566

--- src/kern/ia32/dirq-ia32.cpp
+++ src/kern/ia32/dirq-ia32.cpp
@@ -5,46 +5,42 @@ INTERFACE[ia32 || amd64]:
 
 IMPLEMENTATION[ia32 || amd64]:
 
-#include <cassert>
-
-#include "cpu_lock.h"
-#include "globalconfig.h"
-#include "globals.h"
 #include "irq.h"
 #include "logdefs.h"
-#include "std_macros.h"
 #include "thread.h"
-#include "timer.h"
 
-// screen spinner for debugging purposes
-static inline void irq_spinners(int irqnum)
+/**
+ * Screen spinner for debugging purposes.
+ */
+static inline void irq_spinners([[maybe_unused]] Mword pin)
 {
 #ifdef CONFIG_IRQ_SPINNER
-  Unsigned16 *p = reinterpret_cast<Unsigned16 *>(Mem_layout::Adap_vram_cga_beg);
-  p += (20 + cxx::int_value<Cpu_number>(current_cpu())) * 80 + irqnum;
-  if (p < reinterpret_cast<Unsigned16 *>(Mem_layout::Adap_vram_cga_end))
-    (*p)++;
-#else
-  (void)irqnum;
+  Unsigned16 *cell
+    = reinterpret_cast<Unsigned16 *>(Mem_layout::Adap_vram_cga_beg);
+  cell += (20 + cxx::int_value<Cpu_number>(current_cpu())) * 80 + pin;
+
+  if (cell < reinterpret_cast<Unsigned16 *>(Mem_layout::Adap_vram_cga_end))
+    (*cell)++;
 #endif
 }
 
-/** Hardware interrupt entry point.  Calls corresponding Dirq instance's
-    Dirq::hit() method.
-    @param irqobj hardware-interrupt object
+/**
+ * Hardware interrupt entry point.
+ *
+ * Called with interrupts disabled. Pass execution to the corresponding
+ * Irq_base::hit() method.
+ *
+ * \param irqobj  Hardware-interrupt object.
  */
 extern "C" FIASCO_FASTCALL
 void
-irq_interrupt(Mword irqobj, Mword ip)
+irq_interrupt(Mword irqobj, Mword)
 {
   Thread::assert_irq_entry();
-
   CNT_IRQ;
-  (void)ip;
 
-  // we're entered with disabled irqs
-  Irq_base *i = reinterpret_cast<Irq_base*>(irqobj);
-  i->log();
-  irq_spinners(i->pin());
-  i->hit(nullptr);
+  Irq_base *irq = reinterpret_cast<Irq_base *>(irqobj);
+  irq->log();
+  irq_spinners(irq->pin());
+  irq->hit(nullptr);
 }
