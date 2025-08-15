a82e41a414b8d6856adeae8e3be0245be65d8d7f
Frank Mehnert <frank.mehnert@kernkonzept.com>
2025-08-15 14:43:52 +0200

    libc/glue/puts: fix handling of return codes
    The function __libc_backend_outs() does never return a negative value:
    It returns 1 on success and 0 on error. Consider this in puts() and
    also fix the puts() return code which should be EOF if failure and a
    non-negative value on success.
    
    Change-Id: Ia56458de3d3b64a96f2267e9d09c0ee754183d5e

--- src/lib/libc/glue/puts.c
+++ src/lib/libc/glue/puts.c
@@ -6,11 +6,14 @@ int puts(const char *s)
 {
   unsigned long state = __libc_backend_printf_lock();
   int ret = __libc_backend_outs(s, strlen(s));
-  if (ret >= 0)
+  if (ret > 0)
     {
       char c = '\n';
       ret = __libc_backend_outs(&c, 1);
     }
   __libc_backend_printf_unlock(state);
-  return ret;
+  if (ret == 0)
+    return EOF;
+  else
+    return ret;
 }
