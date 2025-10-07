64eae025d8f6b24ae39fe1b58190058b54fadce6
Frank Mehnert <frank.mehnert@kernkonzept.com>
2025-10-07 10:42:42 +0200

    libc/stdio/vfprintf: remove DBL/LDBL format specifier states
    Support for printing (double) floats / long double floats was removed in
    I4547d909e2d4c5475ddf09516250053ab1d71b15. Remove the corresponding
    states from the format specifier state machine.
    
    Change-Id: I75fc97351cd1450df90b24ac7c24ef8f3af289a5

--- src/lib/libc/src/stdio/vfprintf.c
+++ src/lib/libc/src/stdio/vfprintf.c
@@ -35,7 +35,6 @@ enum {
 	LONG, ULONG,
 	SHORT, USHORT, CHAR, UCHAR,
 	LLONG, SIZET, IMAX, UMAX, PDIFF, UIPTR,
-	DBL, LDBL,
 	MAXSTATE
 };
 
@@ -45,8 +44,6 @@ static const unsigned char states[]['z'-'A'+1] = {
 	{ /* 0: bare types */
 		S('d') = INT, S('i') = INT,
 		S('o') = UINT, S('u') = UINT, S('x') = UINT, S('X') = UINT,
-		S('e') = DBL, S('f') = DBL, S('g') = DBL, S('a') = DBL,
-		S('E') = DBL, S('F') = DBL, S('G') = DBL, S('A') = DBL,
 		S('c') = INT, S('C') = UINT,
 		S('s') = PTR, S('S') = PTR, S('p') = UIPTR, S('n') = PTR,
 		S('l') = LPRE, S('h') = HPRE, S('L') = BIGLPRE,
@@ -54,8 +51,6 @@ static const unsigned char states[]['z'-'A'+1] = {
 	}, { /* 1: l-prefixed */
 		S('d') = LONG, S('i') = LONG,
 		S('o') = ULONG, S('u') = ULONG, S('x') = ULONG, S('X') = ULONG,
-		S('e') = DBL, S('f') = DBL, S('g') = DBL, S('a') = DBL,
-		S('E') = DBL, S('F') = DBL, S('G') = DBL, S('A') = DBL,
 		S('c') = UINT, S('s') = PTR, S('n') = PTR,
 		S('l') = LLPRE,
 	}, { /* 2: ll-prefixed */
@@ -75,8 +70,6 @@ static const unsigned char states[]['z'-'A'+1] = {
 		S('x') = UCHAR, S('X') = UCHAR,
 		S('n') = PTR,
 	}, { /* 5: L-prefixed */
-		S('e') = LDBL, S('f') = LDBL, S('g') = LDBL, S('a') = LDBL,
-		S('E') = LDBL, S('F') = LDBL, S('G') = LDBL, S('A') = LDBL,
 		S('n') = PTR,
 	}, { /* 6: z- or t-prefixed (assumed to be same size) */
 		S('d') = PDIFF, S('i') = PDIFF,
