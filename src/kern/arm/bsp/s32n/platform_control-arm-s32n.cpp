f176400af17c50984fa81c43f5460f99bafb14c1
Manuel Argüelles <manuel.arguelles@nxp.com>
2025-12-12 12:00:27 +0100

    arm: bsp: add NXP S32N5 platform support
    Introduce platform support for NXP S32N5 running on the RTU subsystem.
    This enables L4Re Microhypervisor bring‑up on RTU by adding BSP
    definitions and init hooks.
    
    RTU includes two clusters of Cortex‑R52 (2 cores each), configurable
    in lock‑step or split‑lock. AMP=y is supported across clusters.
    
    Change-Id: I6b495efd3c7b71b6a210b73dff81548ffa13125d
    Signed-off-by: Manuel Argüelles <manuel.arguelles@nxp.com>

---
+++ src/kern/arm/bsp/s32n/platform_control-arm-s32n.cpp
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2023-2025 NXP
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+// ------------------------------------------------------------------------
+IMPLEMENTATION [arm && amp && pf_s32n]:
+
+#include "cpu.h"
+#include "kmem_mmio.h"
+#include "koptions.h"
+#include "mem_layout.h"
+#include "mmio_register_block.h"
+#include "panic.h"
+
+IMPLEMENT_OVERRIDE
+void
+Platform_control::amp_ap_early_init()
+{
+  // Setup core TCMs
+  static Mword const tcm_bases[4] = {
+#if defined(CONFIG_PF_S32N_RTU_0)
+    0x26000000, 0x26400000, 0x27000000, 0x27400000
+#elif defined(CONFIG_PF_S32N_RTU_1)
+    0x28000000, 0x28400000, 0x29000000, 0x29400000
+#elif defined(CONFIG_PF_S32N_RTU_2)
+    0x2A000000, 0x2A400000, 0x2B000000, 0x2B400000
+#elif defined(CONFIG_PF_S32N_RTU_3)
+    0x2C000000, 0x2C400000, 0x2D000000, 0x2D400000
+#else
+#error "Invalid RTU configuration"
+#endif
+  };
+  Mword tcm_base = tcm_bases[cxx::int_value<Amp_phys_id>(Amp_node::phys_id())];
+
+  // IMP_xTCMREGIONR. ENABLEEL2=1, ENABLEEL10=1
+  asm volatile ("mcr p15, 0, %0, c9, c1, 0" : : "r"(tcm_base | 0x00000003));
+  asm volatile ("mcr p15, 0, %0, c9, c1, 1" : : "r"(tcm_base | 0x00100003));
+  asm volatile ("mcr p15, 0, %0, c9, c1, 2" : : "r"(tcm_base | 0x00200003));
+}
+
+PUBLIC static void
+Platform_control::amp_boot_init()
+{
+  // Need to always start on first core of the expected cluster. Otherwise the
+  // GIC initialization or node memory assignment will fail.
+  assert(Amp_node::phys_id() == Amp_node::first_node());
+
+  amp_boot_ap_cpus(Amp_node::Max_cores);
+}
+
+static void
+setup_amp()
+{ Platform_control::amp_prepare_ap_cpus(); }
+
+STATIC_INITIALIZER_P(setup_amp, EARLY_INIT_PRIO);
