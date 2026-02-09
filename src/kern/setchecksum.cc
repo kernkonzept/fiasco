cdd7a3d8c15d3955b6b45b637eed5cc38ee042bc
Frank Mehnert <frank.mehnert@kernkonzept.com>
2026-02-09 00:00:00 +0000

    convert setchecksum.c to C++
    Change-Id: I672e9addff6eb8c0b3223349096a16af3486b23d
    Signed-off-by: Frank Mehnert <frank.mehnert@kernkonzept.com>

--- src/kern/setchecksum.cc
+++ src/kern/setchecksum.cc
@@ -15,26 +15,17 @@ static void usage(void)
   printf("Usage: set_checksum <kernel_image_name> <ro_checksum> <rw_checksum>\n");
 }
 
-static int fd;
-static void *image;
-static struct stat stats;
-static char const * const match = "FIASCOCHECKSUM=";
-
+static char const *const match = "FIASCOCHECKSUM=";
 
 int main(int argc, char *argv[])
 {
-  void *base;
-  uint32_t volatile *chksum;
-  unsigned long left;
-  struct timespec ts[2];
-
-  if (argc!=4)
+  if (argc != 4)
     {
       usage();
       exit(1);
     }
 
-  fd = open(argv[1], O_RDWR | O_EXCL);
+  int fd = open(argv[1], O_RDWR | O_EXCL);
   if (fd < 0)
     {
       fprintf(stderr, "error opening kernel image: '%s': ", argv[1]);
@@ -42,27 +33,30 @@ int main(int argc, char *argv[])
       exit(1);
     }
 
+  struct stat stats;
   if (fstat(fd, &stats) < 0)
     {
       perror("error: can't stat kernel image");
       exit(1);
     }
 
-  image = mmap(NULL, stats.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+  auto *image = static_cast<char *>(mmap(NULL, stats.st_size,
+                                         PROT_READ | PROT_WRITE,
+                                         MAP_SHARED, fd, 0));
   if (image == MAP_FAILED)
     {
       perror("error: can't mmap kernel image");
       exit(1);
     }
 
-  base = image;
-  left = stats.st_size;
-  while ((base = memchr(base, match[0], left)))
+  char *base = image;
+  unsigned long left = stats.st_size;
+  while ((base = static_cast<char *>(memchr(base, match[0], left))))
     {
       if (memcmp(match, base, strlen(match) + 1) == 0)
         break;
 
-      base = (char*)base + 1;
+      base = base + 1;
       left = stats.st_size - (base - image);
     }
 
@@ -72,9 +66,9 @@ int main(int argc, char *argv[])
       return 2;
     }
 
-  base = (char*)base + strlen(match) + 1;
+  base = base + strlen(match) + 1;
 
-  chksum = base;
+  auto *chksum = reinterpret_cast<uint32_t volatile *>(base);
   *chksum = strtoul(argv[2], NULL, 0);
   *(chksum + 1) = strtoul(argv[3], NULL, 0);
 
@@ -85,6 +79,7 @@ int main(int argc, char *argv[])
     }
 
   // restore original access and modification time
+  struct timespec ts[2];
   ts[0].tv_sec  = stats.st_atim.tv_sec;
   ts[0].tv_nsec = stats.st_atim.tv_nsec;
   ts[1].tv_sec  = stats.st_mtim.tv_sec;
