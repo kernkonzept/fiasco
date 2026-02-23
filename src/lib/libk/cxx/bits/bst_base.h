b38e461ceaefd0d50729d73d04c7acf35bcf720f
Adam Lackorzynski <adam@l4re.org>
2026-02-23 00:00:00 +0000

    cxx: Sync license header with user-level
    I.e. point to LICENSE.spdx.
    
    Change-Id: If293f34d1c70758babd1a63f6c2a79898f74b523
    Signed-off-by: Adam Lackorzynski <adam@l4re.org>

--- src/lib/libk/cxx/bits/bst_base.h
+++ src/lib/libk/cxx/bits/bst_base.h
@@ -8,18 +8,7 @@
  *               Carsten Weinhold <weinhold@os.inf.tu-dresden.de>
  *     economic rights: Technische Universität Dresden (Germany)
  *
- * This file is part of TUD:OS and distributed under the terms of the
- * GNU General Public License 2.
- * Please see the COPYING-GPL-2 file for details.
- *
- * As a special exception, you may use this file as part of a free software
- * library without restriction.  Specifically, if other files instantiate
- * templates or use macros or inline functions from this file, or you compile
- * this file and link it with other files to produce an executable, this
- * file does not by itself cause the resulting executable to be covered by
- * the GNU General Public License.  This exception does not however
- * invalidate any other reasons why the executable file might be covered by
- * the GNU General Public License.
+ * License: see LICENSE.spdx (in this directory or the directories above)
  */
 
 #pragma once
