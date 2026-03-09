8a4115a04ae5c606e6f0d71de45fe6a9a9eb9e4b
Marcus Haehnel <marcus.haehnel@kernkonzept.com>
2026-03-09 00:00:00 +0000

    koptions: Use recursive members instead of recursive inheritance
    This prevents shadowing of the member introduced in each inheritance
    step. Due to terminating the recursion at the last node we also do not
    suffer the one byte overhead of having an empty struct member.
    
    Change-Id: If35b448cd40ccf716b50892dd3bb23bf3cf8732c
    Signed-off-by: Marcus Haehnel <marcus.haehnel@kernkonzept.com>
    Signed-off-by: Frank Mehnert <frank.mehnert@kernkonzept.com>

--- src/kern/koptions.cpp
+++ src/kern/koptions.cpp
@@ -15,27 +15,33 @@ IMPLEMENTATION:
 
 namespace Koptions_ns
 {
-  template<unsigned NODES>
-  struct Amp_koptions : Amp_koptions<NODES - 1>
+  constexpr Koptions::Options get_options(int node)
   {
-    Koptions::Options options =
-      {
-        Koptions::Magic,
-        Koptions::Version_current,
-        0,              // flags
-        0,              // kmemsize
-        { 0, 0, 0, 0, 0, 0, "" },       // uart
-        0,                              // core_spin_addr
-        "",                             // jdb_cmd
-        0,                              // tbuf_entries
-        0,                              // out_buf
-        cxx::int_value<Amp_phys_id>(Amp_node::phys_id(NODES - 1)),
-      };
+    return Koptions::Options
+             {
+               Koptions::Magic,
+               Koptions::Version_current,
+               0,                              // flags
+               0,                              // kmemsize
+               { 0, 0, 0, 0, 0, 0, "" },       // uart
+               0,                              // core_spin_addr
+               "",                             // jdb_cmd
+               0,                              // tbuf_entries
+               0,                              // out_buf
+               cxx::int_value<Amp_phys_id>(Amp_node::phys_id(node)),
+             };
+  }
+
+  template<unsigned NODES>
+  struct Amp_koptions
+  {
+    Amp_koptions<NODES - 1> next;
+    Koptions::Options options = get_options(NODES - 1);
   };
 
   template<>
-  struct Amp_koptions<0>
-  {};
+  struct Amp_koptions<1>
+  { Koptions::Options options = get_options(0); };
 
   Amp_koptions<Amp_node::Max_num_nodes> const
   o __attribute__((section(".koptions")));
