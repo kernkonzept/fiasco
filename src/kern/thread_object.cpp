cf8cf2f2fe852e0bc0e4cccf8a73e69d0569e7eb
Jan Klötzke <jan.kloetzke@kernkonzept.com>
2026-03-09 00:00:00 +0000

    thread_object: remove redundant check in Obj_cap::deref
    There is only one "special" capability: the invalid capability selector
    that is interpreted as self reference. Checking again for self() is
    thus redundant.
    
    Change-Id: I590bdff83cf38b9de1ed83bcda0f80f7a2f3630d
    Signed-off-by: Jan Klötzke <jan.kloetzke@kernkonzept.com>

--- src/kern/thread_object.cpp
+++ src/kern/thread_object.cpp
@@ -57,9 +57,7 @@ Obj_cap::deref(L4_fpage::Rights *rights, bool dbg = false)
 
   if (EXPECT_FALSE(special()))
     {
-      if (!self())
-        return nullptr;
-
+      // "self"
       *rights = L4_fpage::Rights::CWS();
       return current;
     }
