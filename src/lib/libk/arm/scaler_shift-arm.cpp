cee19eb0987870fc372912b22f892d553c1b17b7
Frank Mehnert <frank.mehnert@kernkonzept.com>
2026-03-16 00:00:00 +0000

    arm/32: some optimization in kip-time.S / Scaler_shift::transform()
    Use fewer registers avoiding the need to save them to stack. Keep the
    versions in kip-time.S and Scaler_shift::transform() as consistent as
    possible.
    
    Change-Id: Idf4e031e1ff2cb4ebf09335a9dafd04f87ad9737
    Signed-off-by: Frank Mehnert <frank.mehnert@kernkonzept.com>

--- src/lib/libk/arm/scaler_shift-arm.cpp
+++ src/lib/libk/arm/scaler_shift-arm.cpp
@@ -6,23 +6,23 @@ Scaler_shift::transform(Unsigned64 value) const
 {
   Mword lo = value & 0xffffffffU;
   Mword hi = value >> 32;
-  Mword dummy1, dummy2, dummy3, dummy4;
+  Mword dummy1, dummy2, dummy3;
   // This code is written in Assembler so that it doesn't require libgcc. It
   // should be also very similar to kip_time_fn_read_us()/kip_time_fn_read_us()
   // used by userland to get the same results as the kernel code.
-  asm ("umull   %0, %3, %[scaler], %0   \n\t"
-       "umull   %4, %5, %[scaler], %1   \n\t"
-       "adds    %1, %4, %3              \n\t"
-       "adc     %2, %5, #0              \n\t"
+  asm ("umull   %0, %2, %[scaler], %0   \n\t"
+       "umull   %1, %3, %[scaler], %1   \n\t"
+       "adds    %1, %1, %2              \n\t"
+       "adc     %2, %3, #0              \n\t"
        "mov     %3, #32                 \n\t"
        "sub     %3, %3, %[shift]        \n\t"
 #ifdef __thumb__
        "lsl     %4, %1, %[shift]        \n\t"
-       "lsl     %5, %2, %[shift]        \n\t"
        "lsr     %0, %0, %3              \n\t"
        "orr     %0, %0, %4              \n\t"
+       "lsl     %4, %2, %[shift]        \n\t"
        "lsr     %1, %1, %3              \n\t"
-       "orr     %1, %1, %5              \n\t"
+       "orr     %1, %1, %4              \n\t"
 #else
        "lsr     %0, %0, %3              \n\t"
        "orr     %0, %0, %1, LSL %[shift]\n\t"
@@ -30,7 +30,7 @@ Scaler_shift::transform(Unsigned64 value) const
        "orr     %1, %1, %2, LSL %[shift]\n\t"
 #endif
        : "+r"(lo), "+r"(hi),
-         "=&r"(dummy1), "=&r"(dummy2), "=&r"(dummy3), "=&r"(dummy4)
+         "=&r"(dummy1), "=&r"(dummy2), "=&r"(dummy3)
        : [scaler]"r"(scaler), [shift]"r"(shift)
        : "cc");
   return (Unsigned64{hi} << 32) | lo;
