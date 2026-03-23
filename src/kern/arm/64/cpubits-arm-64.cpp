68e40b970e1f4758c00c2832dd726f82282a2644
Marcus Haehnel <marcus.haehnel@kernkonzept.com>
2026-03-23 00:00:00 +0000

    arm64: use integral type for pa_range_bits
    We have Unsigned8 to represent 8-bit values, so use it and not the
    character type.
    
    Change-Id: I662a61cb29a3ea27615bf3f5aee0fdee510778e7
    Signed-off-by: Marcus Haehnel <marcus.haehnel@kernkonzept.com>

--- src/kern/arm/64/cpubits-arm-64.cpp
+++ src/kern/arm/64/cpubits-arm-64.cpp
@@ -5,7 +5,8 @@ INTERFACE [arm && 64bit]:
 class Cpubits
 {
 public:
-  static char constexpr pa_range_bits[16] = { 32, 36, 40, 42, 44, 48, 52, 56 };
+  static constexpr Unsigned8 pa_range_bits[16] = { 32, 36, 40, 42,
+                                                   44, 48, 52, 56 };
 };
 
 // ------------------------------------------------------------------------
