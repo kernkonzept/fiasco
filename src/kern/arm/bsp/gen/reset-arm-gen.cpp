eb2554bd6f012cabece8ca2b776fe5fc5cfd8e6d
Jakub Jermář <jakub.jermar@kernkonzept.com>
2026-04-06 00:00:00 +0000

    arm_gen: Reset via PSCI only if available
    Change-Id: I18a762a85d7cbb25d8f5eb1675a492bcf89170e0
    Signed-off-by: Jakub Jermar <jakub.jermar@kernkonzept.com>

--- src/kern/arm/bsp/gen/reset-arm-gen.cpp
+++ src/kern/arm/bsp/gen/reset-arm-gen.cpp
@@ -2,11 +2,15 @@ IMPLEMENTATION [arm && pf_arm_gen]:
 
 #include "infinite_loop.h"
 #include "psci.h"
+#include <cstdio>
 
 void __attribute__ ((noreturn))
 platform_reset(void)
 {
-  Psci::system_reset();
+  if (Psci::is_detected())
+    Psci::system_reset();
+  else
+    printf("No way to reset the system.\n");
 
   L4::infinite_loop();
 }
